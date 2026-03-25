ccc55f2b5dc35c487014e7641d23dfe6ca8d8eba
deepshika <308341@ust.com>
2026-03-25 18:42:15 +0530

    Aeithmetic Operations Program

--- src/com/ust/ArithmeticOperations.java
+++ src/com/ust/ArithmeticOperations.java
@@ -14,12 +14,15 @@ class Arithmetic {
     public static int add(int a, int b) {
         return a + b;
     }
+
     public static int subtract(int a, int b) {
         return a - b;
     }
+
     public static int multiply(int a, int b) {
         return a * b;
     }
+
     public static int divide(int a, int b) {
         return a / b;
     }
