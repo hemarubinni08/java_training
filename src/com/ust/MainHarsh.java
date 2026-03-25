6e17863bc7ca8b604923618095cf892b85f5e7c1
Harsh Pratap Singh <harshpratap.singh13579@gmail.com>
2026-03-25 12:38:38 +0530

    java code

---
+++ src/com/ust/MainHarsh.java
@@ -0,0 +1,11 @@
+package com.ust;
+
+public class MainHarsh {
+    public static void main(String args[]) {
+        System.out.println(addstring("harsh", "Singh"));
+    }
+
+    public static String addstring(String A, String B) {
+        return A + B;
+    }
+}
