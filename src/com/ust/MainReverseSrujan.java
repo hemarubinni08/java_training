e0c1360b3278b7f08523854685e95012424ab99a
srujan <123@gmail.com>
2026-03-25 15:13:03 +0530

    my reversing code in java

--- src/com/ust/MainReverseSrujan.java
+++ src/com/ust/MainReverseSrujan.java
@@ -2,9 +2,9 @@ package com.ust;
 
 public class MainReverseSrujan {
     public static void main(String[] args) {
-        String name = "srujan";
+        String original = "srujan";
         String reversed = " ";
-        for (int i = name.length() - 1; i >= 0; i--) {
+        for (int i = original.length() - 1; i >= 0; i--) {
             reversed = reversed + name.charAt(i);
         }
         System.out.println("The reversed name is : " + reversed);
