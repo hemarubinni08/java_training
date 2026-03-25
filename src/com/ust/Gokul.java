8163d8d882eb46ebf7833ce72a5fa5b767d8bc67
gokul <308346@ust.com>
2026-03-25 17:51:38 +0530

    Arithmetic operation

--- src/com/ust/Gokul.java
+++ src/com/ust/Gokul.java
@@ -2,10 +2,15 @@ package com.ust;
 
 public class Gokul {
     public static void main(String[] args) {
-        System.out.println(addTwoNumbers(5, 7));
-    }
+        Operation arith = new Operation();
+        System.out.println("Addition:" + arith.addition(2, 3));
+        System.out.println("Subtraction:" + arith.subtraction(5, 3));
+        System.out.println("Multiplication:" + arith.multiplication(3, 5));
+        System.out.println("Division:" + arith.division(10, 5));
 
-    public static Integer addTwoNumbers(int a, int b) {
-        return a + b;
     }
 }
+
+
+
+
