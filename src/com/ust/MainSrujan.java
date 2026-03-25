cde6abb80f8228a88e68b85d4aa9c98c849dfe6e
srujan <123@gmail.com>
2026-03-25 12:46:56 +0530

    my first java

---
+++ src/com/ust/MainSrujan.java
@@ -0,0 +1,11 @@
+package com.ust;
+
+public class MainSrujan {
+    public static void main(String[] args) {
+        System.out.println(multiplyNumber(2, 9));
+    }
+
+    public static Integer multiplyNumber(int a, int b) {
+        return a * b;
+    }
+}
