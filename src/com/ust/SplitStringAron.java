758a3576d3baa995fe350bfaef589ebfe117add5
Aron George Jain <arongeorgejain@gmail.com>
2026-03-25 16:50:40 +0530

    Updated the code to accept the string and character input

--- src/com/ust/SplitStringAron.java
+++ src/com/ust/SplitStringAron.java
@@ -1,19 +1,22 @@
 package com.ust;
-
-import java.lang.*;
+import java.util.Scanner;
 
 public class SplitStringAron {
     public static void main(String[] args) {
-        String name = "Raja";
-        printCharacters(name);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the name: ");
+        String name = sc.next();
+        System.out.println("Enter the character to print until: ");
+        char character=sc.next().charAt(0);
+        printCharacters(name, character);
     }
 
     // Function to print characters of a string until "A" is found
-    public static void printCharacters(String name) {
+    public static void printCharacters(String name, char character) {
         int i = 0;
         while (i < name.length()) {
             Character currentCharacter = name.charAt(i);
-            if (currentCharacter.equals('a') || currentCharacter.equals('A'))
+            if (currentCharacter.equals(character))
                 break;
             else
                 System.out.println(currentCharacter);
