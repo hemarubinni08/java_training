de4442f877e22cdd09d8b054acfdac3e5ba61f09
shashipreetham <spdevipi@gmail.com>
2026-03-25 17:40:38 +0530

    ArithmeticOperations and object usage

---
+++ src/com/ust/ArithmeticOperations.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+public class ArithmeticOperations {
+    public int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractTwoNumbers(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicationTwoNumbers(int a, int b) {
+        return a * b;
+    }
+}
--- src/com/ust/MainShashi.java
+++ src/com/ust/MainShashi.java
@@ -2,15 +2,19 @@ package com.ust;
 
 public class MainShashi {
     public static void main(String[] args) {
-        System.out.println(addTwoNumbers(4, 5));
-        System.out.println(subtractTwoNumber(15, 8));
+        // System.out.println(addTwoNumbers(4, 5));
+        //System.out.println(subtractTwoNumber(15, 8));
+        ArithmeticOperations arithmeticObject = new ArithmeticOperations();
+        System.out.println(arithmeticObject.addTwoNumbers(10, 20));
+        System.out.println(arithmeticObject.subtractTwoNumbers(40, 28));
+        System.out.println(arithmeticObject.multiplicationTwoNumbers(15, 3));
     }
 
-    public static Integer addTwoNumbers(int a, int b) {
+   /* public static Integer addTwoNumbers(int a, int b) {
         return a + b;
     }
 
     public static Integer subtractTwoNumber(int a, int b) {
         return a - b;
-    }
+    }*/
 }
