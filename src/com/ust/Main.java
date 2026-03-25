c0239581536546529788d263863c14efa9fa144a
Harshavardhan S <308227@ust.com>
2026-03-25 17:39:10 +0530

    Find the character, using while loop

--- src/com/ust/Main.java
+++ src/com/ust/Main.java
@@ -2,10 +2,9 @@ package com.ust;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(addTwoNumbers(2, 5));
-    }
-
-    public static Integer addTwoNumbers(int a, int b) {
-        return a + b;
+        ArithemeticOperationsHarsha obj = new ArithemeticOperationsHarsha();
+        System.out.println(obj.addNumbers(1, 2));
+        System.out.println(obj.substractNumbers(5, 3));
+        System.out.println(obj.MultiplyNumbers(3, 5));
     }
 }
