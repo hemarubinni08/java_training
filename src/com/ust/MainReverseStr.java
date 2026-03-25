9563f18230e5431a7d07e685c64f8eedf82e3375
ChrisJohns <Chris.Johnson@ust.com>
2026-03-25 15:29:11 +0530

    String reversal

--- src/com/ust/MainReverseStr.java
+++ src/com/ust/MainReverseStr.java
@@ -2,15 +2,15 @@ package com.ust;
 
 public class MainReverseStr {
     public static void main(String[] args){
-        String name="Chris_Johnson";
-        String reverse=StringReversal(name);
+        String name = "Chris_Johnson";
+        String reverse = StringReversal(name);
         System.out.println(reverse);
     }
     public static String StringReversal(String str){
-        String rev="";
-        for(int i=str.length()-1;i>=0;i--){
-            rev=rev+str.charAt(i);
+        String reversed = "";
+        for(int i = str.length() - 1; i >= 0; i--){
+            reversed = reversed + str.charAt(i);
         }
-        return rev;
+        return reversed;
     }
 }
