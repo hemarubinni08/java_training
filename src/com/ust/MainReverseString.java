1594b01595c4cbc92a97936d43f4e204a9bcd100
Bewin Mathews George <308230@ust.com>
2026-03-25 14:54:52 +0530

    Reverse a String - Bewin

---
+++ src/com/ust/MainReverseString.java
@@ -0,0 +1,20 @@
+package com.ust;
+
+public class MainReverseString {
+    public static void main(){
+        String name = "Bewin" ;
+        String reversed = reverseString(name) ;
+        System.out.println(reversed);
+    }
+
+    public static String reverseString(String str){
+        String reverse = " " ;
+
+        for(int i = str.length() - 1; i >= 0; i--){
+            reverse = reverse + str.charAt(i);
+        }
+
+        return reverse;
+
+    }
+}
