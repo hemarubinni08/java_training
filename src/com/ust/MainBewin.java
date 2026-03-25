6e7fd64bd5725d27215f710387abfab50ced32e7
Bewin Mathews George <308230@ust.com>
2026-03-25 14:20:24 +0530

    pushing pod2

--- src/com/ust/MainBewin.java
+++ src/com/ust/MainBewin.java
@@ -2,11 +2,11 @@ package com.ust;
 
 public class MainBewin {
     public static void main() {
-        int diff = diffTwoNumbers(5, 2);
+        int diff = differenceTwoNumbers(5, 2);
         System.out.println(diff);
     }
 
-    public static Integer diffTwoNumbers(int a, int b) {
+    public static Integer differenceTwoNumbers(int a, int b) {
         return a - b;
     }
 }
