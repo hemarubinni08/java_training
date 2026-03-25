92a705f54aedf5a1a9b9c2d47712e3de5820bc58
pranavp <pranavp@users.noreply.github.com>
2026-03-25 14:40:27 +0530

    Reverse a String

---
+++ src/com/ust/StringReversePranav.java
@@ -0,0 +1,19 @@
+package com.ust;
+
+
+public class StringReversePranav {
+    public static void main(String[] args) {
+        String result = strReverse("Hari");
+        System.out.println(result);
+    }
+
+    public static String strReverse(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        return reversed;
+
+    }
+}
