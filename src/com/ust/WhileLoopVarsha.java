564eef9bdb9ffbaacc6ed0edb11a8605d6be1b51
“varu” <Varsha.Thammi@ust.com>
2026-03-25 16:02:27 +0530

    update

---
+++ src/com/ust/WhileLoopVarsha.java
@@ -0,0 +1,13 @@
+package com.ust;
+
+public class WhileLoopVarsha {
+    public static void main(String[] args) {
+        String str = "varsha";
+        int i = 0;
+        while (i < str.length()) {
+
+            System.out.print(str.charAt(i) + " ");
+            i++;
+        }
+    }
+}
