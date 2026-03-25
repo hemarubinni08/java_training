00cbcc7962e4d9314ce5a424edb4977bbced5d5f
Logeshwaran <308343@ust.com>
2026-03-25 15:01:56 +0530

    reversed update

--- src/com/ust/MainReverse.java
+++ src/com/ust/MainReverse.java
@@ -2,9 +2,9 @@ package com.ust;
 
 public class MainReverse {
     public static void main(String[] args) {
-        String str="logesh";
-        StringBuilder rev=new StringBuilder();
-        for(int i=str.length()-1;i>=0;i--){
+        String str = "logesh";
+        StringBuilder rev = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
             rev.append(str.charAt(i));
         }
         System.out.print(rev);
