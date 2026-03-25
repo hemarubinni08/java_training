e42e5f4ad30ebac2900e2a2a85aa87141daa0b5e
Anudeepika <308347@ust.com>
2026-03-25 17:42:27 +0530

    Arithmetic

---
+++ src/com/ust/MainArithemetic.java
@@ -0,0 +1,19 @@
+package com.ust;
+
+public class MainArithemetic {
+    public static void main(String[] args) {
+
+    }
+
+    public static Integer addition(int x, int y) {
+        return x + y;
+    }
+
+    public static Integer subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public static Integer multiplication(int c, int d) {
+        return c * d;
+    }
+}
