2b676aa63b2080633679a5376b5daba863b0416b
“navya” <Navya.Samudrala@ust.com>
2026-03-25 15:09:28 +0530

    reverse a string

--- src/com/ust/Rev.java
+++ src/com/ust/Rev.java
@@ -1,9 +1,17 @@
 package com.ust;
 
-public class Rev {
-    public static void main(String args[]){
-        String str="navya";
-        String reversed=new StringBuilder(str).reverse().toString();
-        System.out.println(reversed);
+public class ReverseString {
+    public static void main(String[] args) {
+        String name = "navya";
+        System.out.println(reverseString(name));
+
+    }
+
+    public static String reverseString(String name) {
+        String rev = "";
+        for (int i = name.length() - 1; i >= 0; i--) {
+            rev = rev + name.charAt(i);
+        }
+        return rev;
     }
 }
