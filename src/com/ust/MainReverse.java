f00c57f636bc667a09adb75d9ae97054afd99d07
Logeshwaran <308343@ust.com>
2026-03-25 14:48:35 +0530

    reversed

---
+++ src/com/ust/MainReverse.java
@@ -0,0 +1,13 @@
+package com.ust;
+
+public class MainReverse {
+    public static void main(String[] args) {
+        String str="logesh";
+        StringBuilder rev=new StringBuilder();
+        for(int i=str.length()-1;i>=0;i--){
+            rev.append(str.charAt(i));
+        }
+        System.out.print(rev);
+
+    }
+}
