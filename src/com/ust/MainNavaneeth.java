7926abfed181fb4fdd46bbbfa7e2c303e95e45ec
Navaneeth <308320@ust.com>
2026-03-25 12:41:15 +0530

    Created Main class

---
+++ src/com/ust/MainNavaneeth.java
@@ -0,0 +1,11 @@
+package com.ust;
+
+public class MainNavaneeth {
+    public static void main(String[] args){
+        System.out.println(subNum(5,3));
+
+    }
+    public static Integer subNum(int a, int b){
+        return a - b;
+    }
+}
