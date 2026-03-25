3dd5968d1d8d72ce4482c75a79d7be4a7c6f7ee0
mamtha <308335@ust.com>
2026-03-25 16:05:51 +0530

    splitting

--- src/com/ust/MainMamthaSplitting.java
+++ src/com/ust/MainMamthaSplitting.java
@@ -2,7 +2,7 @@ package com.ust;
 
 public class MainMamthaSplitting {
     public static void main(String[] args) {
-        String str = "Mamatha";
+        String str = "Mamathah";
         int i = 0;
         while (i < str.length()) {
             char c = str.charAt(i);
