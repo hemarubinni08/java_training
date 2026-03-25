4ea6e995d96963fb48f8ae4310c4a50cd67f8c86
Keerthi <308344@ust.com>
2026-03-25 15:02:25 +0530

    reverse

--- src/com/ust/Reverse.java
+++ src/com/ust/Reverse.java
@@ -2,8 +2,18 @@ package com.ust;
 
 public class Reverse {
     public static void main(String[] args) {
-        String str = "keerthi";
-        String reversed = new StringBuilder(str).reverse().toString();
-        System.out.println(reversed);
+        String str= "keerthi";
+       // String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println(reverseString(str));
+    }
+
+    public static String reverseString(String name)
+    {
+        String rev="";
+        for(int i=name.length()-1;i>=0;i--)
+        {
+            rev=rev+ name.charAt(i);
+        }
+        return rev;
     }
 }
