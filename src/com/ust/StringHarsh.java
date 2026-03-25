461418753f3c5813addb36931198593d3395f26e
Harsh Pratap Singh <harshpratap.singh13579@gmail.com>
2026-03-25 15:02:11 +0530

    updated code

--- src/com/ust/StringHarsh.java
+++ src/com/ust/StringHarsh.java
@@ -2,13 +2,13 @@ package com.ust;
 
 public class StringHarsh {
     public static void main(String args[]) {
-        String s = "harsh Pratap Singh";
-        StringBuffer sb = new StringBuffer(s);
+        String name = "harsh Pratap Singh";
+        StringBuffer sb = new StringBuffer(name);
         System.out.println(sb.reverse().toString());
 
         String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            rev += s.charAt(i);
+        for (int i = name.length() - 1; i >= 0; i--) {
+            rev += name.charAt(i);
         }
         System.out.println(rev);
     }
