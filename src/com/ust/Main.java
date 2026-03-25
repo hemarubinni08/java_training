f85e85bec65d82de2e20ae3439dde1911983ec96
“varu” <Varsha.Thammi@ust.com>
2026-03-25 17:47:11 +0530

    update

--- src/com/ust/Main.java
+++ src/com/ust/Main.java
@@ -2,10 +2,12 @@ package com.ust;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(addTwoNumbers(2, 5));
-    }
+        ArithmeticOperatorsVarsha a = new ArithmeticOperatorsVarsha();
+        System.out.println(a.addition(2,3));
+        System.out.println(a.substraction(2,3));
+        System.out.println(a.multiplication(2,3));
+        System.out.println(a.division(2,3));
 
-    public static Integer addTwoNumbers(int a, int b) {
-        return a + b;
+
     }
 }
