623e16651aae4d6de2fe480604524fb557499c23
SkyRanjan <akashranjan.lko@gmail.com>
2026-03-25 17:39:48 +0530

    code for object passing

---
+++ src/com/ust/ArithmeticAkash.java
@@ -0,0 +1,20 @@
+package com.ust;
+
+//  code for add, subtract, multiply and division
+public class ArithmeticAkash {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return Math.abs(a - b);
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double division(double a, double b) {
+        return a / b;
+    }
+}
