803fddd1707f9be197876017d0adaa7ba6cae75b
Fijul Ali <308351@ust.com>
2026-03-25 15:11:46 +0530

    reversed string

--- src/com/ust/ReverseString.java
+++ src/com/ust/ReverseString.java
@@ -6,9 +6,9 @@ public class ReverseString {
     }
 
     public static String reverseString(String s) {
-        int l = s.length();
+        int length = s.length();
         String reversed = "";
-        for (int i = l - 1; i >= 0; i--) {
+        for (int i = length - 1; i >= 0; i--) {
             reversed += s.charAt(i);
         }
         return reversed;
