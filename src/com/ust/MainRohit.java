a1065df4797c65592a6ce7c8916afb91836cc6f0
Rohit Raj Saji <rohitraj.saji@ust.com>
2026-03-25 12:35:59 +0530

    Multiply

---
+++ src/com/ust/MainRohit.java
@@ -0,0 +1,11 @@
+package com.ust;
+
+public class MainRohit {
+    public static void main(String[] args) {
+        System.out.println(mulTwoDigits(5, 6));
+    }
+
+    public static Integer mulTwoDigits(int a, int b) {
+        return a * b;
+    }
+}
