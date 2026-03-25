7c137aed1346965f70cbe6a8abc02fcc1f623288
Anudeepika <308347@ust.com>
2026-03-25 14:58:04 +0530

    Reverse String

--- src/com/ust/MainAnudeepika.java
+++ src/com/ust/MainAnudeepika.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class MainAnudeepika {
     public static void main(String[] args) {
         String original = "Hello World";
-        String reversed2 = "";
+        String reversed = "";
         for (int i = original.length() - 1; i >= 0; i--) {
-            reversed2 += original.charAt(i);
+            reversed += original.charAt(i);
         }
-        System.out.println("Reversed using loop: " + reversed2);
+        System.out.println("Reversed using loop: " + reversed);
     }
 }
