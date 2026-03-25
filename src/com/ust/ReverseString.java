c2c8e262b2afcc9fb21a1ddd139ac60540d3be89
Gopinath <308340@ust.com>
2026-03-25 14:51:40 +0530

    reverse

---
+++ src/com/ust/ReverseString.java
@@ -0,0 +1,12 @@
+package com.ust;
+
+public class ReverseString {
+    public static void main(String[] args){
+        System.out.println(name("Gopinath"));
+    }
+    public static String name(String Name ){
+        for(int i=0;i>=Name.length();i--) {
+            return Name;
+
+        }
+}
