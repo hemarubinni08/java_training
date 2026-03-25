96982ace529689b3b3b8d555a7d7770377a9d4ba
“ardra” <ardra.ah@ust.com>
2026-03-25 17:52:02 +0530

    added arithmetic operations program

--- src/com/ust/ArithmeticOperationsArdra.java
+++ src/com/ust/ArithmeticOperationsArdra.java
@@ -1,16 +1,19 @@
 package com.ust;
 
 public class ArithmeticOperationsArdra {
-    int addition(int a,int b){
-        return a+b;
+    int addition(int a, int b) {
+        return a + b;
     }
-    int subtraction(int a, int b){
-        return a-b;
+
+    int subtraction(int a, int b) {
+        return a - b;
     }
-    int multiply(int a, int b){
-        return a*b;
+
+    int multiply(int a, int b) {
+        return a * b;
     }
-    float divide(int a, int b){
-        return (float)a/(float)b;
+
+    float divide(int a, int b) {
+        return (float) a / (float) b;
     }
 }
