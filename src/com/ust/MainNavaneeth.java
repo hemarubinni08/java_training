e12b2913ac2936cb075397792d3b7e8139fc77d4
Navaneeth <308320@ust.com>
2026-03-25 14:19:27 +0530

    Created Main class

--- src/com/ust/MainNavaneeth.java
+++ src/com/ust/MainNavaneeth.java
@@ -2,10 +2,10 @@ package com.ust;
 
 public class MainNavaneeth {
     public static void main(String[] args){
-        System.out.println(subNum(5,3));
+        System.out.println(subtractNumber(5,3));
 
     }
-    public static Integer subNum(int a, int b){
+    public static Integer subtractNumber(int a, int b){
         return a - b;
     }
 }
