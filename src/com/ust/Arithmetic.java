94f999c038f2815d10e7ca2d6b57aa42c146f8ec
aiswarya <308332@ust.com>
2026-03-25 18:00:31 +0530

    pushed

---
+++ src/com/ust/Arithmetic.java
@@ -0,0 +1,16 @@
+package com.ust;
+
+public class Arithmetic {
+
+    public Integer addition(int a, int b) {
+        return a + b;
+    }
+
+    public Integer subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public Integer multiplication(int a, int b) {
+        return a * b;
+    }
+}
--- src/com/ust/SplitName.java
+++ src/com/ust/SplitName.java
@@ -4,7 +4,7 @@ public class SplitName {
     public static void main(String[] args) {
         String str = "aishu";
         int i = 0;
-        while (str.charAt(i) != 's') {
+        while (str.charAt(i) != 's'){
             System.out.println(str.charAt(i));
             i++;
         }
