2134e005d7bec1e45dd575edd39bbd5dafc6b17c
kushal <kushal.s@ust.com>
2026-03-25 16:06:32 +0530

    Added WhileKushal.java File

--- src/com/ust/MainReverseSrujan.java
+++ src/com/ust/MainReverseSrujan.java
@@ -5,7 +5,7 @@ public class MainReverseSrujan {
         String original = "srujan";
         String reversed = " ";
         for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + name.charAt(i);
+            reversed = reversed + original.charAt(i);
         }
         System.out.println("The reversed name is : " + reversed);
     }
