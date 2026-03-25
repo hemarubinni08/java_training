67f4853524020e58ad4540a5c66fa8edb6e3a9eb
“varu” <Varsha.Thammi@ust.com>
2026-03-25 12:51:00 +0530

    update

---
+++ src/com/ust/MainVarsha.java
@@ -0,0 +1,12 @@
+package com.ust;
+
+public class MainVarsha {
+    public static void main(String[] args) {
+        System.out.println(addNumbers(2, 3));
+
+    }
+
+    public static Integer addNumbers(int a, int b) {
+        return a + b;
+    }
+}
