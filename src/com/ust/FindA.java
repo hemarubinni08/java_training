a5253fe5c75057c8a4bd862521f4cf6ecd7074fe
Yeshwnath <308331@ust.com>
2026-03-25 15:57:54 +0530

    Reversing my name

---
+++ src/com/ust/FindA.java
@@ -0,0 +1,17 @@
+package com.ust;
+
+public class FindA {
+    public static void findingA(String name) {
+        int i = 0;
+        while (i < name.length() && ((name.charAt(i)) != ('a'))) {
+            System.out.print(name.charAt(i));
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        String name = "Yeshwanth";
+        findingA(name);
+    }
+
+}
