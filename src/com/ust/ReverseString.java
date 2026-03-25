dc16b01b19a9a924733ea2033d8ee9ecfadcb44c
Hemanth <hemanth@gmail.com>
2026-03-25 14:48:10 +0530

    Reversing a String.

--- src/com/ust/ReverseString.java
+++ src/com/ust/ReverseString.java
@@ -1,19 +1,22 @@
 package com.ust;
+
 import java.util.*;
+
 public class ReverseString {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String s;
         System.out.println("Enter String");
-        s=sc.next();
+        s = sc.next();
         System.out.println(reverseString(s));
     }
-    public static String reverseString(String s){
+
+    public static String reverseString(String s) {
         char c;
-        String rev="";
-        for(int i=0;i<s.length();i++){
-            c=s.charAt(i);
-            rev=c+rev;
+        String rev = "";
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            rev = c + rev;
         }
         return rev;
     }
