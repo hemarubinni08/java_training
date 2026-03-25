5b4d8cf273aaf60498f37e5f6c748f1a7c894153
Chaila Shree <chailashree03@gmail.com>
2026-03-25 12:51:08 +0530

    file created

---
+++ src/com/ust/MainChailaShree.java
@@ -0,0 +1,19 @@
+package com.ust;
+/*
+public class MainChailaShree {
+    public static void main(String[] args){
+        System.out.println(name("Chailashree"));
+    }
+    public static String name(String a){
+        return a;
+    }
+}
+*/
+public class MainChailaShree{
+    public static void main(String[] args){
+        System.out.println(divideNum(50,2));
+    }
+    public static Integer divideNum(int a,int b){
+        return a/b;
+    }
+}
