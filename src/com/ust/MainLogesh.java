9e1acfef32d3ccf358a34507a11f30f06e809252
Logeshwaran <308343@ust.com>
2026-03-25 15:06:11 +0530

    main update

--- src/com/ust/MainLogesh.java
+++ src/com/ust/MainLogesh.java
@@ -3,11 +3,9 @@ package com.ust;
 public class MainLogesh {
     public static void main(String[] args) {
         System.out.println(multiplyTwo(5, 5));
-
     }
 
     public static Integer multiplyTwo(int a, int b) {
         return a * b;
     }
-
 }
