bcc68b4531fbfd40d206f1d67b6b524f0f8b6a5b
chandrashekhar <308239@ust.com>
2026-03-25 17:43:40 +0530

    multiple operations

---
+++ src/com/ust/ArithameticOperationsChandu.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+public class ArithameticOperationsChandu {
+    public int addition(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int substraction(int number1, int number2) {
+        return number1 - number2;
+    }
+
+    public int multiplication(int number1, int number2) {
+        return number1 * number2;
+    }
+}
--- src/com/ust/MainChandu.java
+++ src/com/ust/MainChandu.java
@@ -2,10 +2,14 @@ package com.ust;
 
 public class MainChandu {
     public static void main(String[] args) {
-        System.out.println(addTwoNumbers(2, 5));
+        ArithameticOperationsChandu obj = new ArithameticOperationsChandu();
+
+        int number1 = 5;
+        int number2 = 6;
+
+        System.out.println("Addition : " + obj.addition(number1, number2));
+        System.out.println("Substraction : " + obj.substraction(number1, number2));
+        System.out.println("Multiplication : " + obj.multiplication(number1, number2));
     }
 
-    public static int addTwoNumbers(int a, int b) {
-        return a + b;
-    }
 }
