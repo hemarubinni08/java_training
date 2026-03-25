69d6c77288bc1733e494c21c16568ac74ebbb46d
Harshavardhan S <308227@ust.com>
2026-03-25 14:20:05 +0530

    updated and fixed the method names

--- src/com/ust/MainHarshavardhan.java
+++ src/com/ust/MainHarshavardhan.java
@@ -2,11 +2,11 @@ package com.ust;
 
 public class MainHarshavardhan {
     public static void main(String[] args) {
-        System.out.println(subNumber(3, 2));
+        System.out.println(substractNumber(3, 2));
         System.out.println(concatString("Hi ", "This is Harsha"));
     }
 
-    public static int subNumber(int a, int b) {
+    public static int substractNumber(int a, int b) {
         return a - b;
     }
 
