38e90ed2d5c57a8832183ef531e4cf4b1a0691f9
Aron George Jain <arongeorgejain@gmail.com>
2026-03-25 12:48:33 +0530

    Created MainAron.java class

---
+++ src/com/ust/MainAron.java
@@ -0,0 +1,16 @@
+package com.ust;
+
+public class MainAron {
+    public static void main(String[] args) {
+        System.out.println(addNumbers(2, 5));
+        System.out.println(multiplyNumbers(2, 5));
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiplyNumbers(int a, int b){
+        return a * b;
+    }
+}
