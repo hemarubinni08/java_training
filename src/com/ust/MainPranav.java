41a6a48457a5facf9e0dd7b0c9da9f4f955cbbe7
pranavp <pranavp@users.noreply.github.com>
2026-03-25 12:40:21 +0530

    First main project

---
+++ src/com/ust/MainPranav.java
@@ -0,0 +1,11 @@
+package com.ust;
+
+public class MainPranav {
+    public static void main(String[] args) {
+        System.out.print(isCorrect(1));
+    }
+
+    public static boolean isCorrect(int num) {
+        return num == 1;
+    }
+}
