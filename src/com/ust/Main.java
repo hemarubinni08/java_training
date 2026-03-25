e680d82471f5fb64aacc857491cf749dd5afa2d8
pranavp <pranavp@users.noreply.github.com>
2026-03-25 17:53:02 +0530

    Arithemetic Operations Program commited twice

--- src/com/ust/Main.java
+++ src/com/ust/Main.java
@@ -1,13 +1,5 @@
 package com.ust;
 
 public class Main {
-    public static void main(String[] args) {
-        ArithemeticProgramPranav ar = new ArithemeticProgramPranav();
-        int sum = ar.addNumbers(5, 5);
-        int difference = ar.subtractNumbers(5, 5);
-        int product = ar.multiplyNumbers(5, 5);
-        System.out.println("Sum " + sum);
-        System.out.println("Difference " + difference);
-        System.out.println("Product " + product);
-    }
+
 }
