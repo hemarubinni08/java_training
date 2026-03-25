d67ab69a9b7952acb3bc3e35f6b5f42853afce78
kushal <kushal.s@ust.com>
2026-03-25 17:39:26 +0530

    Added ArithmeticOperationKushal.java File

---
+++ src/com/ust/ArithmeticOperationKushal.java
@@ -0,0 +1,26 @@
+package com.ust;
+
+public class ArithmeticOperationKushal {
+    float result;
+
+    public float add(float n1, float n2) {
+        result = n1 + n2;
+        return result;
+    }
+
+    public float subtract(float n1, float n2) {
+        result = n1 - n2;
+        return result;
+    }
+
+    public float multiply(float n1, float n2) {
+        result = n1 * n2;
+        return result;
+    }
+
+    public float division(float n1, float n2) {
+        result = n1 / n2;
+        return result;
+    }
+
+}
--- src/com/ust/MainKushal.java
+++ src/com/ust/MainKushal.java
@@ -1,11 +1,15 @@
 package com.ust;
 
 public class MainKushal {
-    public static void main() {
-        System.out.println(addNum(3, 6));
+    static void main() {
+        ArithmeticOperationKushal aok = new ArithmeticOperationKushal();
+        System.out.println("Addition of two numbers is : " + " " + aok.add(6, 2));
+        System.out.println("Subtraction of two numbers is : " + " " + aok.subtract(6, 2));
+        System.out.println("Multiplication of two numbers is : " + " " + aok.multiply(6, 2));
+        System.out.println("Division of two numbers is : " + " " + aok.division(6, 2));
     }
-
+    /*
     public static int addNum(int n1, int n2) {
         return n1 + n2;
-    }
+    }*/
 }
