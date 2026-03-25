a37b8a988c66525c17113819af2661efaa0d316d
Ritika <ritika_kumari@ust.com>
2026-03-25 14:40:30 +0530

    Reversed Name

--- src/com/ust/name.java
+++ src/com/ust/name.java
@@ -3,7 +3,7 @@ package com.ust;
 public class name {
     public static void main(String[] args) {
         String name = "Ritika Kumari";
-        System.out.println("Reversed name:" + " " + reverseName(name));
+        System.out.println("Reversed Name:" + " " + reverseName(name));
 
     }
 
