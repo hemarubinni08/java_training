a69828c59fdea072c2daee14bed79d86d51da0ab
“varu” <Varsha.Thammi@ust.com>
2026-03-25 16:09:43 +0530

    update

--- src/com/ust/WhileLoopVarsha.java
+++ src/com/ust/WhileLoopVarsha.java
@@ -2,11 +2,14 @@ package com.ust;
 
 public class WhileLoopVarsha {
     public static void main(String[] args) {
-        String str = "varsha";
+        String name = "varsha";
         int i = 0;
-        while (i < str.length()) {
+        while (i<name.length()) {
+            if(name.charAt(i)=='s'){
+                break;
+            }
 
-            System.out.print(str.charAt(i) + " ");
+            System.out.print(name.charAt(i));
             i++;
         }
     }
