a9e6b06c46c220be8ebe5fa8e4ab23d56eac8091
“ardra” <ardra.ah@ust.com>
2026-03-25 17:49:27 +0530

    added arithmetic operations program

---
+++ src/com/ust/ArithmeticArdra.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+public class ArithmeticArdra {
+    public static void main(String[] args){
+        ArithmeticOperationsArdra obj= new ArithmeticOperationsArdra();
+        System.out.println("enter your first number ");
+        int firstNumber = 10;
+        int secondNumber = 3;
+        System.out.printf("addition: %d \n",obj.addition(firstNumber,secondNumber));
+        System.out.printf("subtraction: %d \n",obj.subtraction(firstNumber,secondNumber));
+        System.out.printf("multiply: %d \n",obj.multiply(firstNumber,secondNumber));
+        System.out.printf("divide: %f \n",obj.divide(firstNumber,secondNumber));
+    }
+}
+
