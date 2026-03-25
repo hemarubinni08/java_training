9b94ff85202ce0607ffee8a07f15a10e295ece08
Shoaib Ejaz <308232@ust.com>
2026-03-25 17:35:18 +0530

    created string prog using while loop

--- src/com/ust/Shoaibloop.java
+++ src/com/ust/Shoaibloop.java
@@ -3,19 +3,22 @@ package com.ust;
 import java.util.Scanner;
 
 public class Shoaibloop {
-    public static void main(String[] args) {
+    public static void loop() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the name");
         String name = in.nextLine();
         int i = 0;
-        while (i < name.length())
-        {
-            if (name.charAt(i) == 'a' || name.charAt(i) == 'A')
-            {
+        while (i < name.length()) {
+            if (name.charAt(i) == 'a' || name.charAt(i) == 'A') {
                 break;
             }
             System.out.print(name.charAt(i));
             i++;
         }
     }
-}
+        public static void main(String[]args)
+        {
+                loop();
+
+        }
+    }
