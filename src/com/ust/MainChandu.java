a1a89fb63a2213362c701e85d5e22ade6b5fc66e
chandrashekhar <308239@ust.com>
2026-03-25 12:40:36 +0530

    created new main class

---
+++ src/com/ust/MainChandu.java
@@ -0,0 +1,11 @@
+package com.ust;
+
+public class MainChandu {
+    public static void main(String[] args) {
+        System.out.println(addTwoNumbers(2, 5));
+    }
+
+    public static int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+}
