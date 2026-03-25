230c37d3430f35d0a8e0e4819a2111f0bb7f695f
Hemanth <hemanth@gmail.com>
2026-03-25 12:44:17 +0530

    MainHemant sample file

--- src/com/ust/MainHemanth.java
+++ src/com/ust/MainHemanth.java
@@ -1,10 +1,11 @@
 package com.ust;
 
 public class MainHemanth {
-    public static void main(String[] args){
-        System.out.println(addTwoNumbers(2,5));
+    public static void main(String[] args) {
+        System.out.println(addTwoNumbers(2, 5));
     }
-    public static int addTwoNumbers(int a, int b){
-        return a+b;
+
+    public static int addTwoNumbers(int a, int b) {
+        return a + b;
     }
 }
