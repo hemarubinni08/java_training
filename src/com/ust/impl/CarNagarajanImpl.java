c14c37e361a7add56dc715a764a82d542547c054
naga <308242@ust.com>
2026-03-26 17:20:26 +0530

    Interface Exercise !!

---
+++ src/com/ust/impl/CarNagarajanImpl.java
@@ -0,0 +1,29 @@
+package com.ust.impl;
+
+import com.ust.VehicleInterfaceNagarajan;
+
+public class CarNagarajanImpl implements VehicleInterfaceNagarajan {
+    public String brand;
+    public int speed;
+
+    @Override
+    public String start() {
+
+        return brand + " is  Starting";
+    }
+
+    @Override
+    public String stop() {
+        return brand + " is stopped";
+    }
+
+    @Override
+    public String displayInfo() {
+
+        return "Brand : " + brand + ", Speed : " + speed;
+    }
+
+    public String openCarDoor() {
+        return "Car door is opened";
+    }
+}
