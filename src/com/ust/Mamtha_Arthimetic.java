cf37818d825c84f245122352e18f3d4f5f541450
mamtha <308335@ust.com>
2026-03-26 14:34:30 +0530

    arthimetic

--- src/com/ust/Mamtha_Arthimetic.java
+++ src/com/ust/Mamtha_Arthimetic.java
@@ -17,11 +17,9 @@ class Arithmetic {
     public static int subtraction(int num1, int num2) {
         return num1 - num2;
     }
-
     public static int multiplication(int num1, int num2) {
         return num1 * num2;
     }
-
     public static int division(int num1, int num2) {
         return num1 / num2;
     }
