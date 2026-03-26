2ca010f21bd8b0a179084454b4a11b26ffe80c59
mamtha <308335@ust.com>
2026-03-26 14:33:55 +0530

    Vehicle Classes Added

--- src/com/ust/Mamtha_Arthimetic.java
+++ src/com/ust/Mamtha_Arthimetic.java
@@ -14,7 +14,6 @@ class Arithmetic {
     public static int addition(int num1, int num2) {
         return num1 + num2;
     }
-
     public static int subtraction(int num1, int num2) {
         return num1 - num2;
     }
