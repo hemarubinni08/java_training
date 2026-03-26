fa09c473565612dcb64289baaf8f2c7707356478
Bewin Mathews George <308230@ust.com>
2026-03-26 14:40:45 +0530

    Inheritance Code Bewin

---
+++ src/com/ust/CarBew.java
@@ -0,0 +1,12 @@
+package com.ust;
+
+public class CarBew extends HeavyVehicleBew {
+
+    public String colour() {
+        return "Blue";
+    }
+
+    public Boolean isPublicTransport() {
+        return false;
+    }
+}
