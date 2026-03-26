5e41af582b7e0dd2c7f9c9b4aebe76c894e255b6
Gopi1701 <146619497+Gopi1701@users.noreply.github.com>
2026-03-26 14:28:52 +0530

    Update TruckGopi.java

--- src/com/ust/TruckGopi.java
+++ src/com/ust/TruckGopi.java
@@ -4,11 +4,9 @@ public class TruckGopi {
     void price() {
         System.out.println(10000);
     }
-
     void name() {
         System.out.println("honda");
     }
-
     void mileage() {
         System.out.println(67);
     }
