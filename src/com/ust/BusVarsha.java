6bf907e10093eed4290669c09ec079ffcdbb085e
“varu” <Varsha.Thammi@ust.com>
2026-03-26 12:37:46 +0530

    update

---
+++ src/com/ust/BusVarsha.java
@@ -0,0 +1,39 @@
+package com.ust;
+
+public class BusVarsha extends HeavyVehicleVarsha {
+    @Override
+    public int numberOfWheels() {
+        return 6;
+    }
+
+    @Override
+    public int numberOfLights() {
+        return 4;
+    }
+
+    @Override
+    public int capacityOfFuel() {
+        return 1000;
+    }
+
+    @Override
+    public int numberOfDoors() {
+        return 2;
+    }
+
+    @Override
+    public boolean publicTransportation() {
+        return true;
+    }
+
+    @Override
+    public boolean hasAc() {
+        return true;
+    }
+
+    @Override
+    public boolean sunRoof() {
+        return false;
+    }
+}
+
--- src/com/ust/MainNavaneeth.java
+++ src/com/ust/MainNavaneeth.java
@@ -1,10 +1,11 @@
 package com.ust;
 
 public class MainNavaneeth {
-    ArithemiticOperationNavaneeth ao = new ArithemiticOperationNavaneeth();
-    int sum = ao.addition(2, 3);
-    int subraction = ao.subraction(5, 3);
-    int multiply = ao.multiply(8, 5);
+    public static void main(String[] args) {
+        ArithemiticOperationNavaneeth ao = new ArithemiticOperationNavaneeth();
+        int sum = ao.addition(2, 3);
+        int subraction = ao.subraction(5, 3);
+        int multiply = ao.multiply(8, 5);
 
         System.out.println("Addition:" + sum);
         System.out.println("Subraction:" + subraction);
