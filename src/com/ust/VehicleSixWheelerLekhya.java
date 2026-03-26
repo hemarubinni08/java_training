f9e59edfdd51f5460cd2a9824eda7c82d7edb7d5
Lekhya <308337@ust.com>
2026-03-26 14:40:17 +0530

    Inheritance concept

---
+++ src/com/ust/VehicleSixWheelerLekhya.java
@@ -0,0 +1,17 @@
+package com.ust;
+
+import java.time.LocalDate;
+
+public class VehicleSixWheelerLekhya extends HeavyVehicleLekhya {
+    public static LocalDate getRegistrationDate() {
+        return LocalDate.now();
+    }
+
+    public static String getPurposeOfUsage() {
+        return "Exporting Goods";
+    }
+
+    public static String getRegistrationName() {
+        return "Kelvin Das";
+    }
+}
---
+++ src/com/ust/VehicleTwoWheelerLekhya.java
@@ -0,0 +1,16 @@
+package com.ust;
+
+public class VehicleTwoWheelerLekhya extends VehicleLekhya {
+
+    public static String getBrandOfTheVehicle() {
+        return "Honda";
+    }
+
+    public static String getColourOfTheVehicle() {
+        return "Black";
+    }
+
+    public static Integer getWarranty() {
+        return 5;
+    }
+}
