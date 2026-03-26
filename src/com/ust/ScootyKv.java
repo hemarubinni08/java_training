801a8fbc78da4ebead81df2fc0cf6082209ee935
Keerthi <308344@ust.com>
2026-03-26 12:52:17 +0530

    Vehicle

---
+++ src/com/ust/ScootyKv.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+public class ScootyKv extends Vehicle{
+    public int noOfWheels(){
+        return 2;
+    }
+
+    public int maxSpeed(){
+        return 80;
+    }
+
+    public String getBrand(){
+        return "Honda";
+    }
+}
