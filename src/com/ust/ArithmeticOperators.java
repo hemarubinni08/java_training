0b385805b29e0d0af6f49174f89efb2e102012f2
deepshika <308341@ust.com>
2026-03-26 09:50:43 +0530

    Arithmetic Operations Program

--- src/com/ust/ArithmeticOperators.java
+++ src/com/ust/ArithmeticOperators.java
@@ -1,25 +1,29 @@
 package com.ust;
 
 public class ArithmeticOperators {
-    public static void main (String[] args){
-        Arithmetic oper=new Arithmetic();
-        System.out.println("Addition :"+oper.add(2,5));
-        System.out.println("Subtraction :"+oper.sub(2,5));
-        System.out.println("Multiplication :"+oper.mul(2,5));
-        System.out.println(" Division:"+oper.div(2,5));
+    public static void main(String[] args) {
+        Arithmetic oper = new Arithmetic();
+        System.out.println("Addition :" + oper.add(2, 5));
+        System.out.println("Subtraction :" + oper.sub(2, 5));
+        System.out.println("Multiplication :" + oper.mul(2, 5));
+        System.out.println(" Division:" + oper.div(5,10));
     }
 }
-class Arithmetic{
-    public static int add(int a,int b){
-        return  a+b;
+
+class Arithmetic {
+    public static int add(int a, int b) {
+        return a + b;
     }
-    public static int sub(int a,int b){
-        return  a-b;
+
+    public static int sub(int a, int b) {
+        return a - b;
     }
-    public static int mul(int a,int b){
-        return  a*b;
+
+    public static int mul(int a, int b) {
+        return a * b;
     }
-    public static int div(int a,int b){
-        return  a/b;
+
+    public static int div(int a, int b) {
+        return a / b;
     }
 }
