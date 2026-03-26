af49eabbb0e440429d45b134c568cf1cc2a358f2
Harsh Pratap Singh <harshpratap.singh13579@gmail.com>
2026-03-26 12:37:04 +0530

    creating object odf differnt vehicle and calling it

---
+++ src/com/ust/VechileHarsh.java
@@ -0,0 +1,16 @@
+package com.ust;
+
+public class VechileHarsh {
+    public int wheeles() {
+        return 4;
+    }
+
+    public String color() {
+        return "white";
+    }
+
+    public String brand() {
+        return "Kingfisher";
+    }
+}
+
