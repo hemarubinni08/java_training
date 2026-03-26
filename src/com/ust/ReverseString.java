b85483a70c06035cd5517cd3e7d2c544cb021b70
mamtha4 <308335@ust.com>
2026-03-26 09:54:55 +0530

    Revert "Pod2 java"

--- src/com/ust/ReverseString.java
+++ src/com/ust/ReverseString.java
@@ -2,17 +2,11 @@ package com.ust;
 
 public class ReverseString {
     public static void main(String[] args) {
-        String name = "aishu";
-        String reversed = reversingWord(name);
-        System.out.println(reversed);
-    }
-
-    public static String reversingWord(String str) {
-        String reverse = " ";
+        String str = "Deepshika";
+        String reversed = "";
         for (int i = str.length() - 1; i >= 0; i--) {
-            reverse = reverse + str.charAt(i);
+            reversed = reversed + str.charAt(i);
         }
-        return reverse;
+        System.out.println("Reversed string: " + reversed);
     }
 }
-
