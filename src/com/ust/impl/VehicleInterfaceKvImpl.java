0528c14ab2bfd646422d794c116f4fddc01b6c61
Keerthi <308344@ust.com>
2026-03-26 17:03:42 +0530

    InterfaceKv

---
+++ src/com/ust/impl/VehicleInterfaceKvImpl.java
@@ -0,0 +1,23 @@
+package com.ust.impl;
+
+import com.ust.Vehicle;
+import com.ust.VehicleInterfaceKv;
+
+public class VehicleInterfaceKvImpl implements VehicleInterfaceKv {
+
+
+    @Override
+    public int noOfWheels() {
+        return 0;
+    }
+
+    @Override
+    public int maxSpeed() {
+        return 0;
+    }
+
+    @Override
+    public String getBrand() {
+        return "";
+    }
+}
