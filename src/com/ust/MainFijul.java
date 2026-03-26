44a368bddd28c3ea10a05e1d67a8ec0c71581da0
Fijul Ali <308351@ust.com>
2026-03-26 09:51:25 +0530

    bank balance updating

--- src/com/ust/MainFijul.java
+++ src/com/ust/MainFijul.java
@@ -2,6 +2,7 @@ package com.ust;
 
 public class MainFijul {
     public static void main(String[] args) {
+        // learning training
         BankDetails object = new BankDetails();
         System.out.println(object.addBalance(2000, 3000));
         System.out.println(object.minusBalance(4000, 2000));
