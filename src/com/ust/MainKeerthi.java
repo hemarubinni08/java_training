0b771ca69a36a67dad878c89442f933de1a781d1
Keerthi <308344@ust.com>
2026-03-26 17:03:42 +0530

    InterfaceKv

--- src/com/ust/MainKeerthi.java
+++ src/com/ust/MainKeerthi.java
@@ -1,15 +1,50 @@
 package com.ust;
 
+import com.ust.impl.BikeInterfaceKvImpl;
+import com.ust.impl.ScootyInterfaceKvImpl;
+import com.ust.impl.TruckInterfaceKvImpl;
+
 public class MainKeerthi {
     public static void main(String[] args) {
-        Vehicle scooty = new ScootyKv();
-        Vehicle bike = new BikeKv();
-        CarKv car=new CarKv();
-        Vehicle truck = new TruckKv();
-        System.out.println("no of wheels:" + scooty.noOfWheels());
+        //Vehicle scooty = new ScootyKv();
+        //Vehicle bike = new BikeKv();
+        //CarKv car=new CarKv();
+        //Vehicle truck = new TruckKv();
+        //System.out.println("no of wheels:" + scooty.noOfWheels());
+        //System.out.println("Max speed:" + car.maxSpeed());
+        //System.out.println("Brand name:" + bike.getBrand());
+        //System.out.println("Car has AC:" + car.hasAc());
+        //System.out.println("No of passengers:" + car.noOfPassengers());
+        //CarInterfaceKvImpl cr=new CarInterfaceKvImpl();
+        //BikeInterfaceKvImpl bk=new BikeInterfaceKvImpl();
+        //TruckInterfaceKvImpl tk=new TruckInterfaceKvImpl();
+        //ScootyInterfaceKvImpl so=new ScootyInterfaceKvImpl();
+        //System.out.println("no of wheels:" + cr.noOfWheels());
+        //System.out.println("Max speed:" + cr.maxSpeed());
+        //System.out.println("Brand name:" + cr.getBrand());
+        //System.out.println("no of wheels:" + bk.noOfWheels());
+        //System.out.println("Max speed:" + bk.maxSpeed());
+        //System.out.println("Brand name:" + bk.getBrand());
+        //System.out.println("no of wheels:" + tk.noOfWheels());
+        //System.out.println("Max speed:" + tk.maxSpeed());
+        //System.out.println("Brand name:" + tk.getBrand());
+        //System.out.println("no of wheels:" + so.noOfWheels());
+        //System.out.println("Max speed:" + so.maxSpeed());
+        //System.out.println("Brand name:" + so.getBrand());
+        //System.out.println("Car has AC:" + cr.hasAc());
+        //System.out.println("No of passengers:" + cr.noOfPassengers());
+        //System.out.println("Truck has AC:" + tk.hasAc());
+        //System.out.println("No of passengers:" + tk.noOfPassengers());
+        CarAbstractKvImpl car=new CarAbstractKvImpl();
+        BikeAbstractKvImpl bike=new BikeAbstractKvImpl();
+        System.out.println("no of wheels:" + car.noOfWheels());
         System.out.println("Max speed:" + car.maxSpeed());
-        System.out.println("Brand name:" + bike.getBrand());
+        System.out.println("Brand name:" + car.getBrand());
         System.out.println("Car has AC:" + car.hasAc());
-        System.out.println("No of passengers:" + car.noOfPassengers());
+        System.out.println("no of wheels:" + bike.noOfWheels());
+        System.out.println("Max speed:" + bike.maxSpeed());
+        System.out.println("Brand name:" + bike.getBrand());
+
+
     }
 }
