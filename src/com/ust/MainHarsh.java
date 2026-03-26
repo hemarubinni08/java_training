fd74b6fc3923171506dee12950837052d680e728
HarshPratapSingh-UST <308352@ust.com>
2026-03-26 09:50:13 +0530

    Revert "Feature/harsh"

--- src/com/ust/MainHarsh.java
+++ src/com/ust/MainHarsh.java
@@ -3,11 +3,6 @@ package com.ust;
 public class MainHarsh {
     public static void main(String args[]) {
         System.out.println(addString("harsh", "Singh"));
-        ArithmeticOperation ob = new ArithmeticOperation();
-        System.out.println(ob.addition(3, 5));
-        System.out.println(ob.substract(10, 5));
-        System.out.println(ob.multiply(3, 5));
-        System.out.println(ob.divide(3, 5));
     }
 
     public static String addString(String A, String B) {
