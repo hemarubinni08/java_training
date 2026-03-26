49d1f6150d520010ee4b735f56b0342e49cbbd55
hema <308250@ust.com>
2026-03-26 17:13:01 +0530

    Interface Concept

---
+++ src/com/ust/BusHema.java
@@ -0,0 +1,18 @@
+package com.ust;
+
+public class BusHema {
+    public int getWheelsCount() {
+
+        return 6;
+    }
+
+    public String getBrand() {
+
+        return "Tata";
+    }
+
+    public int getDoorsCount() {
+
+        return 3;
+    }
+}
---
+++ src/com/ust/vehicleInterfaceHema.java
@@ -0,0 +1,11 @@
+package com.ust;
+
+public interface vehicleInterfaceHema {
+    String getBrand();
+
+    int getWheelsCount();
+
+    String getFuelType();
+
+    int doorsCount();
+}
