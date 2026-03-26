e061d8cacc9207cb74ae9afdc150be43d60b15de
Hemanth <hemanth@gmail.com>
2026-03-26 17:39:23 +0530

    MultiLevel Inheritance

--- src/com/ust/Main.java
+++ src/com/ust/Main.java
@@ -5,11 +5,7 @@ import com.ust.impl.TwoWheelersHemanthImpl;
 
 public class Main {
     public static void main(String[] args) {
-        FourWheelersHemanthImpl four = new FourWheelersHemanthImpl();
-        System.out.println(four.typeOfVehicle());
-        System.out.println(four.hasAc());
-        VehicleInterfaceHemanth bike = new TwoWheelersHemanthImpl();
-        bike.typeOfVehicle();
-        bike.mileage();
+        VehicleBikeHemanth bike = new VehicleBikeHemanth();
+        bike.noOfTyres();
     }
 }
