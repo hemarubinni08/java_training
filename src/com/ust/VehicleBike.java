32065e90a8778c06010fe1d15f9827580aeb7ef6
deepshika <308341@ust.com>
2026-03-26 14:42:40 +0530

    Vehicle classes added

---
+++ src/com/ust/VehicleBike.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+public class VehicleBike extends VehicleDeepshika {
+    public String color() {
+        return "Blue";
+    }
+
+    public int noOfWheels() {
+        return 2;
+    }
+
+    public String typeOfFuel() {
+        return "petrol";
+    }
+}
