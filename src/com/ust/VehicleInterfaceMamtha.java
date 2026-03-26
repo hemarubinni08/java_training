ea93a9fc70368ec0dcb5e6b9924049e4e1351ad9
mamtha <308335@ust.com>
2026-03-26 17:13:22 +0530

    Vehicle Interface

---
+++ src/com/ust/VehicleInterfaceMamtha.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+public interface VehicleInterfaceMamtha {
+
+    int numberOfWheels();
+    String getColor();
+    String getFuelType();
+    boolean isPublicTransport();
+    String getBrand();
+    int numberOfSeats();
+
+
+
+
+}
