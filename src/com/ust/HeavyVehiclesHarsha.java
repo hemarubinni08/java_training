d7f9dcb50302c991e88ba2c00199c719a48b8857
Harshavardhan S <308227@ust.com>
2026-03-26 12:15:50 +0530

    Java inheritance code

---
+++ src/com/ust/HeavyVehiclesHarsha.java
@@ -0,0 +1,14 @@
+package com.ust;
+
+public class HeavyVehiclesHarsha extends VehicleHarsha{
+
+    public boolean hasAc(){
+        return true;
+    }
+    public boolean isPublicTransport(){
+        return false;
+    }
+    public boolean hasSunRoof(){
+        return false;
+    }
+}
