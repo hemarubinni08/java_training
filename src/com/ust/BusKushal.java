64b33b22b92d76e511f9cd9317faca3428f65fee
kushal <kushal.s@ust.com>
2026-03-26 12:44:49 +0530

    InheritencePractice Codes

---
+++ src/com/ust/BusKushal.java
@@ -0,0 +1,16 @@
+package com.ust;
+
+public class BusKushal extends HeavyVehicleKushal {
+    public int getWheelsCount() {
+        return 6;
+    }
+
+    public String getBrand() {
+        return "Tata";
+    }
+
+    public int getDoorsCount() {
+        return 3;
+    }
+}
+
---
+++ src/com/ust/VehicleKushal.java
@@ -0,0 +1,23 @@
+package com.ust;
+
+public class VehicleKushal {
+    public int getWheelsCount() {
+        return 4;
+    }
+
+    public String getColour() {
+        return "white";
+    }
+
+    public int getDoorsCount() {
+        return 2;
+    }
+
+    public String getBrand() {
+        return "Volvo";
+    }
+
+    public String getVehicleType() {
+        return "SUV";
+    }
+}
