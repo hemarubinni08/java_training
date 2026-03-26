ba75db14523457e6869ec4b3d7f38f1839b7c214
Rohit Raj Saji <rohitraj.saji@ust.com>
2026-03-26 09:47:07 +0530

    Revert "Feature/rohit"

--- src/com/ust/MainRohit.java
+++ src/com/ust/MainRohit.java
@@ -2,11 +2,10 @@ package com.ust;
 
 public class MainRohit {
     public static void main(String[] args) {
-        Arithmetic ob = new Arithmetic();
-        System.out.println(ob.add(5, 6));
-        System.out.println(ob.subtract(5, 6));
-        System.out.println(ob.multiply(5, 6));
-        System.out.println(ob.divide(5, 6));
+        System.out.println(multiplyTwoDigits(5, 6));
+    }
 
+    public static Integer multiplyTwoDigits(int a, int b) {
+        return a * b;
     }
 }
