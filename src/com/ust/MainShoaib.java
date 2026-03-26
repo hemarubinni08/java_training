0f83e25efd8f28221eda8a3ca34e79452bf22714
Shoaib Ejaz <308232@ust.com>
2026-03-26 14:49:46 +0530

    Revert "Committing Inheritance program"
    This reverts commit 6543c5d0dd9938f89de68f47bdaa2947b9a2112e.

--- src/com/ust/MainShoaib.java
+++ src/com/ust/MainShoaib.java
@@ -7,15 +7,5 @@ public class MainShoaib {
         System.out.println(obj.addition(1,3));
         System.out.println(obj.subtract(3,8));
         System.out.println(obj.multiply(8,8));
-
-
-        ShoaibVehicle bike = new ShoaibBikes();
-        ShoaibBus bus = new ShoaibBus();
-        ShoaibVehicle scooty = new ShoaibScooty();
-        ShoaibCar car = new ShoaibCar();
-
-        System.out.println(scooty.vehicleBrand());
-
-
     }
 }
