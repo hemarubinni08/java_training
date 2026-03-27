26499e0da00d77fb0b1e81e4d96badb16cfb9b18
Navaneeth <308320@ust.com>
2026-03-27 11:36:07 +0530

    Revert "Arethimetic overloading"
    This reverts commit 837026670783f45d1adbcc1ab682caaaddf59ad9.

--- src/com/ust/ArithemiticOperationNavaneeth.java
+++ src/com/ust/ArithemiticOperationNavaneeth.java
@@ -13,8 +13,4 @@ public class ArithemiticOperationNavaneeth {
     public int multiply(int a, int b) {
         return a * b;
     }
-
-    public int addition(int Num1, int Num2, int Num3) {
-        return Num1 + Num2 + Num3;
-    }
 }
--- src/com/ust/MainNavaneeth.java
+++ src/com/ust/MainNavaneeth.java
@@ -4,15 +4,12 @@ import com.ust.impl.*;
 
 public class MainNavaneeth {
     public static void main(String[] args) {
-        ArithemiticOperationNavaneeth ac = new ArithemiticOperationNavaneeth();
-        System.out.println(ac.addition(2,3,6));
-        System.out.println(ac.addition(3,5));
-        /*BikeAbsractNavaneeth bn = new BikeAbsractNavaneeth();
+        BikeAbsractNavaneeth bn = new BikeAbsractNavaneeth();
         CarAbstractNavaneeth cn = new CarAbstractNavaneeth();
         BusAbstractNavaneeth bbn = new BusAbstractNavaneeth();
 
         System.out.println("Bike : " + bn.numberOfWheels() + " Wheels, " + bn.brand() + ", " + bn.engineCapacity() + " CC, " + "Gears: " + bn.hasGears()); // + " AC : " + bn.hasAc() + ", " + " Public Tansport : " + bn.hasPublicTransport() + ", " + "DOORS :" + bn.doors());
         System.out.println("Car : " + cn.numberOfWheels() + " Wheels, " + cn.brand() + ", " + cn.engineCapacity() + " CC, " + "Gears: " + cn.hasGears()); //+ " AC : " + cn.hasAc() + ", " + " Public Tansport : " + cn.hasPublicTransport() + ", " + "DOORS :" + cn.doors());
-        System.out.println("Bus : " + bbn.numberOfWheels() + " Wheels, " + bbn.brand() + ", " + bbn.engineCapacity() + " CC, " + "Gears: " + bbn.hasGears()); //+ " AC : " + bbn.hasAc() + ", " + " Public Tansport : " + bbn.hasPublicTransport() + ", " + "DOORS :" + bbn.doors());*/
+        System.out.println("Bus : " + bbn.numberOfWheels() + " Wheels, " + bbn.brand() + ", " + bbn.engineCapacity() + " CC, " + "Gears: " + bbn.hasGears()); //+ " AC : " + bbn.hasAc() + ", " + " Public Tansport : " + bbn.hasPublicTransport() + ", " + "DOORS :" + bbn.doors());
     }
 }
