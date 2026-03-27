91f8ed2260413318d5024170dedaf3a6f35dd285
ChrisJohns <Chris.Johnson@ust.com>
2026-03-27 18:05:50 +0530

    Lambda Expressions.....

---
+++ src/com/ust/AdditionChris.java
@@ -0,0 +1,17 @@
+package com.ust;
+
+
+public class AdditionChris {
+
+    public int addition(int num1,int num2){
+        int sum=0;
+        sum = num1 + num2;
+        return sum;
+    }
+
+    public int addition(int numb1,int numb2, int numb3){
+        int sum=0;
+        sum = numb1 + numb2 + numb3;
+        return sum;
+    }
+}
