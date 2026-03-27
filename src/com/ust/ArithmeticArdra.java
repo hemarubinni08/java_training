a042e8f3676691b00dd4817870d7c9a1be5e6ed8
“ardra” <ardra.ah@ust.com>
2026-03-27 11:28:10 +0530

    added method overloading to ArithmeticOperationsArdra.java

--- src/com/ust/ArithmeticArdra.java
+++ src/com/ust/ArithmeticArdra.java
@@ -6,10 +6,8 @@ public class ArithmeticArdra {
         System.out.println("enter your first number ");
         int firstNumber = 10;
         int secondNumber = 3;
-        System.out.printf("addition: %d \n", obj.addition(firstNumber, secondNumber));
-        System.out.printf("subtraction: %d \n", obj.subtraction(firstNumber, secondNumber));
-        System.out.printf("multiply: %d \n", obj.multiply(firstNumber, secondNumber));
-        System.out.printf("divide: %f \n", obj.divide(firstNumber, secondNumber));
+        System.out.println(obj.addition(2, 3, 4));
+        System.out.println(obj.addition(2, 3));
     }
 }
 
