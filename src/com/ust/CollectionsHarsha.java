a1d27e22dec643849b7e52777fb83f734ef0447b
Harshavardhan S <308227@ust.com>
2026-03-27 23:40:49 +0530

    Collections part 1

---
+++ src/com/ust/CollectionsHarsha.java
@@ -0,0 +1,137 @@
+package com.ust;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CollectionsHarsha {
+    public void doIteration(List<String> names) {
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+
+    public void doIterationByStream(List<String> names) {
+        names.forEach(System.out::println);
+    }
+
+    public void igonreNeighbour(List<String> names) {
+        names.stream().filter(name -> !name.equalsIgnoreCase("Pranav")).forEach(System.out::println);
+    }
+
+    public void ignoreNeighbourForLoop(List<String> names) {
+        for (String name : names) {
+            if (!name.equalsIgnoreCase("Pranav")) {
+                System.out.println(name);
+            }
+        }
+    }
+
+    public List<String> removeDuplicate2(List<String> nameList1, List<String> nameList2) {
+        List<String> result = new ArrayList<>();
+        int count = 0;
+        for (String name : nameList1) {
+            for (String name2 : nameList2) {
+                if (name.equalsIgnoreCase(name2)) {
+                    System.out.println(name2 + " " + name);
+                    count = count + 1;
+                    break;
+
+                } else if (!name.equalsIgnoreCase(name2)) {
+                    System.out.println(name2 + " " + name);
+                    count = 0;
+
+                }
+            }
+            System.out.println(count);
+            if (count == 0) {
+                result.add(name);
+                System.out.println("added");
+            } else {
+                System.out.println("Not added");
+            }
+        }
+        System.out.println("-------------");
+        System.out.println(nameList1);
+        System.out.println(nameList2);
+
+        for (String name2 : nameList2) {
+            for (String name : nameList1) {
+                if (name2.equalsIgnoreCase(name)) {
+                    System.out.println(name2 + " " + name);
+                    count = count + 1;
+                    break;
+
+                } else if (!name2.equalsIgnoreCase(name)) {
+                    System.out.println(name2 + " " + name);
+                    count = 0;
+
+                }
+            }
+            System.out.println(count);
+            if (count == 0) {
+                result.add(name2);
+                System.out.println("added");
+            } else {
+                System.out.println("Not added");
+            }
+        }
+        Collections.sort(result);
+        return result;
+    }
+
+    public List<String> removeDuplicate3(List<String> nameList1, List<String> nameList2){
+        List<String> result = new ArrayList<>();
+
+        for (String name : nameList1){
+            if (!nameList2.contains(name)){
+                result.add(name);
+            }
+        }
+
+        for (String name : nameList2){
+            if (!nameList1.contains(name)){
+                result.add(name);
+            }
+        }
+
+        return result;
+    }
+
+    public List<String> removeDuplicate4(List<String> nameList1, List<String> nameList2){
+
+        List<String> newList = new ArrayList<>();
+
+        newList.addAll(nameList1.stream().filter(name->!nameList2.contains(name)).toList());
+
+        newList.addAll(nameList2.stream().filter(name->!nameList1.contains(name)).toList());
+
+        return newList;
+
+    }
+
+    public List<String> removeDuplicate5(List<String> nameList1, List<String> nameList2){
+
+        List<String> combineList = Stream.concat(nameList1.stream().filter(name->!nameList2.contains(name)),nameList2.stream().filter(name->!nameList1.contains(name))).toList();
+
+        return combineList;
+
+    }
+
+    public List<String> removeDuplicate6(List<String> nameList1, List<String> nameList2){
+        List<String> result = new ArrayList<>();
+        result.addAll(nameList1);
+        result.addAll(nameList2);
+
+        String bufferName = "";
+        for (String name : result){
+            if(name.equalsIgnoreCase(bufferName)){
+                
+            }
+            
+          bufferName = name;
+        }
+        return result;
+    }
+}
--- src/com/ust/MainHarshavardhan.java
+++ src/com/ust/MainHarshavardhan.java
@@ -2,41 +2,37 @@ package com.ust;
 
 import com.ust.impl.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainHarshavardhan {
     public static void main(String[] args) {
 
-        BikeAbstractHarshaImpl bikeObj = new BikeAbstractHarshaImpl();
-        System.out.println();
-        System.out.println("Number of wheels in bike: " + bikeObj.numberOfWheels());
-        System.out.println("Engine capacity in bike: " + bikeObj.engineCapacity());
-        System.out.println("fuel type of bike: " + bikeObj.fuelType());
-        System.out.println("Can do wheelie: "+ bikeObj.canDoWheelie());
+        List<String> names = new ArrayList<>();
+        names.add("Harsha");
+        names.add("Chandra");
+        names.add("Pranav");
+        names.add("Navneeth");
+        names.add("Varsha");
+        names.add("Deepa");
 
-        HeavyVehiclesAbstractHarsha carObj = new CarAbstractHarshaImpl();
-        System.out.println();
-        System.out.println("Number of wheels in car: " + carObj.numberOfWheels());
-        System.out.println("Engine capacity in car: " + carObj.engineCapacity());
-        System.out.println("fuel type of car: " + carObj.fuelType());
-        System.out.println("car has AC: " + carObj.hasAc());
-        System.out.println("Is public transport: " + carObj.isPublicTransport());
-        System.out.println("Has sunroof: " + carObj.hasSunRoof());
+        List<String> names2 = new ArrayList<>();
+        names2.add("Anjan");
+        names2.add("Kushal");
+        names2.add("Divya");
+        names2.add("Harsha");
+        names2.add("Rohan");
+        names2.add("Moksha");
+        names2.add("Harsha");
+        names2.add("Deeksha");
 
-        HeavyVehiclesAbstractHarsha busObj = new BusAbstractHarshaImpl();
-        System.out.println();
-        System.out.println("Number of wheels in bus: " + busObj.numberOfWheels());
-        System.out.println("Engine capacity in bus: " + busObj.engineCapacity());
-        System.out.println("fuel type of bus: " + busObj.fuelType());
-        System.out.println("bus has AC: " + busObj.hasAc());
-        System.out.println("Is public transport: " + busObj.isPublicTransport());
-        System.out.println("Has sunroof: " + busObj.hasSunRoof());
 
-        HeavyVehiclesAbstractHarsha lorryObj = new LorryAbstractHarshaImpl();
-        System.out.println();
-        System.out.println("Number of wheels in Lorry: " + lorryObj.numberOfWheels());
-        System.out.println("Engine capacity in Lorry: " + lorryObj.engineCapacity());
-        System.out.println("fuel type of Lorry: " + lorryObj.fuelType());
-        System.out.println("lorry has AC: " + lorryObj.hasAc());
-        System.out.println("Is public transport: " + lorryObj.isPublicTransport());
-        System.out.println("Has sunroof: " + lorryObj.hasSunRoof());
+        CollectionsHarsha obj = new CollectionsHarsha();
+//        obj.doIteration(names);
+//        obj.doIterationByStream(names);
+//          obj.igonreNeighbour(names);
+//          System.out.println();
+//          obj.ignoreNeighbourForLoop(names);
+        System.out.println( obj.removeDuplicate2(names,names2));
     }
 }
