becf450c447c22c7f99587d34269f77cbdcbb12b
Logeshwaran <308343@ust.com>
2026-03-27 11:30:42 +0530

    overloading

--- src/com/ust/MainLogesh.java
+++ src/com/ust/MainLogesh.java
@@ -4,45 +4,50 @@ import com.ust.impl.*;
 
 public class MainLogesh {
     public static void main(String[] args) {
-        VehicleInterfaceLogeshImpl vehicle = new VehicleInterfaceLogeshImpl();
-        System.out.println(vehicle.getNumberOfWheels());
-        System.out.println(vehicle.getModel());
-        System.out.println(vehicle.getPrice());
-        System.out.println();
-        System.out.print("CAR");
-        CarAbstractLogesh car = new CarAbstractLogesh();
-        System.out.println(car.getNumberOfWheels());
-        System.out.println(car.getPrice());
-        System.out.println(car.getLoadBed());
-        System.out.println(car.getNumberOfSeat());
-        System.out.println(car.getNumberOfSeat());
-        System.out.println();
-        System.out.println("BIKE");
-        BikeAbstractLogesh bike = new BikeAbstractLogesh();
-        System.out.println(bike.getPrice());
-        System.out.println(bike.getSpeed());
-        System.out.println(bike.getModel());
-        System.out.println(bike.getNumberOfWheels());
-        System.out.println();
-        System.out.println("LORRY");
-        LorryAbstractLogesh lorry = new LorryAbstractLogesh();
-        System.out.println(lorry.getModel());
-        System.out.println(lorry.getPrice());
-        System.out.println(lorry.getNumberOfWheels());
-        System.out.println(lorry.getGuranteeYears());
-        System.out.println(lorry.getFuel());
-        System.out.println(lorry.getLoadBed());
-        System.out.println(lorry.getAc());
-        System.out.println(lorry.getNumberOfSeat());
+        ArithmaticOperation1 a1 = new ArithmaticOperation1();
+        System.out.println(a1.addition(5, 4, 6));
 
-
-        System.out.println();
-        System.out.println("CYCLE");
-        CycleAbstractLogesh cycle = new CycleAbstractLogesh();
-        System.out.println(cycle.getFuel());
-        System.out.println(cycle.getNumberOfWheels());
-        System.out.println(cycle.getModel());
-        System.out.println(cycle.getPrice());
-        System.out.println();
     }
 }
+//        VehicleInterfaceLogeshImpl vehicle = new VehicleInterfaceLogeshImpl();
+//        System.out.println(vehicle.getNumberOfWheels());
+//        System.out.println(vehicle.getModel());
+//        System.out.println(vehicle.getPrice());
+//        System.out.println();
+//        System.out.print("CAR");
+//        CarAbstractLogesh car = new CarAbstractLogesh();
+//        System.out.println(car.getNumberOfWheels());
+//        System.out.println(car.getPrice());
+//        System.out.println(car.getLoadBed());
+//        System.out.println(car.getNumberOfSeat());
+//        System.out.println(car.getNumberOfSeat());
+//        System.out.println();
+//        System.out.println("BIKE");
+//        BikeAbstractLogesh bike = new BikeAbstractLogesh();
+//        System.out.println(bike.getPrice());
+//        System.out.println(bike.getSpeed());
+//        System.out.println(bike.getModel());
+//        System.out.println(bike.getNumberOfWheels());
+//        System.out.println();
+//        System.out.println("LORRY");
+//        LorryAbstractLogesh lorry = new LorryAbstractLogesh();
+//        System.out.println(lorry.getModel());
+//        System.out.println(lorry.getPrice());
+//        System.out.println(lorry.getNumberOfWheels());
+//        System.out.println(lorry.getGuranteeYears());
+//        System.out.println(lorry.getFuel());
+//        System.out.println(lorry.getLoadBed());
+//        System.out.println(lorry.getAc());
+//        System.out.println(lorry.getNumberOfSeat());
+//
+//
+//        System.out.println();
+//        System.out.println("CYCLE");
+//        CycleAbstractLogesh cycle = new CycleAbstractLogesh();
+//        System.out.println(cycle.getFuel());
+//        System.out.println(cycle.getNumberOfWheels());
+//        System.out.println(cycle.getModel());
+//        System.out.println(cycle.getPrice());
+//        System.out.println();
+//    }
+//}
