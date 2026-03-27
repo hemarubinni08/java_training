30db8f1351665d9027a00c31b476a611677d9e32
Aron George Jain <arongeorgejain@gmail.com>
2026-03-27 11:28:28 +0530

    Perform method overloading with arithmetic addition

--- src/com/ust/MainAron.java
+++ src/com/ust/MainAron.java
@@ -1,40 +1,45 @@
 package com.ust;
 
-import com.ust.impl.*;
+import com.ust.impl.CarInterfaceAronImpl;
+import com.ust.impl.BikeInterfaceAronImpl;
+import com.ust.impl.LorryInterfaceAronImpl;
+import com.ust.impl.AutoInterfaceAronImpl;
 
 public class MainAron {
     public static void main(String[] args) {
-        CarAbstractAronImpl car = new CarAbstractAronImpl();
-        System.out.println("Car");
-        System.out.println("Brand Name: " + car.getBrandName());
-        System.out.println("Car makes the sound " + car.getSound());
-        System.out.println("No of wheels in a car " + car.getNoOfWheels());
-        System.out.println("No of doors in a car " + car.getNoOfDoors());
-        System.out.println("Door type: " + car.getDoorType());
-        System.out.println("No of warranty in years: " + car.getWarrantyInYears());
+//        CarInterfaceAronImpl car = new CarInterfaceAronImpl();
+//        System.out.println("Car");
+//        System.out.println("Brand Name: " + car.getBrandName());
+//        System.out.println("Car makes the sound " + car.getSound());
+//        System.out.println("No of wheels in a car " + car.getNoOfWheels());
+//        System.out.println("No of doors in a car " + car.getNoOfDoors());
+//        System.out.println("No of warranty in years: " + car.getWarrantyInYears());
+//
+//        System.out.println("\nBike");
+//        BikeInterfaceAronImpl bike = new BikeInterfaceAronImpl();
+//        System.out.println("Brand Name: " + bike.getBrandName());
+//        System.out.println("Bike makes the sound " + bike.getSound());
+//        System.out.println("No of wheels in a Bike " + bike.getNoOfWheels());
+//        System.out.println("No of warranty in years: " + bike.getWarrantyInYears());
+//
+//        System.out.println("\nAuto");
+//        AutoInterfaceAronImpl auto = new AutoInterfaceAronImpl();
+//        System.out.println("Brand Name: " + auto.getBrandName());
+//        System.out.println("Auto makes the sound " + auto.getSound());
+//        System.out.println("No of wheels in a Bike " + auto.getNoOfWheels());
+//        System.out.println("No of doors in an auto " + auto.getNoOfDoors());
+//        System.out.println("No of warranty in years: " + auto.getWarrantyInYears());
+//
+//        System.out.println("\nLorry");
+//        LorryInterfaceAronImpl lorry = new LorryInterfaceAronImpl();
+//        System.out.println("Brand Name: " + lorry.getBrandName());
+//        System.out.println("Lorry makes the sound " + lorry.getSound());
+//        System.out.println("No of wheels in a Bike " + lorry.getNoOfWheels());
+//        System.out.println("No of doors in a Lorry " + lorry.getNoOfDoors());
+//        System.out.println("No of warranty in years: " + lorry.getWarrantyInYears());
 
-        System.out.println("\nBike");
-        BikeAbstractAronImpl bike = new BikeAbstractAronImpl();
-        System.out.println("Brand Name: " + bike.getBrandName());
-        System.out.println("Bike makes the sound " + bike.getSound());
-        System.out.println("No of wheels in a Bike " + bike.getNoOfWheels());
-        System.out.println("No of warranty in years: " + bike.getWarrantyInYears());
-        System.out.println("This bike is a " + bike.getBikeType());
-
-        System.out.println("\nAuto");
-        AutoAbstractAronImpl auto = new AutoAbstractAronImpl();
-        System.out.println("Brand Name: " + auto.getBrandName());
-        System.out.println("Auto makes the sound " + auto.getSound());
-        System.out.println("No of wheels in an auto " + auto.getNoOfWheels());
-        System.out.println("No of doors in an auto " + auto.getNoOfDoors());
-        System.out.println("No of warranty in years: " + auto.getWarrantyInYears());
-
-        System.out.println("\nLorry");
-        LorryAbstractClassImpl lorry = new LorryAbstractClassImpl();
-        System.out.println("Brand Name: " + lorry.getBrandName());
-        System.out.println("Lorry makes the sound " + lorry.getSound());
-        System.out.println("No of wheels in a Lorry " + lorry.getNoOfWheels());
-        System.out.println("No of doors in a Lorry " + lorry.getNoOfDoors());
-        System.out.println("No of warranty in years: " + lorry.getWarrantyInYears());
+          ArithmeticOperationsAron arithemticObj=new ArithmeticOperationsAron();
+          System.out.println(arithemticObj.addition(1,2));
+          System.out.println(arithemticObj.addition(3,4,5));
     }
 }
