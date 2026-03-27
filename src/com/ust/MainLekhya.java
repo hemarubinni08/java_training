a7565fc3e6e7bb65706a0ea5cdca98e5652532db
Lekhya <308337@ust.com>
2026-03-27 11:26:39 +0530

    method overloading

--- src/com/ust/MainLekhya.java
+++ src/com/ust/MainLekhya.java
@@ -7,10 +7,14 @@ import com.ust.impl.CarLekhyaImpl;
 
 public class MainLekhya {
     public static void main(String[] args) {
-        CarAbstractLekhyaImpl car = new CarAbstractLekhyaImpl();
+        /*CarAbstractLekhyaImpl car = new CarAbstractLekhyaImpl();
         BikeAbstracLekhyatImpl bike = new BikeAbstracLekhyatImpl();
         System.out.println(car.getRpm());
         System.out.println(bike.fuelUsed());
         System.out.println(car.hasAc());
+        */
+        ArithmeticOperations obj1 = new ArithmeticOperations();
+        System.out.println(obj1.addNumbers(2, 3));
+        System.out.println(obj1.addNumbers(2, 3, 4));
     }
 }
