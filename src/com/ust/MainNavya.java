e4051aaca2d3e6d1b82e1dac442b3fedba3f8f75
“navya” <Navya.Samudrala@ust.com>
2026-03-27 14:18:41 +0530

    collection java

---
+++ src/com/ust/MainNavya.java
@@ -0,0 +1,18 @@
+package com.ust;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainNavya {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("Deepak");
+        list.add("Keerthi");
+        list.add("Navya");
+        list.add("Gokul");
+        list.add("Harsh");
+
+        CollectionNavya.iterationCollection(list);
+    }
+}
