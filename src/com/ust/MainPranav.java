f94cb6eb2c3f5a0b07a647bdf03893e6a4401638
pranavp <pranavp@users.noreply.github.com>
2026-03-27 11:26:26 +0530

    Method overloading done

--- src/com/ust/MainPranav.java
+++ src/com/ust/MainPranav.java
@@ -4,17 +4,19 @@ import com.ust.impl.*;
 
 public class MainPranav {
     public static void main(String[] args) {
-        Bike1abstractPranav bp = new Bike1abstractPranav();
-        Car1AbstractPranav cp = new Car1AbstractPranav();
-        Truck1PranavImpl tp = new Truck1PranavImpl();
-        Bus1AbstractPranav bsp = new Bus1AbstractPranav();
-        System.out.println(
-                "Bike = Bike Engine : " + bp.engineCapacity() + " | Bike Wheels : " + bp.numberOfWheels() + " | Bike Fuel : " + bp.fuelType());
-        System.out.println("CAr = Car Engine : " +
-                cp.engineCapacity() + " | Car wheels : " + cp.numberOfWheels() + " | Car Fuel : " + cp.fuelType());
-        System.out.println("Truck = Truck Engine " +
-                tp.engineCapacity() + " | Truck Wheels : " + tp.numberOfWheels() + " | Truck Fuel : " + tp.fuelType() + " | Has Sunroof : " + tp.hasSunRoof() + " | is Heavy : " + tp.isHeavy());
-        System.out.println("Bus = " + bsp.engineCapacity() + " | Bus Wheels : " + bsp.numberOfWheels() + " | Bus Fuel : " + bsp.fuelType() + " | Has Sunroof : " + tp.hasSunRoof() + " | is Heavy : " + tp.isHeavy());
-
+//        Bike1abstractPranav bp = new Bike1abstractPranav();
+//        Car1AbstractPranav cp = new Car1AbstractPranav();
+//        Truck1PranavImpl tp = new Truck1PranavImpl();
+//        Bus1AbstractPranav bsp = new Bus1AbstractPranav();
+//        System.out.println(
+//                "Bike = Bike Engine : " + bp.engineCapacity() + " | Bike Wheels : " + bp.numberOfWheels() + " | Bike Fuel : " + bp.fuelType());
+//        System.out.println("CAr = Car Engine : " +
+//                cp.engineCapacity() + " | Car wheels : " + cp.numberOfWheels() + " | Car Fuel : " + cp.fuelType());
+//        System.out.println("Truck = Truck Engine " +
+//                tp.engineCapacity() + " | Truck Wheels : " + tp.numberOfWheels() + " | Truck Fuel : " + tp.fuelType() + " | Has Sunroof : " + tp.hasSunRoof() + " | is Heavy : " + tp.isHeavy());
+//        System.out.println("Bus = " + bsp.engineCapacity() + " | Bus Wheels : " + bsp.numberOfWheels() + " | Bus Fuel : " + bsp.fuelType() + " | Has Sunroof : " + tp.hasSunRoof() + " | is Heavy : " + tp.isHeavy());
+    ArithemeticProgramPranav ap = new ArithemeticProgramPranav();
+    System.out.println(ap.multiplyNumbers(5,5));
+    System.out.println(ap.multiplyNumbers(5,5,5));
     }
 }
