d0ca79b9e7d0c9276859c96bb99346ad4d66bcca
chandrashekhar <308239@ust.com>
2026-03-27 11:27:44 +0530

    adding same method

--- src/com/ust/MainChandu.java
+++ src/com/ust/MainChandu.java
@@ -5,7 +5,7 @@ import com.ust.impl.*;
 public class MainChandu {
     public static void main(String[] args) {
 
-        BikeAbstractChandu bc = new BikeAbstractChandu();
+      /*  BikeAbstractChandu bc = new BikeAbstractChandu();
         CarAbstractChandu cc = new CarAbstractChandu();
         TruckAbstractChandu tc = new TruckAbstractChandu();
         BusAbstractChandu bbc = new BusAbstractChandu();
@@ -15,6 +15,11 @@ public class MainChandu {
         System.out.println("Truck: " + tc.fuel() + ", " + tc.engine() + "cc, " + tc.wheels() + " wheels, " + tc.passenger() + " Passengers, truck has AC: " + tc.hasAc() + ", truck has sunroof: " + tc.hasSunroof() + ", truck has driver cabin: " + tc.hasDriverCabin());
         System.out.println("Car: " + cc.fuel() + ", " + cc.engine() + "cc, " + cc.wheels() + " wheels, " + cc.passenger() + " Passengers");
         System.out.println("Bus: " + bbc.fuel() + ", " + bbc.engine() + "cc, " + bbc.wheels() + " wheels, " + bbc.passenger() + " Passengers, bus has AC: " + bbc.hasAc() + ", bus has sunroof: " + bbc.hasSunroof() + ", bus has driver cabin: " + bbc.hasDriverCabin());
+      */
+        ArithameticOperationsChandu ac = new ArithameticOperationsChandu();
+        System.out.println(ac.addition(2, 4));
+        System.out.println(ac.addition(2, 4, 6));
+
     }
 
 }
