d5ed117626a9e357a50727365882ab6e1d9354e6
aiswarya <308332@ust.com>
2026-03-27 18:08:41 +0530

    Arithmetic prob

--- src/com/ust/ArithmeticOperators.java
+++ src/com/ust/ArithmeticOperators.java
@@ -2,28 +2,22 @@ package com.ust;
 
 public class ArithmeticOperators {
     public static void main(String[] args) {
-        Arithmetic oper = new Arithmetic();
-        System.out.println("Addition :" + oper.add(2, 5));
-        System.out.println("Subtraction :" + oper.sub(2, 5));
-        System.out.println("Multiplication :" + oper.mul(2, 5));
-        System.out.println(" Division:" + oper.div(5,10));
+
+        System.out.println(add(5, 6, 7));
+        System.out.println(add(3, 5));
+
     }
+
+
+    public static int add(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
 }
 
-class Arithmetic {
-    public static int add(int a, int b) {
-        return a + b;
-    }
 
-    public static int sub(int a, int b) {
-        return a - b;
-    }
 
-    public static int mul(int a, int b) {
-        return a * b;
-    }
-
-    public static int div(int a, int b) {
-        return a / b;
-    }
-}
--- src/com/ust/MainAishu.java
+++ src/com/ust/MainAishu.java
@@ -1,11 +1,33 @@
 package com.ust;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainAishu {
-    public static void main(String[] args){
-        int n=multiplyNum(2,7);
-        System.out.println(n);
-    }
-    public static Integer multiplyNum(int a,int b){
-        return a*b;
+    public static void main(String[] args) {
+//        int n=multiplyNum(2,7);
+//        System.out.println(n);
+//    }
+//    public static Integer multiplyNum(int a,int b){
+//        return a*b;
+//    }
+
+//
+        CollectionAish collectionAish = new CollectionAish();
+        List<String> names = new ArrayList<>();
+        names.add("aishu");
+        names.add("bewin");
+        names.add("chris");
+//        names.add("nagarajan");
+//        names.add("mamtha");
+//        names.add("deepshika");
+        //collectionAish.removeDuplicate(names);
+
+        List<String> name1 = new ArrayList<>();
+        name1.add("bewin");
+        name1.add("mamtha");
+        name1.add("deepshika");
+        collectionAish.removeDuplicatesUsingStream(names, name1);
+
     }
 }
