2383d1d6158b09b83adf12fd8af054f8304bbc35
Logeshwaran <308343@ust.com>
2026-03-27 11:59:33 +0530

    overloading ok

--- src/com/ust/ArithmaticOperation1.java
+++ src/com/ust/ArithmaticOperation1.java
@@ -8,4 +8,7 @@ public class ArithmaticOperation1 {
     public static Integer addition(int num1, int num2,int num3) {
         return num1 + num2 + num3;
     }
+    public static Integer addition(int num1, int num2,int num3,int num4) {
+        return num1 + num2 + num3 + num4;
+    }
 }
