7d08ffc061bfb83040ca5cbbb84dfcefcf76dcb6
Fijul Ali <308351@ust.com>
2026-03-27 11:26:08 +0530

    created and modified vehicle and its related classes

--- src/com/ust/MainFijul.java
+++ src/com/ust/MainFijul.java
@@ -1,26 +1,17 @@
 package com.ust;
 
-import com.ust.impl.BikeInterfaceFijulImpl;
-import com.ust.impl.BusInterfaceFijulImpl;
-import com.ust.impl.CarInterfaceFijulImpl;
-import com.ust.impl.TruckInterfaceFijulImpl;
+import com.ust.impl.*;
 
 import static com.ust.ReverseString.reverseString;
 import static com.ust.WhileFijul.printCharacters;
 
 public class MainFijul {
     public static void main(String[] args) {
+        System.out.println(reverseString("fijul"));
+        printCharacters("hfgdyais");
         BankDetails object = new BankDetails();
-        System.out.println(object.addBalance(2000, 3000));
+        System.out.println(object.addBalance(2000, 3000, 4000));
         System.out.println(object.minusBalance(4000, 2000));
-        CarInterfaceFijulImpl carimpl = new CarInterfaceFijulImpl();
-        TruckInterfaceFijulImpl truckimpl = new TruckInterfaceFijulImpl();
-        BusInterfaceFijulImpl busimpl = new BusInterfaceFijulImpl();
-        BikeInterfaceFijulImpl bikeimpl = new BikeInterfaceFijulImpl();
-        System.out.println(carimpl.getTopSpeed());
-        System.out.println(truckimpl.isServiceDue());
-        System.out.println(busimpl.brandName());
-        System.out.println(bikeimpl.totalKilometer());
         CarFijul car = new CarFijul();
         VehicleFijul bike = new BikeFijul();
         VehicleFijul vehicle = new VehicleFijul();
@@ -29,7 +20,19 @@ public class MainFijul {
         System.out.println(car.isServiceDue());
         System.out.println(bike.numberOfWheels());
         System.out.println(vehicle.brandName());
-        System.out.println(reverseString("fijul"));
-        printCharacters("hfgdyais");
+        System.out.println(bus.brandName());
+        System.out.println(truck.isServiceDue());
+        CarInterfaceFijulImpl carimpl = new CarInterfaceFijulImpl();
+        TruckInterfaceFijulImpl truckimpl = new TruckInterfaceFijulImpl();
+        BusInterfaceFijulImpl busimpl = new BusInterfaceFijulImpl();
+        BikeInterfaceFijulImpl bikeimpl = new BikeInterfaceFijulImpl();
+        System.out.println(carimpl.getTopSpeed());
+        System.out.println(truckimpl.isServiceDue());
+        System.out.println(busimpl.brandName());
+        System.out.println(bikeimpl.totalKilometer());
+        VehicleAbstractFijul carabstract = new CarAbstractFijulImpl();
+        VehicleAbstractFijul bikeabtract = new BikeAbstractFijulimpl();
+        System.out.println(bikeabtract.brandName());
+        System.out.println(carabstract.getTopSpeed());
     }
 }
