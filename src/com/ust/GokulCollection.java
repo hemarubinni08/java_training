e7f984059118be2e6a73c543f5092355b15855d0
gokul <308346@ust.com>
2026-03-28 09:22:23 +0530

    collection merging two list

--- src/com/ust/GokulCollection.java
+++ src/com/ust/GokulCollection.java
@@ -4,9 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GokulCollection {
-    public static void displayNames(List<String> names) {
-        for(String name:names){
+    public static void displayNames(List<String> names1, List<String> names2 ) {
+
+        List<String> result = new ArrayList<>();
+
+        for (String name : names1) {
+            if (!names2.contains(name)) {
+                result.add(name);
+            }
+        }
+
+        for (String name : names2) {
+            if (!names1.contains(name)) {
+                result.add(name);
+            }
+        }
+
+        for (String name : result) {
             System.out.println(name);
         }
+
+        names1.stream().filter(name -> !names2.contains(name)).forEach(name->result.add(name));
+
+        names2.stream().filter(name -> !names1.contains(name)).forEach(name->result.add(name));
+
+        result.forEach(name->System.out.println(name));
+
+
+
+
+
+        //   names.stream().filter(name -> !name.equalsIgnoreCase("deepak")).forEach(name ->System.out.println(name));
     }
+
 }
+
--- src/com/ust/MainGokul.java
+++ src/com/ust/MainGokul.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 
 public class MainGokul {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<>();
-        names.add("gokul");
-        names.add("harsh");
-        names.add("rohit");
-        names.add("navya");
-        names.add("keerthi");
-        GokulCollection.displayNames(names);
+        ArrayList<String> names1 = new ArrayList<>();
+        names1.add("gokul");
+        names1.add("harsh");
+        names1.add("rohit");
+        names1.add("navya");
+        names1.add("keerthi");
+        names1.add("deepak");
+        ArrayList<String>names2=new ArrayList<>();
+        names2.add("raina");
+        names2.add("virat");
+        names2.add("dhoni");
+        names2.add("gokul");
+        names2.add("bravo");
+        GokulCollection.displayNames(names1,names2);
 }
 }
