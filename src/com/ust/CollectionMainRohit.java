eaec78a92f2fc2ee6c27ffa48bbc95a8bc9d9373
Rohit Raj Saji <rohitraj.saji@ust.com>
2026-03-28 10:04:24 +0530

    HashSet

--- src/com/ust/CollectionMainRohit.java
+++ src/com/ust/CollectionMainRohit.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionMainRohit {
-    public static void main(String[] args){
-        List<String> l1=new ArrayList<>();
+    public static void main(String[] args) {
+        List<String> l1 = new ArrayList<>();
         l1.add("Rohit");
         l1.add("Harsh");
         l1.add("Keerthi");
         l1.add("Navya");
         l1.add("Deepak");
         l1.add("Gokul");
-        CollectionRohit C=new CollectionRohit();
+        CollectionRohit C = new CollectionRohit();
 
-        List<String> l2=new ArrayList<>();
+        List<String> l2 = new ArrayList<>();
         l2.add("Sreelekshmi");
         l2.add("Gokul");
         l2.add("Pranav");
@@ -22,7 +22,10 @@ public class CollectionMainRohit {
 
         C.printValues(l1);
         C.removeNeighbour(l1);
-        C.combinedList(l1,l2);
+        C.combinedList(l1, l2);
+        C.combinedListUsingHashSet(l1, l2);
+        C.combinedHashSetForLoop(l1, l2);
+        C.hashSetLambda(l1,l2);
 
     }
 
--- src/com/ust/CollectionRohit.java
+++ src/com/ust/CollectionRohit.java
@@ -3,6 +3,8 @@ package com.ust;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.Set;
+import java.util.HashSet;
 
 public class CollectionRohit {
     public void printValues(List<String> l1) {
@@ -11,34 +13,93 @@ public class CollectionRohit {
             System.out.println(name);
         }
     }
-        public void combinedList (List<String> l1, List<String> l2) {
-            List<String> result = new ArrayList<>();
-            Stream.concat(
-            l1.stream().filter(item->!l2.contains(item)),
-                    l2.stream().filter(item->!l1.contains(item))
-                           ).forEach(result::add);
 
-            System.out.println("Combined list without duplicates:");
-            for (String item : result) {
-                System.out.println(item);
-            }
-            System.out.println("Combined list with LAMBDA:");
-            result.forEach(System.out::println);
-        }
-
-
-            public void removeNeighbour (List < String > l1) {
-                System.out.println("Without neighbour:");
-                for (String name : l1) {
-                    if (!name.contains("Harsh")) {
-                        System.out.println(name);
-                    }
-
-
-                }
-                System.out.println("Using lambda");
-                l1.stream().filter(name -> !name.contains("Rohit")).
-                        forEach(name -> System.out.println(name));
-
+    public void combinedListFor(List<String> l1, List<String> l2) {
+        List<String> result = new ArrayList<>();
+        for (String item : l1) {
+            if (!l2.contains(item)) {
+                result.add(item);
             }
         }
+
+        for (String item : l2) {
+            if (!l1.contains(item)) {
+                result.add(item);
+            }
+        }
+
+        System.out.println("Combined list without duplicates using for loop:");
+        for (String item : result) {
+            System.out.println(item);
+        }
+
+    }
+
+    public void combinedList(List<String> l1, List<String> l2) {
+        List<String> result = new ArrayList<>();
+        Stream.concat(
+                l1.stream().filter(item -> !l2.contains(item)),
+                l2.stream().filter(item -> !l1.contains(item))
+        ).forEach(result::add);
+
+        System.out.println("Combined list without duplicates:");
+        for (String item : result) {
+            System.out.println(item);
+        }
+        System.out.println("Combined list with LAMBDA:");
+        result.forEach(System.out::println);
+
+    }
+
+
+    public void removeNeighbour(List<String> l1) {
+        System.out.println("Without neighbour:");
+        for (String name : l1) {
+            if (!name.contains("Harsh")) {
+                System.out.println(name);
+            }
+
+
+        }
+        System.out.println("Using lambda");
+        l1.stream().filter(name -> !name.contains("Rohit")).
+                forEach(name -> System.out.println(name));
+
+    }
+
+    public void combinedListUsingHashSet(List<String> l1, List<String> l2) {
+        Set<String> result = new HashSet<>();
+        result.addAll(l1);
+        result.addAll(l2);
+        System.out.println(result);
+    }
+
+    public void combinedHashSetForLoop(List<String> l1, List<String> l2) {
+        Set<String> result = new HashSet<>();
+        for (String item : l1) {
+            if (!l2.contains(item)) {
+                result.add(item);
+            }
+        }
+
+        for (String item : l2) {
+            if (!l1.contains(item)) {
+                result.add(item);
+            }
+        }
+
+        System.out.println("HashForLoop:");
+        System.out.println(result);
+    }
+
+    public void hashSetLambda(List<String> l1, List<String> l2) {
+        Set<String> result = new HashSet<>();
+        Stream.concat(
+                l1.stream().filter(item -> !l2.contains(item)),
+                l2.stream().filter(item -> !l1.contains(item))
+        ).forEach(result::add);
+        System.out.println("HashLambda:" + result);
+    }
+
+
+}
