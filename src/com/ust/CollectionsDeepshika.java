1c8c1aec0afb3c92b7172ad042ac2c8492c48a05
deepshika <308341@ust.com>
2026-03-28 17:56:49 +0530

    Collections framework

--- src/com/ust/CollectionsDeepshika.java
+++ src/com/ust/CollectionsDeepshika.java
@@ -1,7 +1,8 @@
 package com.ust;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CollectionsDeepshika {
 
@@ -37,5 +38,97 @@ public class CollectionsDeepshika {
         result.addAll(names.stream().filter(name1 -> !names2.contains(name1)).toList());
         result.addAll(names2.stream().filter(name2 -> !names.contains(name2)).toList());
         return result;
+    }
+
+    public void removeDuplicateSingleLoop(List<String> names, List<String> names2) {
+        List<String> newList1 = new ArrayList<>();
+        List<String> newList2 = new ArrayList<>();
+        newList1.addAll(names);
+        newList1.addAll(names2);
+        for (String name : newList2) {
+            if (!(names.contains(name) && names2.contains(name))) {
+                newList1.add(name);
+            }
+        }
+        System.out.println("List" + newList1);
+
+    }
+
+    public void removeDuplicateUsingForEach(List<String> names, List<String> names2) {
+        List<String> newList = new ArrayList<>();
+        newList.addAll(names);
+        for (String name : names2) {
+            if (!newList.contains(name)) {
+                newList.add(name);
+            }
+        }
+        newList.clear();
+        newList.addAll(names);
+        newList.addAll(names2.stream().filter(name -> !newList.contains(name)).toList());
+        iteration(newList);
+    }
+
+    public void hashIteration(Set<String> hashNames, Set<String> hashNames1) {
+        for (String name : hashNames) {
+            if (hashNames1.contains(name)) {
+                hashNames1.remove(name);
+            }
+            System.out.println(name);
+        }
+        System.out.println();
+    }
+
+    public void hashRemove(Set<String> hashNames, Set<String> hashNames1) {
+        Set<String> resultSet = new HashSet<>();
+        Set<String> resultSet1 = new HashSet<>();
+        resultSet.addAll(hashNames);
+        resultSet.addAll(hashNames1);
+        for (String name : resultSet) {
+            if (!(hashNames.contains(name) && hashNames1.contains(name))) {
+                resultSet1.add(name);
+            }
+        }
+        resultSet1.forEach(name -> System.out.println(name));
+    }
+
+    public void removeDuplicateUsingLamda(Set<String> hashNames, Set<String> hashNames1) {
+        Set<String> resultSet = Stream.concat(hashNames.stream(), hashNames1.stream()).collect(Collectors.toSet()).stream().filter(name -> !(hashNames.contains(name) && hashNames1.contains(name))).collect(Collectors.toSet());
+        System.out.println(resultSet);
+    }
+
+    public void toPrintMapNames(Map<Integer, String> mapNames) {
+        for (Map.Entry<Integer, String> name : mapNames.entrySet()) {
+            System.out.println(name.getKey() + "-" + name.getValue());
+        }
+    }
+
+    public void toPrintMapNamesKeySet(Map<Integer, String> mapNames) {
+        for (Integer name : mapNames.keySet()) {
+            System.out.println(name + "-" + mapNames.get(name));
+        }
+    }
+
+    public void toCheckWithKey(Map<Integer, String> mapNames) {
+        for (Integer name : mapNames.keySet()) {
+            if (name != 308341) {
+                System.out.println(name + "-" + mapNames.get(name));
+            }
+        }
+    }
+
+    public void toCheckWithKeyRemove(Map<Integer, String> mapNames) {
+        int empId = 308341;
+        if (mapNames.containsKey(empId)) {
+            mapNames.remove(empId);
+        }
+        System.out.println(mapNames);
+
+        mapNames.entrySet().removeIf(entry -> entry.getKey() == empId);
+        System.out.println(mapNames);
+    }
+
+    public void toRemoveKeyUsingLamda(Map<Integer, String> mapNames) {
+        int eId = 308341;
+        mapNames.entrySet().stream().filter(entry -> entry.getKey() != eId).forEach(entry -> System.out.println(entry.getKey() + "-" + entry.getValue()));
     }
 }
--- src/com/ust/MainDeepshika.java
+++ src/com/ust/MainDeepshika.java
@@ -1,8 +1,7 @@
 package com.ust;
 
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MainDeepshika {
     public static void main(String[] args) {
@@ -26,11 +25,54 @@ public class MainDeepshika {
         names2.add("visagan");
         names2.add("Deepak");
 
+        HashSet<String> hashNames = new HashSet<>();
+        hashNames.add("Deepshika");
+        hashNames.add("hema");
+        hashNames.add("visagan");
+        hashNames.add("Mamtha");
+
+        HashSet<String> hashNames1 = new HashSet<>();
+        hashNames1.add("hema");
+        hashNames1.add("nagarajan");
+        hashNames1.add("priya");
+        hashNames1.add("praneesh");
+
+        Map<Integer, String> mapNames = new HashMap<>();
+        mapNames.put(308341, "Deepshika");
+        mapNames.put(308335, "Mamatha");
+        mapNames.put(308331, "Chris");
+        mapNames.put(308242, "Nagarajan");
+        mapNames.put(308335, "Aishu");
+        mapNames.put(308336, "Bewin");
+
         CollectionsDeepshika pod2 = new CollectionsDeepshika();
         pod2.iteration(names);
         System.out.println();
+
         pod2.printNames(names);
         System.out.println();
+
         System.out.println(pod2.compareNames(names, names2));
+        System.out.println();
+
+
+        pod2.removeDuplicateUsingForEach(names, names2);
+        System.out.println();
+
+        pod2.removeDuplicateSingleLoop(names, names2);
+        //  pod2.hashIteration(hashNames, hashNames1);
+        System.out.println();
+        pod2.hashRemove(hashNames, hashNames1);
+        System.out.println();
+        pod2.removeDuplicateUsingLamda(hashNames, hashNames1);
+        pod2.toPrintMapNames(mapNames);
+        System.out.println();
+        pod2.toPrintMapNamesKeySet(mapNames);
+        System.out.println();
+        pod2.toCheckWithKey(mapNames);
+        System.out.println();
+        pod2.toCheckWithKeyRemove(mapNames);
+        System.out.println();
+        pod2.toRemoveKeyUsingLamda(mapNames);
     }
 }
