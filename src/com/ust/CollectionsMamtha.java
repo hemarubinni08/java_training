a1a0d5448993d543046f88c3eff59e619193aaf4
mamtha <308335@ust.com>
2026-03-28 17:56:49 +0530

    Collections Framework

--- src/com/ust/CollectionsMamtha.java
+++ src/com/ust/CollectionsMamtha.java
@@ -1,8 +1,10 @@
 package com.ust;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.security.Key;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CollectionsMamtha {
 
@@ -10,50 +12,137 @@ public class CollectionsMamtha {
         for (String name : names) {
             System.out.println(name);
         }
+        System.out.println();
+        names.forEach(name -> System.out.println(name));
 
     }
-    public void printExceptDeepshika(List<String> names){
-        for(String name:names){
-            if(!name.equalsIgnoreCase("deepshi")){
+
+    public void printExceptDeepshika(List<String> names) {
+        for (String name : names) {
+            if (!name.equalsIgnoreCase("deepshi")) {
                 System.out.println(name);
             }
         }
 
         System.out.println();
-        names.stream().filter(name->!name.equalsIgnoreCase("deepshi")).toList().forEach(name-> System.out.println(name));
+        names.stream().filter(name -> !name.equalsIgnoreCase("deepshi")).toList().forEach(name -> System.out.println(name));
     }
-    public List<String> compareTwoLists(List<String> names,List<String> names1){
-        List<String> resultList=new ArrayList<>();
 
-        for(String n1:names){
-            if(!names1.contains(n1)){
-                resultList.add(n1);
+    public List<String> compareTwoLists(List<String> names, List<String> names1) {
+        List<String> resultList = new ArrayList<>();
+        for (String name1 : names) {
+            if (!names1.contains(name1)) {
+                resultList.add(name1);
             }
         }
-        for (String n2:names1){
-            if(!names.contains(n2)){
-                resultList.add(n2);
+        for (String name2 : names1) {
+            if (!names.contains(name2)) {
+                resultList.add(name2);
             }
         }
-
         resultList.clear();
-
-        resultList.addAll(names.stream().filter(n1->!names1.contains(n1)).toList());
-        resultList.addAll(names1.stream().filter(n2->!names.contains(n2)).toList());
+        resultList.addAll(names.stream().filter(name1 -> !names1.contains(name1)).toList());
+        resultList.addAll(names1.stream().filter(name2 -> !names.contains(name2)).toList());
         return resultList;
     }
 
-    public void removeDuplicate(List<String> names,List<String> names1){
-        List<String> common=new ArrayList<>(names);
-        common.retainAll(names1);
+    public void removeDuplicate(List<String> names, List<String> names1) {
+//        List<String> common=new ArrayList<>(names);
+//        common.retainAll(names1);
+//
+//        List<String> result=new ArrayList<>(names);
+//        result.addAll(names1);
+//
+//        result.removeAll(common);
+//        printPod2Names(result);
 
-        List<String> result=new ArrayList<>(names);
-        result.addAll(names1);
+        List<String> resultList = new ArrayList<>();
+        List<String> commonList = new ArrayList<>();
 
-        result.removeAll(common);
-//        System.out.println(result);
-        printPod2Names(result);
+        commonList.addAll(names);
+        commonList.addAll(names1);
+        for (String n1 : commonList) {
+            if (!(names.contains(n1) && names1.contains(n1))) {
+                resultList.add(n1);
+            }
+        }
+        System.out.println("list:" + resultList);
+        System.out.println();
+        // List<String> lambdaResult=Stream.concat(names.stream(),names1.stream()).collect(Collectors.toSet()).stream().filter(name->!(names.contains(name)&&(names1.contains(name))))toList()
+//        resultList.clear();
+//        resultList.addAll(names);
+//        resultList.addAll(names1.stream().filter(n1 -> !resultList.contains(n1)).toList());
+//        printPod2Names(resultList);
+    }
+
+    public void toItrateSet(Set<String> names) {
+        names.forEach(System.out::println);
+    }
+
+    public void hashSetNames(Set<String> hNames, Set<String> hNames1) {
+        List<String> commonList = new ArrayList<>();
+        Set<String> resultSet = new HashSet<>();
+        commonList.addAll(hNames);
+        commonList.addAll(hNames1);
+        for (String name : commonList) {
+            if (!(hNames.contains(name) && hNames1.contains(name))) {
+                resultSet.add(name);
+            }
+        }
+        toItrateSet(resultSet);
+    }
+
+    public void removeDuplicate(Set<String> hNames, Set<String> hNames1) {
+        Set<String> resultSet = Stream.concat(hNames.stream(), hNames1.stream()).collect(Collectors.toSet()).stream().filter(name -> !(hNames.contains(name) && hNames1.contains(name))).collect(Collectors.toSet());
+        System.out.println("result:" + resultSet);
+    }
+
+    public void printingMap(Map<String, String> mapNames) {
+        for (Map.Entry<String, String> name : mapNames.entrySet()) {
+            System.out.println(name.getKey() + "->" + name.getValue());
+        }
+        System.out.println();
+        for (String name : mapNames.keySet()) {
+            System.out.println(name + "->" + mapNames.get(name));
+        }
 
     }
+
+    public void printExceptMamthaMap(Map<String, String> mapNames) {
+        for (String name : mapNames.keySet()) {
+            if (!name.equalsIgnoreCase("308335")) {
+                System.out.println(name + "-" + mapNames.get(name));
+            }
+        }
+
+    }
+
+    public void printExceptMyEmployeeId(Map<String, String> mapNames) {
+        String myEmployeeId = "308333";
+        if (mapNames.containsKey(myEmployeeId)) {
+            mapNames.remove(myEmployeeId);
+        }
+        System.out.println(mapNames);
+        System.out.println();
+
+        for (Map.Entry<String, String> name : mapNames.entrySet()) {
+            if (name.getKey().contains(myEmployeeId)) continue;
+
+            else System.out.println(name.getKey() + "-" + name.getValue());
+        }
+        System.out.println();
+        String myEmp = "308341";
+
+        mapNames.entrySet().removeIf(entry -> entry.getKey().equals(myEmp));
+        System.out.println(mapNames);
+
+    }
+
+    public void toRemoveKeyUsingLambda(Map<String, String> mapNames) {
+        String empId = "308335";
+        mapNames.entrySet().stream().filter(entry -> !entry.getKey().contains(empId)).
+                forEach(entry -> System.out.println(entry.getKey() + "-" + entry.getValue()));
+    }
 }
+
 
--- src/com/ust/MainMamtha1.java
+++ src/com/ust/MainMamtha1.java
@@ -1,41 +1,79 @@
 package com.ust;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class MainMamtha1{
-    public static void main(String[] args){
+public class MainMamtha1 {
+    public static void main(String[] args) {
 
-            List<String> names = new ArrayList<>();
-            names.add("Mamatha");
-            names.add("Deepshi");
-            names.add("Nagarajan");
-            names.add("CJ");
-            names.add("Aishu");
-            names.add("Bew");
+        List<String> names = new ArrayList<>();
+        names.add("Mamatha");
+        names.add("Deepshi");
+        names.add("Nagarajan");
+        names.add("CJ");
+        names.add("Aishu");
+        names.add("Bew");
 
-            List<String> names1=new ArrayList<>();
-            names1.add("hema");
-            names1.add("nadhi");
-            names1.add("deepak");
-            names1.add("CJ");
-            names1.add("Praneesh");
-            names1.add("Visagan");
+        List<String> names1 = new ArrayList<>();
+        names1.add("hema");
+        names1.add("nadhi");
+        names1.add("deepak");
+        names1.add("CJ");
+        names1.add("Praneesh");
+        names1.add("Visagan");
 
-            CollectionsMamtha pod2=new CollectionsMamtha();
-            pod2.printPod2Names(names);
-            System.out.println();
-            pod2.printExceptDeepshika(names);
-            System.out.println();
-            System.out.println(pod2.compareTwoLists(names,names1));
-            System.out.println();
-            pod2.removeDuplicate(names,names1);
-//            pod2.compareTwoLists(names,names1);
+        HashSet<String> hNames = new HashSet<>();
+        hNames.add("Mamtha");
+        hNames.add("Deepshi");
+        hNames.add("Nagarajn");
+        hNames.add("cj");
+        hNames.add("aishu");
+        hNames.add("bew");
 
+        Set<String> hNames1 = new HashSet<>();
+        hNames1.add("hema");
+        hNames1.add("nadhi");
+        hNames1.add("deepak");
+        hNames1.add("cj");
+        hNames1.add("Praneesh");
+        hNames1.add("Visagan");
 
+        CollectionsMamtha pod2 = new CollectionsMamtha();
+        pod2.printPod2Names(names);
+        System.out.println();
+        pod2.printExceptDeepshika(names);
+        System.out.println();
+        System.out.println(pod2.compareTwoLists(names, names1));
+        System.out.println();
+        pod2.removeDuplicate(names, names1);
+        System.out.println();
+        pod2.hashSetNames(hNames, hNames1);
+        System.out.println();
+        pod2.removeDuplicate(hNames, hNames1);
+        System.out.println();
+//          pod2.compareTwoLists(names,names1);
 
-        }
+        Map<String, String> mapNames = new HashMap<>();
+        mapNames.put("308335", "Mamatha");
+        mapNames.put("308341", "Deepshi");
+        mapNames.put("308334", "Nagarajan");
+        mapNames.put("308333", "cj");
+        mapNames.put("308342", "aishu");
+        mapNames.put("308343", "bew");
+
+        pod2.toRemoveKeyUsingLambda(mapNames);
+        System.out.println();
+
+        pod2.printingMap(mapNames);
+        System.out.println();
+        pod2.printExceptMamthaMap(mapNames);
+        System.out.println();
+        pod2.printExceptMyEmployeeId(mapNames);
+        System.out.println();
+//        pod2.toRemoveKeyUsingLambda(mapNames);
 
 
     }
+
+
+}
 
