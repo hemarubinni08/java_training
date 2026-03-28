2e0194a41ea2ad6aa8479572b43c9318745274c2
Harshavardhan S <308227@ust.com>
2026-03-28 18:01:55 +0530

    Collections--> Sets, hash maps

--- src/com/ust/CollectionsHarsha.java
+++ src/com/ust/CollectionsHarsha.java
@@ -1,11 +1,13 @@
 package com.ust;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CollectionsHarsha {
+
+    //Array List
+
     public void doIteration(List<String> names) {
         for (String name : names) {
             System.out.println(name);
@@ -28,51 +30,37 @@ public class CollectionsHarsha {
         }
     }
 
-    public List<String> removeDuplicate2(List<String> nameList1, List<String> nameList2) {
+    public List<String> removeDuplicateByTwoLoop(List<String> nameList1, List<String> nameList2) {
         List<String> result = new ArrayList<>();
         int count = 0;
         for (String name : nameList1) {
             for (String name2 : nameList2) {
                 if (name.equalsIgnoreCase(name2)) {
-                    System.out.println(name2 + " " + name);
                     count = count + 1;
                     break;
-
                 } else if (!name.equalsIgnoreCase(name2)) {
-                    System.out.println(name2 + " " + name);
                     count = 0;
-
                 }
             }
             System.out.println(count);
             if (count == 0) {
                 result.add(name);
-                System.out.println("added");
             } else {
                 System.out.println("Not added");
             }
         }
-        System.out.println("-------------");
-        System.out.println(nameList1);
-        System.out.println(nameList2);
-
         for (String name2 : nameList2) {
             for (String name : nameList1) {
                 if (name2.equalsIgnoreCase(name)) {
-                    System.out.println(name2 + " " + name);
                     count = count + 1;
                     break;
-
                 } else if (!name2.equalsIgnoreCase(name)) {
-                    System.out.println(name2 + " " + name);
                     count = 0;
-
                 }
             }
             System.out.println(count);
             if (count == 0) {
                 result.add(name2);
-                System.out.println("added");
             } else {
                 System.out.println("Not added");
             }
@@ -81,71 +69,132 @@ public class CollectionsHarsha {
         return result;
     }
 
-    public List<String> removeDuplicate3(List<String> nameList1, List<String> nameList2){
+    public List<String> removeDuplicateByContainsMethod(List<String> nameList1, List<String> nameList2) {
         List<String> result = new ArrayList<>();
-
-        for (String name : nameList1){
-            if (!nameList2.contains(name)){
+        for (String name : nameList1) {
+            if (!nameList2.contains(name)) {
                 result.add(name);
             }
         }
-
-        for (String name : nameList2){
-            if (!nameList1.contains(name)){
+        for (String name : nameList2) {
+            if (!nameList1.contains(name)) {
                 result.add(name);
             }
         }
-
         return result;
     }
 
-    public List<String> removeDuplicate4(List<String> nameList1, List<String> nameList2){
-
+    public List<String> removeDuplicateByStreamMethods(List<String> nameList1, List<String> nameList2) {
         List<String> newList = new ArrayList<>();
-
-        newList.addAll(nameList1.stream().filter(name->!nameList2.contains(name)).toList());
-
-        newList.addAll(nameList2.stream().filter(name->!nameList1.contains(name)).toList());
-
+        newList.addAll(nameList1.stream().filter(name -> !nameList2.contains(name)).toList());
+        newList.addAll(nameList2.stream().filter(name -> !nameList1.contains(name)).toList());
         return newList;
-
     }
 
-    public List<String> removeDuplicate5(List<String> nameList1, List<String> nameList2){
-
-        List<String> combineList = Stream.concat(nameList1.stream().filter(name->!nameList2.contains(name)),nameList2.stream().filter(name->!nameList1.contains(name))).toList();
-
+    public List<String> removeDuplicateByStreamConcat(List<String> nameList1, List<String> nameList2) {
+        List<String> combineList = Stream.concat(nameList1.stream().filter(name -> !nameList2.contains(name)), nameList2.stream().filter(name -> !nameList1.contains(name))).toList();
         return combineList;
-
     }
 
-    public List<String> removeDuplicate6(List<String> nameList1, List<String> nameList2){
+    public List<String> removeDuplicateByCombiningLists(List<String> nameList1, List<String> nameList2) {
         List<String> combine = new ArrayList<>();
         combine.addAll(nameList1);
         combine.addAll(nameList2);
         List<String> result = new ArrayList<>();
-
-        for (String name:combine){
-            if(!(nameList1.contains(name) && nameList2.contains(name))){
+        for (String name : combine) {
+            if (!(nameList1.contains(name) && nameList2.contains(name))) {
                 result.add(name);
             }
         }
-
         return result;
     }
 
-    //facing some exception issue  in the below removeDuplicate7() method
-    public List<String> removeDuplicate7(List<String> nameList1, List<String> nameList2){
-        List<String> result = new ArrayList<>();
-        result.addAll(nameList1);
-        result.addAll(nameList2);
-        int count = 0;
-        for (String name : result){
-            count = result.stream().filter(name2-> name.equalsIgnoreCase(name2)).toList().size();
-            if (count!=1){
-                result.remove(name);
+    //sets
+
+    public void GetElementsFromSetLoop(Set<String> nameSet) {
+        for (String name : nameSet) {
+            System.out.println(name);
+        }
+        // nameSet.stream().forEach(name-> System.out.println(name));
+    }
+
+    public Set<String> removeDuplicateInSet(Set<String> nameSet1,Set<String> nameSet2){
+        Set<String> result = new HashSet<>();
+        for (String name: nameSet1){
+         if (!nameSet2.contains(name)){
+             result.add(name);
+         }
+        }
+        for (String name : nameSet2){
+            if (!nameSet1.contains(name)){
+                result.add(name);
             }
         }
         return result;
+    }
+    public Set<String> removeDuplicateInSetByStream(Set<String> nameSet1,Set<String> nameSet2){
+        Set<String> result = new HashSet<>();
+        result.addAll(nameSet1.stream().filter(name->!nameSet2.contains(name)).toList());
+        result.addAll(nameSet2.stream().filter(name->!nameSet1.contains(name)).toList());
+        return result;
+    }
+    public Set<String> removeDuplicateInSetByCombine(Set<String> nameSet1,Set<String> nameSet2){
+        Set<String> combine = new HashSet<>();
+        combine.addAll(nameSet1);
+        combine.addAll(nameSet2);
+//      System.out.println(nameSet1.stream().filter(name->nameSet2.contains(name)).toList());
+        combine.removeAll(nameSet1.stream().filter(name->nameSet2.contains(name)).toList());
+        return  combine;
+    }
+    public Set<String> removeDuplicateInSetOnlyByStream(Set<String> nameSet1,Set<String> nameSet2){
+        Set<String> combine = new HashSet<>();
+        combine.addAll(nameSet1);
+        combine.addAll(nameSet2);
+        return  combine.stream().filter(name->!(nameSet1.stream().filter(x->nameSet2.contains(x)).toList()).contains(name)).collect(Collectors.toSet());
+    }
+
+    //Maps
+
+    public void displayMapKeyValuesUsingEntrySet(Map<String,String> nameMap){
+        for (Map.Entry<String,String> entry : nameMap.entrySet()){
+            System.out.println(entry.getKey()+"->"+entry.getValue());
+        }
+    }
+    public void displayMapKeyValuesUsingKeySet(Map<String,String> nameMap){
+        for (String key : nameMap.keySet()){
+            String value = nameMap.get(key);
+            System.out.println(key+"->"+value);
+        }
+    }
+    public void displayMapKeyValuesUsingForEach(Map<String,String> nameMap){
+        nameMap.forEach((Key, Value)->{
+            System.out.println(Key+"->"+Value);
+        });
+    }
+    public void findOtherPodMembersNotincludeMe(Map<String,String> nameMap){
+        for(Map.Entry<String,String> entry : nameMap.entrySet()){
+            if(!(entry.getKey() == "308227")){
+                System.out.println(entry.getKey()+"->"+entry.getValue());
+            }
+        }
+    }
+    public void findOtherPodMembersNotincludeMe2(Map<String,String> nameMap){
+        for (String key : nameMap.keySet()){
+            if (!(nameMap.get(key) == "Harshavardhan")){
+                System.out.println(key+"->"+nameMap.get(key));
+            }
+        }
+    }
+    public Map<String,String> findOtherPodMembersNotincludeMe3(Map<String,String> nameMap){
+        String employeeName = "308227";
+        if (nameMap.containsKey(employeeName)){
+            nameMap.remove(employeeName);
+        }
+        return nameMap;
+    }
+    public void StreamMap(Map<String,String> nameMap){
+        Set<Map.Entry<String, String>> entries = nameMap.entrySet();
+//      System.out.println(entries.stream().filter(entry -> !(entry.getKey().equalsIgnoreCase("308227"))).collect(Collectors.toSet()));
+        System.out.println(entries.stream().filter(entry -> !(entry.getKey().equalsIgnoreCase("308227"))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
     }
 }
--- src/com/ust/MainHarshavardhan.java
+++ src/com/ust/MainHarshavardhan.java
@@ -1,39 +1,69 @@
 package com.ust;
 
-import com.ust.impl.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MainHarshavardhan {
     public static void main(String[] args) {
 
-        List<String> names = new ArrayList<>();
-        names.add("Harsha");
-        names.add("Chandra");
-        names.add("Pranav");
-        names.add("Navneeth");
-        names.add("Varsha");
-        names.add("Deepa");
-        names.add("Rohan");
+        List<String> namesList1 = new ArrayList<>();
+        namesList1.add("Harsha");
+        namesList1.add("Chandra");
+        namesList1.add("Pranav");
+        namesList1.add("Navneeth");
+        namesList1.add("Varsha");
+        namesList1.add("Deepa");
+        namesList1.add("Rohan");
 
-        List<String> names2 = new ArrayList<>();
-        names2.add("Anjan");
-        names2.add("Kushal");
-        names2.add("Divya");
-        names2.add("Harsha");
-        names2.add("Rohan");
-        names2.add("Moksha");
-        names2.add("Harsha");
-        names2.add("Deeksha");
+        List<String> namesList2 = new ArrayList<>();
+        namesList2.add("Anjan");
+        namesList2.add("Kushal");
+        namesList2.add("Divya");
+        namesList2.add("Harsha");
+        namesList2.add("Rohan");
+        namesList2.add("Moksha");
+        namesList2.add("Harsha");
+        namesList2.add("Deeksha");
 
 
         CollectionsHarsha obj = new CollectionsHarsha();
 //        obj.doIteration(names);
 //        obj.doIterationByStream(names);
-//          obj.igonreNeighbour(names);
-//          System.out.println();
-//          obj.ignoreNeighbourForLoop(names);
-        System.out.println( obj.removeDuplicate6(names,names2));
+//        obj.igonreNeighbour(names);
+//        System.out.println();
+//        obj.ignoreNeighbourForLoop(names);
+//        System.out.println( obj.removeDuplicate6(names,names2));
+
+        Set<String> nameSet1 = new HashSet<>();
+        nameSet1.add("Anjan");
+        nameSet1.add("Kushal");
+        nameSet1.add("Divya");
+        nameSet1.add("Harsha");
+        nameSet1.add("Rohan");
+        nameSet1.add("Moksha");
+        nameSet1.add("Harsha");
+        nameSet1.add("Deeksha");
+
+        Set<String> nameSet2 = new HashSet<>();
+        nameSet2.add("Harsha");
+        nameSet2.add("Chandra");
+        nameSet2.add("Pranav");
+        nameSet2.add("Navneeth");
+        nameSet2.add("Varsha");
+        nameSet2.add("Deepa");
+        nameSet2.add("Rohan");
+
+       // System.out.println(obj.removeDuplicateInSetByCombine2(nameSet1,nameSet2));
+
+        Map<String,String> newMap = new HashMap<>();
+        newMap.put("308227","Harshavardhan");
+        newMap.put("898766","Pranav");
+        newMap.put("454656","Deepa");
+        newMap.put("456366","Varsha");
+        newMap.put("234256","Navneeth");
+        newMap.put("234553","Chandra");
+        obj.StreamMap(newMap);
+
+//        System.out.println(obj.findOtherPodMembersNotincludeMe3(newMap));
+
     }
 }
