f7b8bd66480a469e38258c0eed8a8caa6904afbd
Aron George Jain <arongeorgejain@gmail.com>
2026-03-28 11:30:12 +0530

    Created a function to perform set operations

---
+++ src/com/ust/CollectionsAron.java
@@ -0,0 +1,76 @@
+package com.ust;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+// function to print all the names of people in your pod
+public class CollectionsAron {
+    public void doIteration(List<String> names) {
+        for (String name : names) {
+            System.out.println(name);
+        }
+        names.forEach(System.out::println);
+    }
+
+    // function to print names that excluding the person sitting near you in your pod
+    public void filteredIteration(List<String> names) {
+        for (String name : names)
+            if (!name.equals("Shashi"))
+                System.out.println(name);
+        // Lambda expression
+        names.stream().filter(name -> !name.equals("Shashi")).forEach(System.out::println);
+    }
+
+    public void uniqueElement(List<String> names1, List<String> names2) {
+        // Create a list to store non duplicated values of both lists
+        List<String> nonUnique = new ArrayList<>();
+        // for each loop to add elements of names1 that are not in names2
+        for (String name1 : names1) {
+            if (!names2.contains(name1)) nonUnique.add(name1);
+        }
+        // for each loop to add elements of names2 that are not in names1
+        for (String name2 : names2) {
+            if (!names1.contains(name2)) nonUnique.add(name2);
+        }
+//        nonUnique.forEach(System.out::println);
+        nonUnique.clear();
+        nonUnique.addAll(names1);
+        nonUnique.addAll(names2);
+        //Lambda expression to print non-duplicate numbers in the merged list
+        nonUnique.stream().filter(nu -> !names1.contains(nu) || !names2.contains(nu)).forEach(System.out::println);
+    }
+
+    public void traverseSet(HashSet<String> setNames1, HashSet<String> setNames2) {
+        // for each loop to add elements of names1 that are not in names2
+        HashSet<String> mergedSet = new HashSet<>();
+        mergedSet.addAll(setNames1);
+        mergedSet.addAll(setNames2);
+        for (String setIterator : mergedSet) {
+            System.out.println(setIterator);
+        }
+        System.out.println();
+        // Lambda expression
+        mergedSet.forEach(System.out::println);
+    }
+
+    public void filterSet(HashSet<String> setNames1) {
+        for (String setName1 : setNames1)
+            if (!setName1.equals("Shashi"))
+                System.out.println(setName1);
+    }
+
+    public void removeDuplicateSet(HashSet<String> setNames1, HashSet<String> setNames2) {
+        HashSet<String> duplicateSet = new HashSet<>();
+        for (String setName1 : setNames1)
+            if(setNames2.contains(setName1))
+                duplicateSet.add(setName1);
+        HashSet<String> mergedSet = new HashSet<>();
+        mergedSet.addAll(setNames1);
+        mergedSet.addAll(setNames2);
+        mergedSet.removeAll(duplicateSet);
+        mergedSet.forEach(System.out::println);
+        System.out.println();
+        mergedSet.stream().filter(Iterator->!duplicateSet.contains(Iterator)).forEach(System.out::println);
+    }
+}
--- src/com/ust/MainAron.java
+++ src/com/ust/MainAron.java
@@ -4,42 +4,49 @@ import com.ust.impl.CarInterfaceAronImpl;
 import com.ust.impl.BikeInterfaceAronImpl;
 import com.ust.impl.LorryInterfaceAronImpl;
 import com.ust.impl.AutoInterfaceAronImpl;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MainAron {
     public static void main(String[] args) {
-//        CarInterfaceAronImpl car = new CarInterfaceAronImpl();
-//        System.out.println("Car");
-//        System.out.println("Brand Name: " + car.getBrandName());
-//        System.out.println("Car makes the sound " + car.getSound());
-//        System.out.println("No of wheels in a car " + car.getNoOfWheels());
-//        System.out.println("No of doors in a car " + car.getNoOfDoors());
-//        System.out.println("No of warranty in years: " + car.getWarrantyInYears());
-//
-//        System.out.println("\nBike");
-//        BikeInterfaceAronImpl bike = new BikeInterfaceAronImpl();
-//        System.out.println("Brand Name: " + bike.getBrandName());
-//        System.out.println("Bike makes the sound " + bike.getSound());
-//        System.out.println("No of wheels in a Bike " + bike.getNoOfWheels());
-//        System.out.println("No of warranty in years: " + bike.getWarrantyInYears());
-//
-//        System.out.println("\nAuto");
-//        AutoInterfaceAronImpl auto = new AutoInterfaceAronImpl();
-//        System.out.println("Brand Name: " + auto.getBrandName());
-//        System.out.println("Auto makes the sound " + auto.getSound());
-//        System.out.println("No of wheels in a Bike " + auto.getNoOfWheels());
-//        System.out.println("No of doors in an auto " + auto.getNoOfDoors());
-//        System.out.println("No of warranty in years: " + auto.getWarrantyInYears());
-//
-//        System.out.println("\nLorry");
-//        LorryInterfaceAronImpl lorry = new LorryInterfaceAronImpl();
-//        System.out.println("Brand Name: " + lorry.getBrandName());
-//        System.out.println("Lorry makes the sound " + lorry.getSound());
-//        System.out.println("No of wheels in a Bike " + lorry.getNoOfWheels());
-//        System.out.println("No of doors in a Lorry " + lorry.getNoOfDoors());
-//        System.out.println("No of warranty in years: " + lorry.getWarrantyInYears());
+        List<String> names1 = new ArrayList<>();
+        names1.add("Aron");
+        names1.add("Lekhya");
+        names1.add("Shashi");
+        names1.add("Logesh");
+        names1.add("Chaila");
+        List<String> names2 = new ArrayList<>();
+        names2.add("Pranav");
+        names2.add("Harsh");
+        names2.add("Chandra");
+        names2.add("Raja");
+        names2.add("Chris");
+        names2.add("Aron");
+        // Class Object
+        CollectionsAron collection = new CollectionsAron();
+//        collection.doIteration(names);
+//        collection.filteredIteration(names);
+//        collection.uniqueElement(names1, names2);
+        HashSet<String> setNames1 = new HashSet<>();
+        setNames1.add("Aron");
+        setNames1.add("Lekhya");
+        setNames1.add("Shashi");
+        setNames1.add("Logesh");
+        setNames1.add("Chaila");
+        setNames1.add("Raja");
+        HashSet<String> setNames2 = new HashSet<>();
+        setNames2.add("Pranav");
+        setNames2.add("Harsh");
+        setNames2.add("Chandra");
+        setNames2.add("Raja");
+        setNames2.add("Chris");
+        setNames2.add("Aron");
+        collection.traverseSet(setNames1, setNames2);
+        System.out.println();
+        collection.filterSet(setNames1);
+        System.out.println();
+        collection.removeDuplicateSet(setNames1, setNames2);
 
-          ArithmeticOperationsAron arithemticObj=new ArithmeticOperationsAron();
-          System.out.println(arithemticObj.addition(1,2));
-          System.out.println(arithemticObj.addition(3,4,5));
     }
 }
