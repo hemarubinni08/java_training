1d6d0b4b935a7dfcdde466c45d9404a62a2327f7
Deepak <308330@ust.com>
2026-03-28 12:33:42 +0530

    Collections-hashset

--- src/com/ust/impl/CollectionsDeepak.java
+++ src/com/ust/impl/CollectionsDeepak.java
@@ -27,25 +27,25 @@ import java.util.stream.Stream;
 public class CollectionsDeepak {
 
     public static void iterationCollections(List<Integer> list1, List<Integer> list2) {
-//
-//        for (Integer num : list1) {
-//            if (list2.contains(num)) {
-//                continue;
-//            }
-//            System.out.println(num);
-//        }
-//        for (Integer num : list2) {
-//            if (!list1.contains(num)) {
-//                System.out.println(num);
-//            }
-//        } for loop.
-//        list1.stream()
-//                .filter(num -> !list2.contains(num))
-//                .forEach(num -> System.out.println(num));
-//
-//        list2.stream()
-//                .filter(num -> !list1.contains(num))
-//                .forEach(num -> System.out.println(num));
+
+        for (Integer num : list1) {
+            if (list2.contains(num)) {
+                continue;
+            }
+            System.out.println(num);
+        }
+        for (Integer num : list2) {
+            if (!list1.contains(num)) {
+                System.out.println(num);
+            }
+        }
+        list1.stream()
+                .filter(num -> !list2.contains(num))
+                .forEach(num -> System.out.println(num));
+
+        list2.stream()
+                .filter(num -> !list1.contains(num))
+                .forEach(num -> System.out.println(num));
         Stream.concat(
                 list1.stream().filter(num -> !list2.contains(num)),
                 list2.stream().filter(num -> !list1.contains(num))
