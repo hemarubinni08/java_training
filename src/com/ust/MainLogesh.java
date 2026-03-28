802b72247475edcf931e1149412c304f652fce47
Logeshwaran <308343@ust.com>
2026-03-28 11:28:22 +0530

    Collected

--- src/com/ust/MainLogesh.java
+++ src/com/ust/MainLogesh.java
@@ -4,26 +4,43 @@ import com.ust.impl.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 public class MainLogesh {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
+//        List<String> names = new ArrayList<>();
+//        names.add("Logesh");
+//        names.add("Shashi");
+//        names.add("Aron");
+//        names.add("Lekhya");
+//        names.add("Chaila");
+//        CollectionLogesh collection = new CollectionLogesh();
+////        collection.filterNeighbour(names);
+//        List<String> names1 = new ArrayList<>();
+//        names1.add("Logesh");
+//        names1.add("Nagarajan");
+//        names1.add("Virat");
+//        names1.add("Ronaldo");
+//        names1.add("Rohit");
+//        names1.add("Vijay");
+//        collection.doItration(names, names1);
+        HashSet<String> names = new HashSet<>();
         names.add("Logesh");
         names.add("Shashi");
         names.add("Aron");
         names.add("Lekhya");
         names.add("Chaila");
-        CollectionLogesh collection = new CollectionLogesh();
-//        collection.filterNeighbour(names);
 
-        List<String> names1 = new ArrayList<>();
+        HashSet<String> names1 = new HashSet<>();
         names1.add("Logesh");
         names1.add("Nagarajan");
         names1.add("Virat");
         names1.add("Ronaldo");
         names1.add("Rohit");
-        names1.add("Vijay");
+        CollectionLogesh collection = new CollectionLogesh();
         collection.doItration(names, names1);
+        collection.filterNeighbour(names);
+        collection.loopingNames(names);
     }
 }
