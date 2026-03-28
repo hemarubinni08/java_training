155daeaf0690952f9745ac98e966cdd632c35a6f
Keerthi <308344@ust.com>
2026-03-28 12:25:34 +0530

    Collection2

--- src/com/ust/MainKeerthi.java
+++ src/com/ust/MainKeerthi.java
@@ -7,30 +7,47 @@ import java.util.List;
 public class MainKeerthi {
     public static void main(String[] args) {
 
-        List<String> names = new ArrayList<>();
-        names.add("keerthi");
-        names.add("navya");
-        names.add("deepak");
-        names.add("gokul");
-        names.add("harsh");
-        names.add("rohit");
-        List<String> sree = new ArrayList<>();
-        sree.add("keerthi");
-        sree.add("anu");
-        sree.add("lekya");
-        sree.add("lali");
-        sree.add("jp");
-        sree.add("kush");
-         CollectionKv.getNonDuplicate(names,sree);
+//        List<String> names = new ArrayList<>();
+//        names.add("keerthi");
+//        names.add("navya");
+//        names.add("deepak");
+//        names.add("gokul");
+//        names.add("harsh");
+//        names.add("rohit");
+//        List<String> sree = new ArrayList<>();
+//        sree.add("keerthi");
+//        sree.add("anu");
+//        sree.add("lekya");
+//        sree.add("lali");
+//        sree.add("jp");
+//        sree.add("kush");
+         //CollectionKv.getNonDuplicate(names,sree);
 //        CollectionKv.collectionIterations(names);
 //        CollectionKv.collectionIteration(names);
 //        CollectionKv.listIteration(sree);
-//        HashSet<String> set = new HashSet<>();
-//        set.add("keerthi");
-//        set.add("navya");
-//        set.add("harsh");
-//        set.add("rohit");
-//        set.add("deepak");
+//        HashSet<String> set1 = new HashSet<>();
+//        set1.add("keerthi");
+//        set1.add("navya");
+//        set1.add("harsh");
+//        set1.add("rohit");
+//        set1.add("deepak");
+//        set1.add("gokul");
+//        HashSet<String> set2 = new HashSet<>();
+//        set2.add("keerthi");
+//        set2.add("anu");
+//        set2.add("lekya");
+//        set2.add("lali");
+//        set2.add("jp");
+//        set2.add("kush");
+//        CollectionSetKvNew collectionSetKvNew = new CollectionSetKvNew();
+//        collectionSetKvNew.collectionSetIterations(set1);
+//        collectionSetKvNew.hashIteration(set1);
+//        collectionSetKvNew.getNonDuplicate(set1,set2);
+//        collectionSetKvNew.removeDuplicateSet(set1,set2);
+//        CollectionSetKvNew obj=new CollectionSetKvNew();
+//        System.out.println(obj.removeDuplicateSet(set1,set2));
+
+
 
 
     }
