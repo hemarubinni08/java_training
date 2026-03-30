fa329f446b22bd8398bd98b17890f9825caf1da8
“ardra” <ardra.ah@ust.com>
2026-03-30 14:42:39 +0530

    added collections (set, map)

--- src/com/ust/CollectionArdra.java
+++ src/com/ust/CollectionArdra.java
@@ -1,162 +1,176 @@
 package com.ust;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class CollectionArdra {
-    void doIterate(List<String> list) {
-        for (String s : list) {
-            System.out.println(s);
+    void doIterateListFor(List<String> namesList) {
+        for (String name : namesList) {
+            System.out.println(name);
         }
     }
 
-    void doIterateLambda(List<String> list) {
-        list.forEach((name) -> System.out.println(name));
+    void doIterateListLambda(List<String> namesList) {
+        namesList.forEach((name) -> System.out.println(name));
     }
 
-    void filterNeighbour(List<String> list) {
-        list.stream().filter((name) -> !name.equals("Hemanth")).forEach(System.out::println);
+    void filterNeighbourList(List<String> namesList) {
+        namesList.stream().filter((name) -> !name.equals("Hemanth")).forEach(System.out::println);
     }
 
-    List<String> deleteDuplicatesFor(List<String> list1, List<String> list2) {
-        List<String> returnList = new ArrayList<String>();
-        for (String s : list2) {
-            if (!list1.contains(s)) {
-                returnList.add(s);
+    List<String> deleteDuplicatesListFor(List<String> fruitsList1, List<String> fruitsList2) {
+        List<String> returnList = new ArrayList<>();
+        for (String fruits : fruitsList2) {
+            if (!fruitsList1.contains(fruits)) {
+                returnList.add(fruits);
             }
         }
-        for (String s : list1) {
-            if (!list2.contains(s)) {
-                returnList.add(s);
+        for (String fruits : fruitsList1) {
+            if (!fruitsList2.contains(fruits)) {
+                returnList.add(fruits);
             }
         }
         return returnList;
     }
 
-    List<String> deleteDuplicatesLambda(List<String> list1, List<String> list2) {
-        List<String> returnList = new ArrayList<String>();
-        list2.stream().filter((s) -> !list1.contains(s)).forEach(returnList::add);
-        list1.stream().filter((s) -> !list2.contains(s)).forEach(returnList::add);
+    List<String> deleteDuplicatesListLambda(List<String> fruitsList1, List<String> fruitsList2) {
+        List<String> returnList = new ArrayList<>();
+        fruitsList2.stream().filter((fruit) -> !fruitsList1.contains(fruit)).forEach(returnList::add);
+        fruitsList1.stream().filter((fruit) -> !fruitsList2.contains(fruit)).forEach(returnList::add);
         return returnList;
     }
 
-    List<String> deleteDuplicatesMergeFor(List<String> list1, List<String> list2) {
+    List<String> deleteDuplicatesListMergeFor(List<String> fruitsList1, List<String> fruitsList2) {
         List<String> returnList = new ArrayList<String>();
         List<String> combinedList = new ArrayList<String>();
-        combinedList.addAll(list1);
-        combinedList.addAll(list2);
-        for (String s : combinedList) {
-            if (!(list1.contains(s) && list2.contains(s))) {
-                returnList.add(s);
+        combinedList.addAll(fruitsList1);
+        combinedList.addAll(fruitsList2);
+        for (String fruit : combinedList) {
+            if (!(fruitsList1.contains(fruit) && fruitsList2.contains(fruit))) {
+                returnList.add(fruit);
             }
         }
         return returnList;
     }
 
-    List<String> deleteDuplicatesMergeLambda(List<String> list1, List<String> list2) {
+    List<String> deleteDuplicatesListMergeLambda(List<String> fruitsList1, List<String> fruitsList2) {
         List<String> returnList = new ArrayList<String>();
         List<String> combinedList = new ArrayList<String>();
-        combinedList.addAll(list1);
-        combinedList.addAll(list2);
-        combinedList.stream().filter((s) -> !(list1.contains(s) && list2.contains(s))).forEach(returnList::add);
+        combinedList.addAll(fruitsList1);
+        combinedList.addAll(fruitsList2);
+        combinedList.stream().filter((fruit) -> !(fruitsList1.contains(fruit) && fruitsList2.contains(fruit))).forEach(returnList::add);
         return returnList;
     }
 
-    void doIterateHashsetFor(Set<String> set) {
-        for (String s : set) {
-            System.out.println(s);
+    void doIterateHashsetFor(Set<String> namesSet) {
+        for (String name : namesSet) {
+            System.out.println(name);
         }
     }
 
-    void doIterateHashsetLambda(Set<String> set) {
-        set.forEach((s) -> System.out.println(s));
+    void doIterateHashsetLambda(Set<String> namesSet) {
+        namesSet.forEach((name) -> System.out.println(name));
     }
 
-    void filterNeighbourSet(Set<String> set) {
-        set.stream().filter((name) -> !name.equals("Hemanth")).forEach(System.out::println);
+    void filterNeighbourSetFor(Set<String> namesSet) {
+        for (String name : namesSet) {
+            if (!name.equals("Hemanth")) {
+                System.out.println(name);
+            }
+        }
     }
 
-    Set<String> deleteDuplicatesSetFor1(Set<String> set1, Set<String> set2) {
+    void filterNeighbourSetLambda(Set<String> namesSet) {
+        namesSet.stream().filter((name) -> !name.equals("Hemanth")).forEach(System.out::println);
+    }
+
+    Set<String> deleteDuplicatesSetFor1(Set<String> fruitsSet1, Set<String> fruitsSet2) {
         Set<String> returnSet = new HashSet<>();
-        for (String s : set2) {
-            if (!set1.contains(s)) {
-                returnSet.add(s);
+
+        for (String fruit : fruitsSet2) {
+            if (!fruitsSet1.contains(fruit)) {
+                returnSet.add(fruit);
             }
         }
-        for (String s : set1) {
-            if (!set2.contains(s)) {
-                returnSet.add(s);
+
+        for (String fruit : fruitsSet1) {
+            if (!fruitsSet2.contains(fruit)) {
+                returnSet.add(fruit);
             }
         }
         return returnSet;
     }
 
-    Set<String> deleteDuplicatesSetFor2(Set<String> set1, Set<String> set2) {
+    Set<String> deleteDuplicatesSetFor2(Set<String> fruitsSet1, Set<String> fruitsSet2) {
         Set<String> duplicatesSet = new HashSet<>();
-        for (String s : set1) if (set2.contains(s)) duplicatesSet.add(s);
-        set1.addAll(set2);
-        for (String s : duplicatesSet) if (set1.contains(s)) set1.remove(s);
-        return set1;
+        for (String fruit : fruitsSet1) if (fruitsSet2.contains(fruit)) duplicatesSet.add(fruit);
+        fruitsSet1.addAll(fruitsSet2);
+
+        for (String fruit : duplicatesSet) {
+            fruitsSet1.remove(fruit);
+        }
+
+        return fruitsSet1;
     }
 
-    Set<String> deleteDuplicatesSetLambda(Set<String> set1, Set<String> set2) {
+    Set<String> deleteDuplicatesSetLambda(Set<String> fruitsSet1, Set<String> fruitsSet2) {
         Set<String> duplicatesSet = new HashSet<>();
-        set1.stream().filter((s) -> ((set2.contains(s)))).forEach(duplicatesSet::add);
-        set1.addAll(set2);
-        //return set1.stream().filter((s)->(!duplicatesSet.contains(s))).collect(Collectors.toSet());
-        duplicatesSet.stream().filter(s -> set1.contains(s)).forEach(set1::remove);
-        return set1;
+        fruitsSet1.stream().filter((fruit) -> ((fruitsSet2.contains(fruit)))).forEach(duplicatesSet::add);
+        fruitsSet1.addAll(fruitsSet2);
+        duplicatesSet.stream().filter(fruit -> fruitsSet1.contains(fruit)).forEach(fruitsSet1::remove);
+        return fruitsSet1;
     }
 
-    void doIterateMapForKeyset(Map<Integer, String> map) {
-        for (Integer k : map.keySet()) {
-            System.out.printf(k + " - " + map.get(k));
+    void doIterateMapForKeyset(Map<Integer, String> namesMap) {
+        for (Integer employeeID : namesMap.keySet()) {
+            System.out.printf(employeeID + " - " + namesMap.get(employeeID));
         }
     }
 
-    void doIterateMapForEntrySet(Map<Integer, String> map) {
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+    void doIterateMapForEntrySet(Map<Integer, String> namesMap) {
+        for (Map.Entry<Integer, String> entry : namesMap.entrySet()) {
             System.out.println(entry.getKey() + " - " + entry.getValue());
         }
     }
 
-    void doIterateMapLambdaKeyset(Map<Integer, String> map) {
-        map.forEach((k, v) -> System.out.println(k + " - " + v));
+    void doIterateMapLambdaKeyset(Map<Integer, String> namesMap) {
+        namesMap.forEach((key, value) -> System.out.println(key + " - " + value));
     }
 
-    void doIterateMapLambdaEntryset(Map<Integer, String> map) {
-        map.entrySet().forEach(entry -> System.out.println(entry.getKey() + " - " + entry.getValue()));
+    void doIterateMapLambdaEntryset(Map<Integer, String> namesMap) {
+        namesMap.forEach((key, value) -> System.out.println(key + " - " + value));
     }
 
-    void filterMemberMapFor(Map<Integer, String> map) {
-        for (Integer k : map.keySet()) {
-            if (k != 308339) System.out.println(k + " - " + map.get(k));
+    void filterMemberMapFor(Map<Integer, String> namesMap) {
+        for (Integer employeeID : namesMap.keySet()) {
+            if (employeeID != 308339) System.out.println(employeeID + " - " + namesMap.get(employeeID));
         }
     }
 
-    void filterMemberMapLambda(Map<Integer, String> map) {
-        map.entrySet().stream().filter(entry -> entry.getKey() != 308339).forEach(entry -> System.out.println(entry.getKey() + " - " + entry.getValue()));
+    void filterMemberMapLambda(Map<Integer, String> namesMap) {
+        namesMap.entrySet().stream().filter(entry -> entry.getKey() != 308339).forEach(entry -> System.out.println(entry.getKey() + " - " + entry.getValue()));
     }
 
-    Map<Integer, String> deleteDuplicatesMapFor(Map<Integer, String> map1, Map<Integer, String> map2) {
+    Map<Integer, String> deleteDuplicatesMapFor(Map<Integer, String> fruitsMap1, Map<Integer, String> fruitsMap2) {
         Map<Integer, String> duplicatesMap = new HashMap<>();
-        for (Map.Entry<Integer, String> entry : map1.entrySet()) {
-            if (map2.containsValue(entry.getValue())) {
+
+        for (Map.Entry<Integer, String> entry : fruitsMap1.entrySet()) {
+            if (fruitsMap2.containsValue(entry.getValue())) {
                 duplicatesMap.put(entry.getKey(), entry.getValue());
             }
         }
-        map1.putAll(map2);
-        for (String s : duplicatesMap.values()) {
-            map1.entrySet().removeIf(entry -> entry.getValue().equals(s));
+        fruitsMap1.putAll(fruitsMap2);
+
+        for (String fruit : duplicatesMap.values()) {
+            fruitsMap1.entrySet().removeIf(entry -> entry.getValue().equals(fruit));
         }
-        return map1;
+        return fruitsMap1;
     }
 
-    Map<Integer, String> deleteDuplicatesMapLambda(Map<Integer, String> map1, Map<Integer, String> map2) {
+    Map<Integer, String> deleteDuplicatesMapLambda(Map<Integer, String> fruitsMap1, Map<Integer, String> fruitsMap2) {
         Map<Integer, String> duplicatesMap = new HashMap<>();
-        map1.entrySet().stream().filter(entry -> ((map2.containsValue(entry.getValue())))).forEach(entry -> duplicatesMap.put(entry.getKey(), entry.getValue()));
-        map1.putAll(map2);
-        duplicatesMap.forEach((key, value) -> map1.entrySet().removeIf(entry1 -> entry1.getValue().equals(value)));
-        return map1;
+        fruitsMap1.entrySet().stream().filter(entry -> ((fruitsMap2.containsValue(entry.getValue())))).forEach(entry -> duplicatesMap.put(entry.getKey(), entry.getValue()));
+        fruitsMap1.putAll(fruitsMap2);
+        duplicatesMap.forEach((key, value) -> fruitsMap1.entrySet().removeIf(entry1 -> entry1.getValue().equals(value)));
+        return fruitsMap1;
     }
 }
--- src/com/ust/CollectionArdraMain.java
+++ src/com/ust/CollectionArdraMain.java
@@ -6,6 +6,40 @@ import java.util.*;
 public class CollectionArdraMain {
     public static void main(String[] args) {
         CollectionArdra collection = new CollectionArdra();
+        List<String> podMembers = new ArrayList<String>(Arrays.asList("Ardra", "Hemanth", "Anu", "Ritu", "Yeshwanth", "Akash"));
+        List<String> list1 = new ArrayList<>(List.of("apple", "mango", "banana", "strawberry", "cherry"));
+        List<String> list2 = new ArrayList<>(List.of("mango", "orange", "grapes", "watermelon", "blueberry"));
+
+        collection.doIterateListFor(podMembers);
+        collection.doIterateListLambda(podMembers);
+        collection.filterNeighbourList(podMembers);
+
+        List<String> list3 = collection.deleteDuplicatesListFor(list1, list2);
+        List<String> list4 = collection.deleteDuplicatesListLambda(list1, list2);
+        List<String> list5 = collection.deleteDuplicatesListMergeFor(list1, list2);
+        List<String> list6 = collection.deleteDuplicatesListMergeLambda(list1, list2);
+
+        Set<String> podMembersSet = new HashSet<>(Arrays.asList("Ardra", "Hemanth", "Anu", "Ritu", "Yeshwanth", "Akash"));
+        Set<String> set1 = new HashSet<>(Arrays.asList("apple", "mango", "banana", "strawberry", "cherry"));
+        Set<String> set2 = new HashSet<>(Arrays.asList("mango", "orange", "grapes", "watermelon", "blueberry"));
+
+        collection.doIterateHashsetFor(podMembersSet);
+        collection.doIterateHashsetLambda(podMembersSet);
+        collection.filterNeighbourSetFor(podMembersSet);
+        collection.filterNeighbourSetLambda(podMembersSet);
+
+        Set<String> set3 = collection.deleteDuplicatesSetFor1(set1, set2);
+        Set<String> set4 = collection.deleteDuplicatesSetFor2(set1, set2);
+        Set<String> set5 = collection.deleteDuplicatesSetLambda(set1, set2);
+
+        Map<Integer, String> podMembersMap = new HashMap<>();
+        podMembersMap.put(308260, "Ritu");
+        podMembersMap.put(308347, "Anu");
+        podMembersMap.put(308279, "Hemanth");
+        podMembersMap.put(308339, "Ardra");
+        podMembersMap.put(308303, "Akash");
+        podMembersMap.put(308331, "Yeshwanth");
+
         Map<Integer, String> map1 = new HashMap<>();
         Map<Integer, String> map2 = new HashMap<>();
         map1.put(1, "apple");
@@ -18,41 +52,15 @@ public class CollectionArdraMain {
         map2.put(8, "pineapple");
         map2.put(9, "strawberry");
         map2.put(10, "watermelon");
-        Map<Integer, String> map3 = collection.deleteDuplicatesMapLambda(map1, map2);
-        collection.doIterateMapLambdaKeyset(map3);
 
-//        Map<Integer,String> podMembersMap=new HashMap<>();
-//        podMembersMap.put(308260, "Ritu");
-//        podMembersMap.put(308347, "Anu");
-//        podMembersMap.put(308279, "Hemanth");
-//        podMembersMap.put(308339, "Ardra");
-//        podMembersMap.put(308303, "Akash");
-//        podMembersMap.put(308331, "Yeshwanth");
+        collection.doIterateMapForKeyset(podMembersMap);
+        collection.doIterateMapForEntrySet(podMembersMap);
+        collection.doIterateMapLambdaKeyset(podMembersMap);
+        collection.doIterateMapLambdaEntryset(podMembersMap);
+        collection.filterMemberMapFor(podMembersMap);
+        collection.filterMemberMapLambda(podMembersMap);
 
-//        if(podMembersMap.containsKey("308339")) podMembersMap.remove("308339");
-
-//        Set<String> set1 = new HashSet<>(Arrays.asList("apple", "mango", "banana", "strawberry", "cherry"));
-//        Set<String> set2 = new HashSet<>(Arrays.asList("mango", "orange", "grapes", "watermelon", "blueberry"));
-//        Set<String> finalSet = collection.deleteDuplicatesSetFor2(set1, set2);
-//        collection.doIterateHashsetFor(finalSet);
-
-//        Set<String> podMembersSet = new HashSet<>(Arrays.asList("Ardra", "Hemanth","Anu","Ritu","Yeshwanth","Akash"));
-//        collection.filterNeighbourSet(podMembersSet);
-
-//        List<String> firstList = new ArrayList<>(List.of("apple", "mango","banana","strawberry","cherry"));
-//        List<String> secondList = new ArrayList<>(List.of("mango", "orange","grapes","watermelon","blueberry"));
-//        List<String> finalList = collection.deleteDuplicatesMergeLambda(firstList, secondList);
-//        collection.doIterate(finalList);
-
-//        List<String> podMembers = new List<String>();
-//        podMembers.add("Ardra");
-//        podMembers.add("Hemanth");
-//        podMembers.add("Anu");
-//        podMembers.add("Ritika");
-//        podMembers.add("Yeshwanth");
-//        podMembers.add("Akash");
-//        collection.filterNeighbour(podMembers);
-//        collection.doIterate(podMembers);
-//        collection.doIterateLambda(podMembers);
+        Map<Integer, String> map3 = collection.deleteDuplicatesMapFor(map1, map2);
+        Map<Integer, String> map4 = collection.deleteDuplicatesMapLambda(map1, map2);
     }
 }
