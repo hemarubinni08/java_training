a60a885ab71027311432f88f4a4aa4687d913e3c
Hemanth <hemanth@gmail.com>
2026-03-30 11:29:49 +0530

    Collection Hemanth Map.

--- src/com/ust/CollectionHemanthMap.java
+++ src/com/ust/CollectionHemanthMap.java
@@ -7,40 +7,37 @@ import java.util.stream.Collectors;
 public class CollectionHemanthMap {
     public void printMap(Map<String, String> batchNames) {
         Map<String, String> resultMap = new HashMap<>();
-        /*for (Map.Entry<String, String> mapEntry : batchNames.entrySet()) {
+        for (Map.Entry<String, String> mapEntry : batchNames.entrySet()) {
             String key = mapEntry.getKey();
             String value = mapEntry.getValue();
-            System.out.println(key + "->" + value);*/
-        /*for (String mapEntry : batchNames.keySet()) {
-            String key = mapEntry;
-            System.out.println(key);*/
-        //batchNames.entrySet().forEach(entry -> System.out.println(entry.getKey() + "->" + entry.getValue()));
-        //.stream().filter(entry -> !(batchNames.containsKey(1))).forEach();
-
-        /*for (Map.Entry<String, String> mapEntry : batchNames.entrySet()) {
-            String key = mapEntry.getKey();
-            String value = mapEntry.getValue();
-            /*if (!(key.equals("1"))) {      //batchNames.containsKey("1");
-                System.out.println(key + "->" + value);
-            }*/
-            /*if(!(value).equals("Hemanth")){  //batchNames.containsValue("Hemanth");
-                System.out.println(key + "->" + value);
-            }
+            System.out.println(key + "->" + value);
         }
-        batchNames.containsValue("Hemanth");*/
+        for (String name : batchNames.keySet()) {
+            String key1 = name;
+            System.out.println(key1);
+        }
+        batchNames.entrySet().forEach(entry -> System.out.println(entry.getKey() + "->" + entry.getValue()));
+
+
+        batchNames.containsValue("Hemanth");
         // batchNames.entrySet().removeIf(entry -> (entry.getKey().equals("1")));
         batchNames.entrySet().stream().filter(entry -> !(entry.getKey().
                 equals("1"))).forEach(entry ->
                 System.out.println(entry.getKey() + "->" + entry.getValue()));
         Map<String, String> result = batchNames.entrySet().stream().filter(entry -> !(entry.getKey().
                 equals("1"))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
     }
 
     public void removeDuplicateMap(Map<String, String> batchNames, Map<String, String> batchNames1) {
-        Map<String,String> resultMap= new HashMap<>(batchNames1);
+        Map<String, String> resultMap = new HashMap<>(batchNames1);
+        Map<String, String> temporaryMap = new HashMap<>();
         resultMap.putAll(batchNames);
-        for (Map.Entry<String,String> name : batchNames.entrySet()){
+        /*for(String name : batchNames.keySet()){
+            if(batchNames1.containsKey(name)){
+                resultMap.remove(name);
+            }
+        }*/
+        /*for (Map.Entry<String,String> name : batchNames.entrySet()){
             if(batchNames1.containsKey(name.getKey()) || batchNames1.containsValue(name.getValue()) ){
                 resultMap.remove(name.getKey());
                 resultMap.values().remove(name.getValue());
@@ -48,6 +45,14 @@ public class CollectionHemanthMap {
         }
         for (Map.Entry<String,String>name : resultMap.entrySet()){
             System.out.println(name.getKey()+"->"+name.getValue());
-        }
+        }*/
+        batchNames.entrySet().stream().filter(entry -> (batchNames1.containsKey(entry.getKey())
+                || batchNames1.containsValue(entry.getValue()))).forEach(entry -> temporaryMap.put(entry.getKey(), entry.getValue()));
+        //resultMap.entrySet().stream().filter(entry-> !(temporaryMap.containsKey(entry.getKey()) ||
+        //      temporaryMap.containsValue(entry.getValue()))).collect(Collectors.toMap(Map.Entry::getKey , Map.Entry::getValue));
+        resultMap.entrySet().stream().filter(entry -> !(temporaryMap.containsKey(entry.getKey()) ||
+                temporaryMap.containsValue(entry.getValue()))).forEach(entry -> System.out.println(entry.getKey() + "->" + entry.getValue()));
+        //resultMap.entrySet().removeIf(entry-> temporaryMap.containsKey(entry.getKey()) || temporaryMap.containsValue(entry.getValue()));
+
     }
 }
