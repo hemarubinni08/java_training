fb0cf46da849b245894d6cda6699cc33d7e6b330
Aron George Jain <arongeorgejain@gmail.com>
2026-03-30 12:12:30 +0530

    Followed coding standards and added comments

--- src/com/ust/CollectionsAron.java
+++ src/com/ust/CollectionsAron.java
@@ -6,14 +6,17 @@ import java.util.stream.Stream;
 // function to print all the names of people in your pod
 public class CollectionsAron {
     public void doIteration(List<String> names) {
+        // use for each to loop through the list and print
         for (String name : names) {
             System.out.println(name);
         }
+        // printing the same in a single line
         names.forEach(System.out::println);
     }
 
     // function to print names that excluding the person sitting near you in your pod
     public void filteredIteration(List<String> names) {
+        // use for each to loop through the list and print only if the name is not your neighbour's
         for (String name : names)
             if (!name.equals("Shashi"))
                 System.out.println(name);
@@ -32,7 +35,6 @@ public class CollectionsAron {
         for (String name2 : names2) {
             if (!names1.contains(name2)) nonUnique.add(name2);
         }
-//        nonUnique.forEach(System.out::println);
         nonUnique.clear();
         nonUnique.addAll(names1);
         nonUnique.addAll(names2);
@@ -53,26 +55,39 @@ public class CollectionsAron {
         mergedSet.forEach(System.out::println);
     }
 
+    // function to print all names in the set excluding the name of your neighbor
     public void filterSet(HashSet<String> setNames1) {
+        // loop to traverse the set
         for (String setName1 : setNames1)
+            // condition that checks if the current value in the loop is your neighbor's name
             if (!setName1.equals("Shashi"))
                 System.out.println(setName1);
     }
 
+    // Function that receives two sets and print only the values that are not common in both sets
     public void removeDuplicateSet(HashSet<String> setNames1, HashSet<String> setNames2) {
+        // create a set to store the duplicate values
         HashSet<String> duplicateSet = new HashSet<>();
+        // loop to traverse one set
         for (String setName1 : setNames1)
+            // check if the current name in setNames1 is in setNames2
             if (setNames2.contains(setName1))
+                // add that value to duplicate set
                 duplicateSet.add(setName1);
+        // set to merge the values of both sets
         HashSet<String> mergedSet = new HashSet<>();
         mergedSet.addAll(setNames1);
         mergedSet.addAll(setNames2);
+        // remove all duplicate values from the merged set
         mergedSet.removeAll(duplicateSet);
+        // print values in the merged set
         mergedSet.forEach(System.out::println);
         System.out.println();
+        // lambda expression
         mergedSet.stream().filter(Iterator -> !duplicateSet.contains(Iterator)).forEach(System.out::println);
     }
 
+    // function to print all the values in the map
     public void traverseMap(Map<String, String> employeesMap) {
         for (Map.Entry<String, String> employee : employeesMap.entrySet()) {
             System.out.println(employee.getKey() + " - " + employee.getValue());
@@ -84,40 +99,49 @@ public class CollectionsAron {
 
     }
 
+    // function to remove your data from the map using key and printing the rest
     public void removeMeMapKey(Map<String, String> employeesMap, String employeeKey) {
+        // loop through the entire loop
         for (String key : employeesMap.keySet()) {
+            // check if the key matches your employee id and print only if it's not
             if (!key.equals("308336"))
                 System.out.println(key + " - " + employeesMap.get(key));
         }
         System.out.println();
+        // check using containsKey method
         if (employeesMap.containsKey(employeeKey))
             employeesMap.remove(employeeKey);
+        // print the map
         for (Map.Entry<String, String> employee : employeesMap.entrySet()) {
             System.out.println(employee.getKey() + " - " + employee.getValue());
         }
     }
 
+    // function to remove your data from the map using value and printing the rest
     public void removeMeMapValue(Map<String, String> employeesMap, String employeeName) {
+        // check of the passed employee name is in the map
         if (employeesMap.containsValue(employeeName)) {
             employeesMap.values().remove(employeeName);
         }
         System.out.println();
+        // After removing just print rest of the values
         for (Map.Entry<String, String> employee : employeesMap.entrySet()) {
             System.out.println(employee.getKey() + " - " + employee.getValue());
         }
     }
 
+    // Remove the common word from both maps by value
     public void removeDuplicatesValueMap(Map<String, String> employeesMap1, Map<String, String> employeesMap2) {
-        HashSet<String> duplicateSet = new HashSet<>();
+        // loop through the entire employeeMap1
         for (Map.Entry<String, String> employee : employeesMap1.entrySet()) {
+            // check if the current value is in employeesMap2
             if (employeesMap2.containsValue(employee.getValue())) {
+                // If yes remove that value from both the maps
                 employeesMap1.values().remove(employee.getValue());
                 employeesMap2.values().remove(employee.getValue());
             }
         }
-        System.out.println(duplicateSet);
-        for (String duplicate : duplicateSet)
-            employeesMap2.values().remove(duplicate);
+        // print both maps
         for (Map.Entry<String, String> employee : employeesMap1.entrySet()) {
             System.out.println(employee.getKey() + " - " + employee.getValue());
         }
@@ -127,17 +151,23 @@ public class CollectionsAron {
     }
 
     public void removeDuplicatesKeyMap(Map<String, String> employeesMap1, Map<String, String> employeesMap2) {
+        // loop through the employeesMap1
         for (String key : employeesMap1.keySet()) {
+            // check if the current key is in employeesMap2
             if (employeesMap2.containsKey(key)) {
+                // if yes remove from both maps
                 employeesMap1.remove(key);
                 employeesMap2.remove(key);
             }
         }
+        // create a map to merge both maps
         Map<String, String> mergedMap = new HashMap<>(employeesMap1);
         mergedMap.putAll(employeesMap2);
+        // loop through the merged map and print the key-value pair
         for (Map.Entry<String, String> employee : mergedMap.entrySet()) {
             System.out.println(employee.getKey() + " - " + employee.getValue());
         }
+        // Do the same with stream
         Stream.concat(employeesMap1.entrySet().stream().filter(e1 -> !employeesMap2.containsKey(e1.getKey())), employeesMap2.entrySet().stream().filter(e2 -> !employeesMap1.containsKey(e2.getKey()))).forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
     }
 }
--- src/com/ust/MainAron.java
+++ src/com/ust/MainAron.java
@@ -1,21 +1,20 @@
 package com.ust;
 
-import com.ust.impl.CarInterfaceAronImpl;
-import com.ust.impl.BikeInterfaceAronImpl;
-import com.ust.impl.LorryInterfaceAronImpl;
-import com.ust.impl.AutoInterfaceAronImpl;
-
 import java.util.*;
 
 public class MainAron {
     public static void main(String[] args) {
+        // Creating list names1
         List<String> names1 = new ArrayList<>();
+        // Adding values to names1
         names1.add("Aron");
         names1.add("Lekhya");
         names1.add("Shashi");
         names1.add("Logesh");
         names1.add("Chaila");
+        // Creating list names2
         List<String> names2 = new ArrayList<>();
+        // Adding values to names2
         names2.add("Pranav");
         names2.add("Harsh");
         names2.add("Chandra");
@@ -24,43 +23,61 @@ public class MainAron {
         names2.add("Aron");
         // Class Object
         CollectionsAron collection = new CollectionsAron();
+        // Call the method doIteration
         collection.doIteration(names1);
+        // Call the method filteredIteration
         collection.filteredIteration(names1);
+        // Call the method uniqueElement
         collection.uniqueElement(names1, names2);
+        // Create a hashset to store names
         HashSet<String> setNames1 = new HashSet<>();
+        // Add names to the set
         setNames1.add("Aron");
         setNames1.add("Lekhya");
         setNames1.add("Shashi");
         setNames1.add("Logesh");
         setNames1.add("Chaila");
         setNames1.add("Raja");
+        // Create another hashset to store names
         HashSet<String> setNames2 = new HashSet<>();
+        // Add names to the set
         setNames2.add("Pranav");
         setNames2.add("Harsh");
         setNames2.add("Chandra");
         setNames2.add("Raja");
         setNames2.add("Chris");
         setNames2.add("Aron");
+        // Call method traverseSet
         collection.traverseSet(setNames1, setNames2);
         System.out.println();
+        // Call method filterSet
         collection.filterSet(setNames1);
         System.out.println();
+        // Call method removeDuplicateSet
         collection.removeDuplicateSet(setNames1, setNames2);
         // HashMap with key as employee id and value as team member name
         Map<String, String> employeesMap1 = new HashMap<>();
+        // Adding values
         employeesMap1.put("308336", "Aron");
         employeesMap1.put("308238", "Shashi");
         employeesMap1.put("308337", "Lekhya");
         employeesMap1.put("308343", "Logesh");
         employeesMap1.put("308333", "Chaila");
+        // create another hashmap to store id and names
         Map<String, String> employeesMap2 = new HashMap<>();
+        // Adding values
         employeesMap2.put("308336", "Aron");
         employeesMap2.put("308239", "Ram");
         employeesMap2.put("308331", "Charan");
+        // call to method traverseMap
         collection.traverseMap(employeesMap1);
+        // call to method removeMeMapKey
         collection.removeMeMapKey(employeesMap1, "308336");
+        // call to method removeMeMapValue
         collection.removeMeMapValue(employeesMap1, "Aron");
+        // call to method removeDuplicatesValueMap
         collection.removeDuplicatesValueMap(employeesMap1, employeesMap2);
+        // call to method removeDuplicatesKeyMap
         collection.removeDuplicatesKeyMap(employeesMap1, employeesMap2);
     }
 }
