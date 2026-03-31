eb858693a68cf640e74348a2525fa882df739bfe
Navaneeth <308320@ust.com>
2026-03-31 09:56:40 +0530

    Collections Navaneeth

--- src/com/ust/CollectionNavaneeth.java
+++ src/com/ust/CollectionNavaneeth.java
@@ -2,10 +2,11 @@ package com.ust;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CollectionNavaneeth {
 
-    public List<String> skipDuplicates(List<String> names, List<String> names1) {
+    public void skipDuplicates(List<String> names, List<String> names1) {
         List<String> result = new ArrayList<>();
 
         for (String s : names) {
@@ -17,40 +18,36 @@ public class CollectionNavaneeth {
             if (!names.contains(s))
                 result.add(s);
         }
-
         result.forEach(System.out::println);
-
-//        Lamda
 
         names.stream().filter(s -> !names1.contains(s)).forEach(result::add);
         names1.stream().filter(s -> !names.contains(s)).forEach(result::add);
         System.out.println(result);
-        return result;
-
-//        List<String> combinedLamda = Stream.concat(names.stream(), names1.stream())
-//                .filter(s -> !(names.contains(s) && names1.contains(s))).toList();
-//        System.out.println(combinedLamda);
-//        return combinedLamda;
-
     }
-//
-//
-    public void skipNeighbour(List<String> names){
 
-        names.stream().filter(name -> !name.equalsIgnoreCase("Chandu")).forEach(name-> System.out.println(name));
+    public List<String> combinedListLambda(List<String> names, List<String> names1) {
 
-        for(String name : names){
-            if(!name.equalsIgnoreCase("Chandu")){
+        List<String> combinedLamda = Stream.concat(names.stream(), names1.stream())
+                .filter(s -> !(names.contains(s) && names1.contains(s))).toList();
+        System.out.println(combinedLamda);
+        return combinedLamda;
+    }
+
+    public void skipNeighbour(List<String> names) {
+
+        names.stream().filter(name -> !name.equalsIgnoreCase("Chandu")).forEach(System.out::println);
+
+        for (String name : names) {
+            if (!name.equalsIgnoreCase("Chandu")) {
                 System.out.println(name);
             }
         }
     }
-////
+
     public void iteration(List<String> names) {
 
         System.out.println(names.stream().filter(name ->
                 !name.equals("navaneeth")).toList());
-
 
         for (String name : names) {
             if (!name.equals("navaneeth")) {
@@ -59,42 +56,65 @@ public class CollectionNavaneeth {
         }
     }
 
-
-    public Set<String> setIteration(Set<String> set1, Set<String> set2){
-        for (String num : set1) {
+    public Set<String> setIteration(Set<String> employeeSet1, Set<String> employeeSet2) {
+        for (String num : employeeSet1) {
             System.out.println(num);
         }
+        System.out.println(employeeSet1.stream().toList());
 
-        System.out.println(set1.stream().toList());
         Set<String> result = new HashSet<>();
-        for (String num1: set1 ){
-            if (!set2.contains(num1)){
-                result.add(num1);;
+        for (String num1 : employeeSet1) {
+            if (!employeeSet2.contains(num1)) {
+                result.add(num1);
             }
         }
 
-        for (String num2: set2 ){
-            if (!set1.contains(num2)){
+        for (String num2 : employeeSet2) {
+            if (!employeeSet1.contains(num2)) {
                 result.add(num2);
             }
         }
         return result;
-
-//        Set<String> duplicateSet = new HashSet<>();
-//        Set<String> combinedSet = new HashSet<>();
-//        for (String num1: set1 ) {
-//            if (set2.contains(num1)) {
-//                duplicateSet.add(num1);
-//            }
-//        }
-//
-//        combinedSet.addAll(set1);
-//        combinedSet.addAll(set2);
-//
-//        combinedSet.removeAll(duplicateSet);
-//        return combinedSet;
-
     }
+
+    public boolean findDuplicatesInSet(Set<String> employeeSet1, Set<String> employeeSet2) {
+        Set<String> result1 = new HashSet<>();
+        Set<String> result2 = new HashSet<>();
+
+        for (String num1 : employeeSet1) {
+            if (!(employeeSet2.contains(num1))) {
+                result1.add(num1);
+            }
+        }
+
+        for (String num2 : employeeSet2) {
+            if (!(employeeSet1.contains(num2))) {
+                result1.add(num2);
+            }
+        }
+
+        employeeSet1.stream().filter(num -> !employeeSet2.contains(num)).forEach(result2::add);
+        employeeSet2.stream().filter(num -> !employeeSet1.contains(num)).forEach(result2::add);
+        return false;
+    }
+
+    public Set<String> combinedDuplicatesSet(Set<String> employeeSet1, Set<String> employeeSet2) {
+        Set<String> duplicateSet = new HashSet<>();
+        Set<String> combinedSet = new HashSet<>();
+
+        for (String num1 : employeeSet1) {
+            if (employeeSet2.contains(num1)) {
+                duplicateSet.add(num1);
+            }
+        }
+
+        combinedSet.addAll(employeeSet1);
+        combinedSet.addAll(employeeSet2);
+        combinedSet.removeAll(duplicateSet);
+        return combinedSet;
+    }
+
+
     public void hashMap(Map<Integer, String> members) {
         for (Integer key : members.keySet()) {
             System.out.println(key + " - " + members.get(key));
@@ -108,35 +128,101 @@ public class CollectionNavaneeth {
                 System.out.println(Key + " - " + Value));
     }
 
-    public Map<Integer, String> removeMe(Map<Integer, String> members){
-
-        if(members.containsValue("Navaneeth")){
+    public Map<Integer, String> removeMe(Map<Integer, String> members) {
+        if (members.containsValue("Navaneeth")) {
             members.values().remove("Navaneeth");
         }
         return members;
     }
-    public Map<Integer, String> removeSpecificUsingStreamMethod(Map<Integer, String> map) {
-        map.entrySet().removeIf(entry -> entry.getKey() == 6);
-        return map;
+
+    public Map<Integer, String> removeSpecificUsingStreamMethod(Map<Integer, String> employeeMap) {
+        employeeMap.entrySet().removeIf(entry -> entry.getKey() == 6);
+        return employeeMap;
     }
 
-    public void removeSpecificUsingStreamMethodUsingFilter(Map<Integer, String> map) {
-        map.entrySet()
+    public void removeSpecificUsingStreamMethodUsingFilter(Map<Integer, String> employeeMap) {
+        employeeMap.entrySet()
                 .stream()
                 .filter(entry -> !entry.getKey().equals(6))
                 .forEach(entry ->
                         System.out.println(entry.getKey() + " - " + entry.getValue())
                 );
     }
-    public Map<Integer, String> removeSpecificUsingToMap(Map<Integer, String> map) {
-        Map<Integer, String> newmap = map.entrySet()
+
+    public Map<Integer, String> removeSpecificUsingToMap(Map<Integer, String> employeeMap) {
+        Map<Integer, String> newMap;
+        newMap = employeeMap.entrySet()
                 .stream()
                 .filter(entry -> !entry.getKey().equals(6))
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         Map.Entry::getValue
                 ));
-        return newmap;
+        return newMap;
+    }
+
+    public Map<Integer, String> removeDuplicatesUsingMap(Map<Integer, String> members, Map<Integer, String> members1) {
+        Map<Integer, String> result = new HashMap<>();
+
+        for (Map.Entry<Integer, String> e : members.entrySet()) {
+            if (!members1.containsKey(e.getKey()) || !members1.get(e.getKey()).equals(e.getValue())) {
+                result.put(e.getKey(), e.getValue());
+            }
+        }
+
+        for (Map.Entry<Integer, String> e : members1.entrySet()) {
+            if (!members.containsKey(e.getKey()) || !members.get(e.getKey()).equals((e.getValue()))) {
+                result.put(e.getKey(), e.getValue());
+            }
+        }
+        return result;
+    }
+
+    public void removeDuplicatesUsingStreamMap(Map<Integer, String> members, Map<Integer, String> members1) {
+        Map<Integer, String> result = Stream.concat(
+                members.entrySet().stream()
+                        .filter(e -> !members1.containsKey(e.getKey()) ||
+                                !members1.get(e.getKey()).equals(e.getValue())),
+                members1.entrySet().stream()
+                        .filter(e -> !members.containsKey(e.getKey()) ||
+                                !members.get(e.getKey()).equals(e.getValue()))
+        ).collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue
+        ));
+        System.out.println(result);
+    }
+
+    public Map<Integer, String> removeDuplicatesCombineUsingForLoopMap(Map<Integer, String> members, Map<Integer, String> members1) {
+        Map<Integer, String> duplicate = new HashMap<>();
+
+        for (Map.Entry<Integer, String> e : members.entrySet()) {
+            if (members1.containsKey(e.getKey()) && members1.get(e.getKey()).equals(e.getValue())) {
+                duplicate.put(e.getKey(), e.getValue());
+            }
+        }
+
+        Map<Integer, String> combine = new HashMap<>(members);
+        combine.putAll(members1);
+
+        for (Integer key : duplicate.keySet()) {
+            combine.remove(key);
+        }
+        return combine;
+
+    }
+
+    public Map<Integer, String> removeDuplicatesCombineUsingStreamMap(Map<Integer, String> members,
+                                                                      Map<Integer, String> members1) {
+        Map<Integer, String> duplicates = members.entrySet().stream().
+                filter(entry -> members1.containsKey(entry.getKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        Map<Integer, String> combined = new HashMap<>(members);
+        combined.putAll(members1);
+        combined.entrySet().stream().filter(entry -> !duplicates.containsKey(entry.getKey())).
+                forEach(System.out::println);
+        combined.entrySet().removeAll(duplicates.entrySet());
+        return combined;
     }
 }
-
--- src/com/ust/MainNavaneeth.java
+++ src/com/ust/MainNavaneeth.java
@@ -23,21 +23,21 @@ public class MainNavaneeth {
         names1.add("Ram");
         names1.add("Pranav");
 
-        Set<String> set1 = new HashSet<>();
-        set1.add("Chandu");
-        set1.add("Varsha");
-        set1.add("Deepa");
-        set1.add("Harsha");
-        set1.add("Pranav");
-        set1.add("Navaneeth");
+        Set<String> employeeSet1 = new HashSet<>();
+        employeeSet1.add("Chandu");
+        employeeSet1.add("Varsha");
+        employeeSet1.add("Deepa");
+        employeeSet1.add("Harsha");
+        employeeSet1.add("Pranav");
+        employeeSet1.add("Navaneeth");
 
-        Set<String> set2 = new HashSet<>();
-        set2.add("1");
-        set2.add("2");
-        set2.add("3");
-        set2.add("4");
-        set2.add("5");
-        set2.add("Navaneeth");
+        Set<String> employeeSet2 = new HashSet<>();
+        employeeSet2.add("1");
+        employeeSet2.add("2");
+        employeeSet2.add("3");
+        employeeSet2.add("4");
+        employeeSet2.add("5");
+        employeeSet2.add("Navaneeth");
 
         Map<Integer, String> members = new HashMap<>();
         members.put(1, "Chandu");
@@ -47,19 +47,33 @@ public class MainNavaneeth {
         members.put(5, "Pranav");
         members.put(6, "Navaneeth");
 
-//        Map<Integer,String> members1 = new HashMap<>();
-//        members1.put(6, "Navaneeth");
-//        members1.put(7,"vishal");
-//        members1.put(8,"mallik");
+        Map<Integer, String> members1 = new HashMap<>();
+        members1.put(6, "Navaneeth");
+        members1.put(7, "vishal");
+        members1.put(8, "mallik");
 
         CollectionNavaneeth cn = new CollectionNavaneeth();
-////        cn.skipDuplicates(names,names1);
-////        cn.setIteration(set1, set2);
-//        System.out.println(cn.setIteration(set1,set2));
-//        System.out.println(cn.removeMe(members));
-//        System.out.println(cn.removeSpecificUsingStreamMethod(members));
-//        cn.removeSpecificUsingStreamMethodUsingFilter(members);
-        System.out.println(cn.removeSpecificUsingToMap(members));
-    }
+        cn.skipNeighbour(names);
+        cn.iteration(names);
+        cn.skipDuplicates(names, names1);
+        cn.combinedListLambda(names, names1);
 
+        cn.setIteration(employeeSet1, employeeSet2);
+        System.out.println(cn.setIteration(employeeSet1, employeeSet2));
+        System.out.println(cn.findDuplicatesInSet(employeeSet1, employeeSet2));
+        System.out.println(cn.combinedDuplicatesSet(employeeSet1, employeeSet2));
+
+        cn.hashMap(members);
+        System.out.println(cn.removeMe(members));
+        System.out.println(cn.removeSpecificUsingStreamMethod(members));
+
+        cn.removeSpecificUsingStreamMethodUsingFilter(members);
+        System.out.println(cn.removeSpecificUsingToMap(members));
+
+        cn.removeDuplicatesUsingStreamMap(members, members1);
+        System.out.println(cn.removeDuplicatesUsingMap(members, members1));
+        System.out.println(cn.removeDuplicatesUsingMap(members, members1));
+        System.out.println(cn.removeDuplicatesCombineUsingForLoopMap(members, members1));
+        System.out.println(cn.removeDuplicatesCombineUsingStreamMap(members, members1));
+    }
 }
