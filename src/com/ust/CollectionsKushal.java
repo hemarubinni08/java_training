90220dbd2bd8512045f1fd97ca43bda7c36af210
kushal <kushal.s@ust.com>
2026-03-31 16:20:30 +0530

    Updated Collections and Main Class

--- src/com/ust/CollectionsKushal.java
+++ src/com/ust/CollectionsKushal.java
@@ -4,33 +4,35 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class CollectionsKushal {
-
-    public void doIteration(List<String> podMembersList) {
+    //LISTS
+    public void doListIterationUsingForLoop(List<String> podMembersList) {
         for (String podMember : podMembersList) {
             System.out.println(podMember);
         }
+        podMembersList.forEach(System.out::println);
+    }
 
-        podMembersList.forEach(podMember -> System.out.println(podMember));
-
+    public void doListIterationUsingStream(List<String> podMembersList) {
         System.out.println(podMembersList.stream().toList());
     }
 
-    public void doIterationExcludingNeighbour(List<String> podMembersList) {
+    public void doListIterationExcludeNeighbourUsingLoop(List<String> podMembersList) {
         for (String podMember : podMembersList) {
             if (!podMember.equalsIgnoreCase("srujaN")) {
                 System.out.println(podMember);
             }
         }
+    }
 
-        System.out.println("c1");
-        podMembersList.stream().filter(podMember -> !podMember.equals("Srujan")).toList().forEach(podMember -> System.out.println(podMember));
-        System.out.println("c2");
+    public void doListIterationExcludeNeighbourUsingStream(List<String> podMembersList) {
+        podMembersList.stream().filter(podMember -> !podMember.equals("Srujan")).toList().
+                forEach(System.out::println);
         System.out.println(podMembersList.stream().filter(podMember -> !podMember.equals("Srujan")).toList());
     }
 
-    public void doIterationRemoveDuplicatesFromTwoLists(List<String> podMembers, List<String> nonPodMembers) {
-        System.out.println("Result Using For Loop:");
-        System.out.println();
+    public void doListIterationRemoveDuplicatesFromTwoListsUsingForLoop(List<String> podMembers, List<String> nonPodMembers) {
+        // USING 2 ForLoop
+        System.out.println("Result Using Two For Loop:");
         List<String> resultList = new ArrayList<>();
         for (String name : podMembers) {
             if (!nonPodMembers.contains(name)) {
@@ -44,139 +46,139 @@ public class CollectionsKushal {
         }
         System.out.println(resultList);
 
-        System.out.println();
-        System.out.println("Result Using Lambda Expression: ");
-        System.out.println();
+        // USING 1 ForLoop
         List<String> resultList1 = new ArrayList<>();
-        resultList1.addAll(podMembers.stream().filter(name -> !nonPodMembers.contains(name)).toList());
-        resultList1.addAll(nonPodMembers.stream().filter(name -> !podMembers.contains(name)).toList());
-        resultList1.forEach(name -> System.out.println(name));
-
-    }
-
-    public void doIterationRemoveDuplicatesFromTwoLists1(List<String> podMembers, List<String> nonPodMembers) {
-        System.out.println("Result Using For Loop:");
-        System.out.println();
-        List<String> resultList = new ArrayList<>();
-        resultList.addAll(podMembers);
-        resultList.addAll(nonPodMembers);
-        nonPodMembers.clear();
-        podMembers.clear();
-        System.out.println(List.of(resultList));
-        for (String memberName : resultList) {
+        List<String> mergedList1 = new ArrayList<>();
+        mergedList1.addAll(podMembers);
+        mergedList1.addAll(nonPodMembers);
+        System.out.println("Result Using One For Loop:");
+        for (String memberName : mergedList1) {
             if (!(podMembers.contains(memberName) && nonPodMembers.contains(memberName))) {
-                podMembers.add(memberName);
+                resultList1.add(memberName);
             }
         }
-        podMembers.forEach(name -> System.out.println(name));
-
-        System.out.println();
-        System.out.println();
-        System.out.println("Result Using Lambda Expression: ");
-        System.out.println();
-        List<String> resultList1 = new ArrayList<>();
-        resultList1.addAll(podMembers);
-        resultList1.addAll(nonPodMembers);
-        nonPodMembers.clear();
-        podMembers.clear();
-        resultList1.stream().filter(memberName -> !(podMembers.contains(memberName) && nonPodMembers.contains(memberName)));
-
-
+        resultList1.forEach(System.out::println);
     }
 
-    public void doIteration(HashSet<String> podMembersSet, HashSet<String> nonPodMembersSet) {
-        System.out.println("Result by using for loop");
+    public void doListIterationRemoveDuplicatesFromTwoListsUsingStream(List<String> podMembers, List<String> nonPodMembers) {
+        //USING TWO STREAMS
+        List<String> resultList = new ArrayList<>();
+        System.out.println("Result Using Two Streams:");
+        resultList.addAll(podMembers.stream().filter(name -> !nonPodMembers.contains(name)).toList());
+        resultList.addAll(nonPodMembers.stream().filter(name -> !podMembers.contains(name)).toList());
+        resultList.forEach(System.out::println);
+
+        //USING ONE STREAM
+        List<String> resultList1 = new ArrayList<>();
+        System.out.println();
+        System.out.println("Result Using One Stream:");
+        resultList1.stream().filter(memberName -> !(podMembers.contains(memberName) && nonPodMembers.contains(memberName)));
+        resultList1.forEach(System.out::println);
+    }
+
+    //SETS
+    public void doSetIterationUsingForLoop(HashSet<String> podMembersSet, HashSet<String> nonPodMembersSet) {
+        //USING ONE FOR LOOP
+        System.out.println("Result Using One For Loops:");
         HashSet<String> resultSet = new HashSet<>();
         resultSet.addAll(podMembersSet);
         resultSet.addAll(nonPodMembersSet);
         for (String name : resultSet) {
             System.out.println(name);
         }
-        System.out.println();
-        System.out.println("Result by using for Lambda Expressio");
 
-        resultSet.forEach(name -> System.out.println(name));
-    }
-
-    public void doIterationRemoveDuplicatesFromTwoSets(HashSet<String> podMembersSet, HashSet<String> nonPodMembersSet) {
-        System.out.println("Result Using For Loop:");
-        HashSet<String> resultSet = new HashSet<>();
+        //USING TWO FOR LOOPS
+        System.out.println("Result Using Two For Loops:");
+        HashSet<String> resultSet1 = new HashSet<>();
         for (String name : podMembersSet) {
             if (!nonPodMembersSet.contains(name)) {
-                resultSet.add(name);
+                resultSet1.add(name);
             }
         }
+
         for (String name : nonPodMembersSet) {
             if (!podMembersSet.contains(name)) {
-                resultSet.add(name);
+                resultSet1.add(name);
             }
         }
-        resultSet.forEach(name -> System.out.println(name));
+        resultSet1.forEach(System.out::println);
+    }
 
+    public void doSetIterationUsingStream(HashSet<String> podMembersSet, HashSet<String> nonPodMembersSet) {
+        //USING ONE STREAM
+        System.out.println("Result Using One Stream:");
+        HashSet<String> resultSet = new HashSet<>();
+        resultSet.forEach(System.out::println);
+
+        //USING TWO STREAMS
         System.out.println();
-        System.out.println("Output Using Lambda Function:");
+        System.out.println("Result Using Two Streams:");
         HashSet<String> resultSet1 = new HashSet<>();
         resultSet1.addAll(podMembersSet.stream().filter(name -> !nonPodMembersSet.contains(name)).collect(Collectors.toSet()));
         resultSet1.addAll(nonPodMembersSet.stream().filter(name -> !podMembersSet.contains(name)).toList());
-        resultSet1.forEach(name -> System.out.println(name));
+        resultSet1.forEach(System.out::println);
     }
 
-    public void doIterationRemoveDuplicatesFromTwoSets1(HashSet<String> podMembersSet, HashSet<String> nonPodMembersSet) {
-        System.out.println("Result Using For Loop:");
+    public void doSetIterationRemoveDuplicatesFromTwoSetsUsingForLoop(HashSet<String> podMembersSet, HashSet<String> nonPodMembersSet) {
+        //USING ONE FOR LOOP
+        System.out.println("Result Using One For Loop:");
         HashSet<String> resultSet = new HashSet<>();
         List<String> commonList = new ArrayList<>();
         resultSet.addAll(podMembersSet);
         resultSet.addAll(nonPodMembersSet);
+
         for (String podMember : podMembersSet) {
             if (nonPodMembersSet.contains(podMember)) {
                 commonList.add(podMember);
             }
         }
-        resultSet.removeAll(commonList);
-        resultSet.forEach(name -> System.out.println(name));
-
-        System.out.println();
-        System.out.println();
-        System.out.println("Result Using Lambda Function: ");
-
-        resultSet.removeAll(podMembersSet.stream().filter(name -> nonPodMembersSet.contains(name)).toList());
-        resultSet.forEach(name -> System.out.println(name));
-
+        commonList.forEach(resultSet::remove);
+        resultSet.forEach(System.out::println);
     }
 
-    public void doIteration(Map<Integer, String> podMembersMap) {
+    public void doSetIterationRemoveDuplicatesFromTwoSetsUsingStream(HashSet<String> podMembersSet, HashSet<String> nonPodMembersSet) {
+        //USING ONE STREAM
+        System.out.println("Result Using One Stream:");
+        HashSet<String> resultSet = new HashSet<>();
+        resultSet.addAll(podMembersSet);
+        resultSet.addAll(nonPodMembersSet);
+        podMembersSet.stream().filter(nonPodMembersSet::contains).toList().forEach(resultSet::remove);
+        resultSet.forEach(System.out::println);
+
+        //USING TWO STREAMS
+    }
+
+    //MAPS
+    public void doMapIterationUsingForLoop(Map<Integer, String> podMembersMap) {
         System.out.println("Result Using For Loop");
-        System.out.println();
-        System.out.println("Pod Member Details:");
         // Using keySet
+        System.out.println("Result Using KeySet");
         for (int id : podMembersMap.keySet()) {
             System.out.println(id + " - " + podMembersMap.get(id));
         }
+
         // Using EntrySet
+        System.out.println();
+        System.out.println("Result Using EntrySet");
         for (Map.Entry<Integer, String> member : podMembersMap.entrySet()) {
             System.out.println(member.getKey() + " - " + member.getValue());
         }
+    }
 
-
-        System.out.println();
+    public void doMapIterationUsingStreams(Map<Integer, String> podMembersMap) {
         System.out.println("Result Using Lambda Expression");
-        System.out.println("Pod Member Details:");
-        System.out.println();
         podMembersMap.forEach((id, name) -> System.out.println(id + " - " + name));
     }
 
-    public void doIterationExcludingMyName(Map<Integer, String> podMembersMap) {
+    public void doMapIterationExcludingMyNameUsingForLoop(Map<Integer, String> podMembersMap) {
+        //REMOVE MY NAME BY CHECKING ID
         for (int id : podMembersMap.keySet()) {
             if (id != 308323) {
                 System.out.println(id + " - " + podMembersMap.get(id));
             }
         }
-        podMembersMap.forEach((id, name) -> {
-            if (id != 308323) {
-                System.out.println(id + " - " + podMembersMap.get(id));
-            }
-        });
 
+        //REMOVE MY NAME BY CHECKING NAME
         int id1 = 308323;
         String name = "Srujan";
         if (podMembersMap.containsKey(id1) && podMembersMap.containsValue(name)) {
@@ -184,8 +186,64 @@ public class CollectionsKushal {
             podMembersMap.values().remove(name);
         }
         podMembersMap.forEach((id, name1) -> System.out.println(id + " - " + name1));
+    }
 
+    public void doMapIterationExcludingMyNameUsingStream(Map<Integer, String> podMembersMap) {
+        //REMOVE MY NAME BY CHECKING ID
+        System.out.println("Result Using one stream and by id");
+        podMembersMap.forEach((id, name) -> {
+            if (id != 308323) {
+                System.out.println(id + " - " + podMembersMap.get(id));
+            }
+        });
 
-        //  Map<Integer,String>resultMap = new HashMap<>();
+        //REMOVE MY NAME BY CHECKING VALUE
+
+    }
+
+    public void doMapIterationRemoveDuplicatesFromTwoMapsUsingLoop(Map<Integer, String> podMembersMap, Map<Integer, String> nonPodMembersMap) {
+        Map<Integer, String> mergedMap = new HashMap<>();
+        Map<Integer, String> resultMap = new HashMap<>();
+        Map<Integer, String> resultMap1 = new HashMap<>();
+        mergedMap.putAll(podMembersMap);
+        mergedMap.putAll(nonPodMembersMap);
+
+        //USING TWO FOR LOOP
+        System.out.println("Result Using Two For Loops");
+        for (Integer key : resultMap.keySet()) {
+            if (!podMembersMap.containsKey(key)) {
+                resultMap.put(key, resultMap.get(key));
+            }
+        }
+
+        for (Integer key : resultMap.keySet()) {
+            if (!nonPodMembersMap.containsKey(key)) {
+                resultMap.put(key, resultMap.get(key));
+            }
+        }
+        resultMap.forEach((id, name) -> System.out.println(id + " - " + name));
+
+        //USING ONE FOR LOOP
+        System.out.println("Result Using One For Loop");
+        for (Map.Entry<Integer, String> id : mergedMap.entrySet()) {
+            if (!(((podMembersMap.containsKey(id.getKey())) && (nonPodMembersMap.containsKey(id.getKey())))
+                    || (podMembersMap.containsValue(id.getValue()) && nonPodMembersMap.containsValue(id.getValue())))) {
+                resultMap1.put(id.getKey(), id.getValue());
+            }
+        }
+        resultMap1.forEach((id, name) -> System.out.println(id + " - " + name));
+    }
+
+    public void doMapIterationRemoveDuplicatesFromTwoMapsUsingStream(Map<Integer, String> podMembersMap, Map<Integer, String> nonPodMembersMap) {
+        Map<Integer, String> mergedMap = new HashMap<>();
+        Map<Integer, String> resultMap = new HashMap<>();
+        mergedMap.putAll(podMembersMap);
+        mergedMap.putAll(nonPodMembersMap);
+        System.out.println("Result Using Stream");
+        mergedMap.entrySet().stream().filter(myKey ->
+                        !((podMembersMap.containsKey(myKey.getKey()) && nonPodMembersMap.containsKey(myKey.getKey())) ||
+                                (podMembersMap.containsValue(myKey.getValue()) && nonPodMembersMap.containsValue(myKey.getValue()))))
+                .forEach(entry -> resultMap.put(entry.getKey(), entry.getValue()));
+        resultMap.forEach((id, name) -> System.out.println(id + " - " + name));
     }
 }
--- src/com/ust/MainKushal.java
+++ src/com/ust/MainKushal.java
@@ -1,94 +1,147 @@
 package com.ust;
 
+import com.ust.Impl.*;
+
+import com.ust.Impl.BikeKushalImpl;
+import com.ust.Impl.TruckAbstractKushal;
+
 import java.util.*;
 
 public class MainKushal {
     static void main() {
-//        ArithmeticOperationKushal aok = new ArithmeticOperationKushal();
-//        System.out.println("Addition of two numbers is : " + " " + aok.add(6, 2));
-//        System.out.println("Addition of three numbers is : " + " " + aok.add(6, 2, 10));
-//        System.out.println("Subtraction of two numbers is : " + " " + aok.subtract(6, 2));
-//        System.out.println("Multiplication of two numbers is : " + " " + aok.multiply(6, 2));
-//        System.out.println("Division of two numbers is : " + " " + aok.division(6, 2));
 
-    /*
-    public static int addNum(int n1, int n2) {
-        return n1 + n2;
-    }*/
-//        BikeKushal bk = new BikeKushal();
-//        CarKushal ck = new CarKushal();
-//        BusKushal buk = new BusKushal();
-//        TruckKushal tk = new TruckKushal();
+        //Arithmetic Operations
+        System.out.println();
+        System.out.println("ArithMetic Operations");
+        ArithmeticOperationKushal aok = new ArithmeticOperationKushal();
+        System.out.println("Addition of two numbers is : " + " " + aok.add(6, 2));
+        System.out.println("Addition of three numbers is : " + " " + aok.add(6, 2, 10));
+        System.out.println("Subtraction of two numbers is : " + " " + aok.subtract(6, 2));
+        System.out.println("Multiplication of two numbers is : " + " " + aok.multiply(6, 2));
+        System.out.println("Division of two numbers is : " + " " + aok.division(6, 2));
+
+        //CLASSES AND INHERITANCE
+        System.out.println();
+        System.out.println("Classes & Interfaces");
+        BikeKushal bk = new BikeKushal();
+        CarKushal ck = new CarKushal();
+        BusKushal buk = new BusKushal();
+        TruckKushal tk = new TruckKushal();
+        System.out.println("Bike Brand is : " + bk.getBrand());
+        System.out.println("Bike Doors Count is : " + bk.getDoorsCount());
+        System.out.println("Car Colour is : " + ck.getColour());
+        System.out.println("Car Wheels Count is : " + ck.getWheelsCount());
+        System.out.println("Car Drive Type is is : " + ck.getDriveType());
+        System.out.println("Truck Capacity is : " + tk.getVehicleCapacity());
+        System.out.println("Truck Wheels Count is : " + tk.getWheelsCount());
+        System.out.println("Bus Vehicle Type is : " + buk.getVehicleType());
+        System.out.println("Bus has extra Tyre : " + buk.hasExtraTrye());
+        System.out.println("Truck has AC : " + tk.hasAC());
 //        String carBrand = bk.getBrand();
-//        VehicleKushal carobj = new CarKushal();
-//        System.out.println("Bike Brand is : " + bk.getBrand());
-//        System.out.println("Bike Doors Count is : " + bk.getDoorsCount());
-//        System.out.println("Car Colour is : " + ck.getColour());
-//        System.out.println("Car Wheels Count is : " + ck.getWheelsCount());
-//        System.out.println("Car Drive Type is is : " + ck.getDriveType());
-//        System.out.println("Truck Capacity is : " + tk.getVehicleCapacity());
-//        System.out.println("Truck Wheels Count is : " + tk.getWheelsCount());
-//        System.out.println("Bus Vehicle Type is : " + buk.getVehicleType());
-//        System.out.println("Bus has extra Tyre : " + buk.hasExtraTrye());
-//        System.out.println("Truck has AC : " + tk.hasAC());
 
-//        BikeKushalImpl bki = new BikeKushalImpl();
-//        CarKushalImpl cki = new CarKushalImpl();
-//        TruckKushalImpl tki = new TruckKushalImpl();
-//        BusKushalImpl buki = new BusKushalImpl();
-//
-//        System.out.println("Car Colour is : " + cki.getColour());
-//        System.out.println("Bus Brand is : " + buki.getBrand());
-//        System.out.println("Bike Comes Under Category of type : " + bki.getVehicleType());
-//        System.out.println("Truck Has AC : " + tki.hasAC());
-//        System.out.println("Truck Has This many Doors : " + tki.getDoorsCount());
+        //INTERFEACES & METHODS
+        System.out.println();
+        System.out.println("Using Interfaces & Methods");
+        //VehicleKushal carobj = new CarKushal();
+        BikeKushalImpl bki = new BikeKushalImpl();
+        com.ust.impl.CarKushalImpl cki = new com.ust.impl.CarKushalImpl();
+        com.ust.impl.TruckKushalImpl tki = new com.ust.impl.TruckKushalImpl();
+        com.ust.impl.BusKushalImpl buki = new com.ust.impl.BusKushalImpl();
 
-//        BikeAbstractKushal bak = new BikeAbstractKushal();
-//        BusAbstractKushal buak = new BusAbstractKushal();
-//        TruckAbstractKushal tak = new TruckAbstractKushal();
-//        CarAbstractKushal cak = new CarAbstractKushal();
-//
-//        System.out.println("Car Colour is : " + cak.getColour());
-//        System.out.println("Bus Brand is : " + buak.getBrand());
-//        System.out.println("Bike Comes Under Category of type : " + bak.getVehicleType());
-//        System.out.println("Truck Has Windows : " + tak.hasWindows());
-//        System.out.println("Truck Has This many Doors : " + tak.hasWindows());
-        CollectionsKushal ck = new CollectionsKushal();
+        System.out.println("Car Colour is : " + cki.getColour());
+        System.out.println("Bus Brand is : " + buki.getBrand());
+        System.out.println("Bike Comes Under Category of type : " + bki.getVehicleType());
+        System.out.println("Truck Has AC : " + tki.hasAC());
+        System.out.println("Truck Has This many Doors : " + tki.getDoorsCount());
 
-//        List<String> podMembers = new ArrayList<>();
-//        List<String> nonPodMembers = new ArrayList<>(List.of("Raja","Kushal","Vignesh","Rajith","Sanjay","Srujan","Srujan"));
-//        podMembers.add("Kushal");
-//        podMembers.add("Srujan");
-//        podMembers.add("Faijul");
-//        podMembers.add("Shoaib");
-//        podMembers.add("Hema");
-//        podMembers.add("Gopi");
-//
-//        HashSet<String> podMembersSet = new HashSet<>();
-//        HashSet<String> nonPodMembersSet = new HashSet<>(Set.of("Raja","Kushal","Vignesh","Rajith","Sanjay","Srujan"));
-//        podMembersSet.add("Kushal");
-//        podMembersSet.add("Srujan");
-//        podMembersSet.add("Faijul");
-//        podMembersSet.add("Shoaib");
-//        podMembersSet.add("Hema");
-//        podMembersSet.add("Gopi");
+        //ABSTRACT CLASSES
+        System.out.println();
+        System.out.println("Using Abstract Classes");
+        // BikeAbstractKushal bak = new com.ust.impl.BikeAbstractKushal();
+        com.ust.impl.BusAbstractKushal buak = new com.ust.impl.BusAbstractKushal();
+        TruckAbstractKushal tak = new TruckAbstractKushal();
+        com.ust.impl.CarAbstractKushal cak = new com.ust.impl.CarAbstractKushal();
 
-//        ck.doIteration(podMembers);
-//        ck.doIterationExcludingNeighbour(podMembers);
-//        ck.doIterationRemoveDuplicatesFromTwoLists1(podMembers,nonPodMembers);
-//        ck.doIteration(podMembersSet,nonPodMembersSet);
-//        ck.doIterationRemoveDuplicatesFromTwoSets(podMembersSet,nonPodMembersSet);
-//        ck.doIterationRemoveDuplicatesFromTwoSets1(podMembersSet,nonPodMembersSet);
+        System.out.println("Car Colour is : " + cak.getColour());
+        System.out.println("Bus Brand is : " + buak.getBrand());
+        //System.out.println("Bike Comes Under Category of type : " + bak.getVehicleType());
+        System.out.println("Truck Has Windows : " + tak.hasWindows());
+        System.out.println("Truck Has This many Doors : " + tak.hasWindows());
 
-        Map<Integer, String> podMembers = new HashMap<>();
-        podMembers.put(308323, "Kushal");
-        podMembers.put(308348, "Srujan");
-        podMembers.put(308351, "Fijul");
-        podMembers.put(308232, "Shoaib");
-        podMembers.put(308250, "Hema");
-        podMembers.put(308340, "Gopinath");
+        //COLLECTIONS
 
-//        ck.doIteration(podMembers);
-        ck.doIterationExcludingMyName(podMembers);
+        CollectionsKushal colk = new CollectionsKushal();
+
+        //MAPS
+
+        Map<Integer, String> podMembersMap = new HashMap<>();
+        podMembersMap.put(308323, "Kushal");
+        podMembersMap.put(308348, "Srujan");
+        podMembersMap.put(308351, "Fijul");
+        podMembersMap.put(308232, "Shoaib");
+        podMembersMap.put(308250, "Hema");
+        podMembersMap.put(308340, "Gopinath");
+
+        Map<Integer, String> nonPodMembersMap = new HashMap<>();
+        nonPodMembersMap.put(308323, "Kushal");
+        nonPodMembersMap.put(22235, "Raja");
+        nonPodMembersMap.put(2322, "Vignesh");
+        nonPodMembersMap.put(3012, "Shabnam");
+        nonPodMembersMap.put(3080, "Sanjay");
+        nonPodMembersMap.put(3340, "Srujan");
+
+        System.out.println();
+        System.out.println("Using Maps");
+
+        colk.doMapIterationUsingForLoop(podMembersMap);
+        colk.doMapIterationUsingStreams(nonPodMembersMap);
+
+        colk.doMapIterationExcludingMyNameUsingForLoop(podMembersMap);
+        colk.doMapIterationExcludingMyNameUsingStream(nonPodMembersMap);
+
+        colk.doMapIterationRemoveDuplicatesFromTwoMapsUsingLoop(podMembersMap, nonPodMembersMap);
+        colk.doMapIterationRemoveDuplicatesFromTwoMapsUsingStream(podMembersMap, nonPodMembersMap);
+
+        //LISTS
+        List<String> podMembersList = new ArrayList<>();
+        List<String> nonPodMembersList = new ArrayList<>(List.of("Raja", "Kushal", "Vignesh", "Rajith", "Sanjay", "Srujan", "Srujan"));
+        podMembersList.add("Kushal");
+        podMembersList.add("Srujan");
+        podMembersList.add("Faijul");
+        podMembersList.add("Shoaib");
+        podMembersList.add("Hema");
+        podMembersList.add("Gopi");
+
+        System.out.println();
+        System.out.println("Using Lists");
+
+        colk.doListIterationUsingForLoop(podMembersList);
+        colk.doListIterationUsingStream(podMembersList);
+
+        colk.doListIterationExcludeNeighbourUsingLoop(podMembersList);
+        colk.doListIterationExcludeNeighbourUsingStream(podMembersList);
+
+        colk.doListIterationRemoveDuplicatesFromTwoListsUsingForLoop(podMembersList, nonPodMembersList);
+        colk.doListIterationRemoveDuplicatesFromTwoListsUsingStream(podMembersList, nonPodMembersList);
+
+        //SETS
+        HashSet<String> podMembersSet = new HashSet<>();
+        HashSet<String> nonPodMembersSet = new HashSet<>(Set.of("Raja", "Kushal", "Vignesh", "Rajith", "Sanjay", "Srujan"));
+        podMembersSet.add("Kushal");
+        podMembersSet.add("Srujan");
+        podMembersSet.add("Faijul");
+        podMembersSet.add("Shoaib");
+        podMembersSet.add("Hema");
+        podMembersSet.add("Gopi");
+
+        System.out.println();
+        System.out.println("Using Sets");
+
+        colk.doSetIterationUsingForLoop(podMembersSet, nonPodMembersSet);
+        colk.doSetIterationUsingStream(podMembersSet, nonPodMembersSet);
+
+        colk.doSetIterationRemoveDuplicatesFromTwoSetsUsingForLoop(podMembersSet, nonPodMembersSet);
+        colk.doSetIterationRemoveDuplicatesFromTwoSetsUsingStream(podMembersSet, nonPodMembersSet);
+
     }
 }
