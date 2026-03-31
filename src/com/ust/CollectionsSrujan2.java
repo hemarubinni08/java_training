f841b031cb6f96db59555f593e18ecfd57df9cb3
srujan <123@gmail.com>
2026-03-31 10:12:00 +0530

    Java collections properly formatted  code.

--- src/com/ust/CollectionsSrujan2.java
+++ src/com/ust/CollectionsSrujan2.java
@@ -8,6 +8,7 @@ public class CollectionsSrujan2 {
         for (Integer keyValues : podMembersMap.keySet()) {
             System.out.println(keyValues + "-" + podMembersMap.get(keyValues));
         }
+
         for (Map.Entry<Integer, String> hashes : podMembersMap.entrySet()) {
             Integer idValues = hashes.getKey();
             String names = hashes.getValue();
@@ -27,6 +28,7 @@ public class CollectionsSrujan2 {
         if (podMembersMap.containsKey(308348)) {
             podMembersMap.remove(308348);
         }
+
         for (Integer keyValues : podMembersMap.keySet()) {
             System.out.println(keyValues + "-" + podMembersMap.get(keyValues));
         }
@@ -36,45 +38,44 @@ public class CollectionsSrujan2 {
         if (podMembersMap.containsValue("Srujan")) {
             podMembersMap.values().remove("Srujan");
         }
+
         for (Integer keyValues : podMembersMap.keySet()) {
             System.out.println(keyValues + "-" + podMembersMap.get(keyValues));
         }
-
-        podMembersMap.entrySet().stream().filter(myName ->
-                !(myName.getValue().equals("Srujan"))).forEach(name ->
-                System.out.println(name.getKey() + "-" + name.getValue()));
     }
 
-    public HashMap doIteration2MapsSrujan(Map<Integer, String> podMembersMap1,
-                                          Map<Integer, String> podMembersMap2) {
+    public void doIterationContainValueLambda(Map<Integer, String> podMembersMap) {
+
+        podMembersMap.entrySet().stream().filter(myName -> !(myName.getValue().equals("Srujan"))).
+                forEach(name -> System.out.println(name.getKey() + "-" + name.getValue()));
+    }
+
+    public HashMap doIteration2MapsSrujan(Map<Integer, String> podMembersMap1, Map<Integer, String> podMembersMap2) {
         HashMap<Integer, String> mergedMap = new HashMap<>();
         HashMap<Integer, String> temporaryMap = new HashMap<>();
         mergedMap.putAll(podMembersMap1);
         mergedMap.putAll(podMembersMap2);
         HashMap<Integer, String> resultMap = new HashMap<>();
         for (Map.Entry<Integer, String> keyValues : mergedMap.entrySet()) {
-            if (!((podMembersMap1.containsKey(keyValues.getKey()) && podMembersMap2.containsKey(keyValues.getKey())) ||
-                    (podMembersMap1.containsValue(keyValues.getValue()) && podMembersMap2.containsValue(keyValues.getValue())))) {
+            if (!((podMembersMap1.containsKey(keyValues.getKey()) && podMembersMap2.containsKey(keyValues.getKey()))
+                    || (podMembersMap1.containsValue(keyValues.getValue()) && podMembersMap2.containsValue(keyValues.getValue())))) {
                 resultMap.put(keyValues.getKey(), keyValues.getValue());
             }
         }
         return resultMap;
     }
 
-    public HashMap doIteration2MapsSrujanLambda(Map<Integer, String> podMembersMap1,
-                                                Map<Integer, String> podMembersMap2) {
+    public Map<Integer, String> doIteration2MapsSrujanLambda(Map<Integer, String> podMembersMap1, Map<Integer, String> podMembersMap2) {
         HashMap<Integer, String> mergedMap = new HashMap<>();
         HashMap<Integer, String> temporaryMap = new HashMap<>();
         mergedMap.putAll(podMembersMap1);
         mergedMap.putAll(podMembersMap2);
         HashMap<Integer, String> resultMap = new HashMap<>();
-        mergedMap.entrySet().stream().filter(myKey ->
-                        !((podMembersMap1.containsKey(myKey.getKey()) && podMembersMap2.containsKey(myKey.getKey())) ||
-                                (podMembersMap1.containsValue(myKey.getValue()) && podMembersMap2.containsValue(myKey.getValue())))).
+        mergedMap.entrySet().stream().filter(myKey -> !
+                        ((podMembersMap1.containsKey(myKey.getKey()) && podMembersMap2.containsKey(myKey.getKey()))
+                                || (podMembersMap1.containsValue(myKey.getValue()) && podMembersMap2.containsValue(myKey.getValue())))).
                 forEach(entry -> resultMap.put(entry.getKey(), entry.getValue()));
 
         return resultMap;
     }
 }
-
-
--- src/com/ust/MainSrujan.java
+++ src/com/ust/MainSrujan.java
@@ -22,8 +22,6 @@ public class MainSrujan {
         namesPod2Set.add("Vinaya");
         namesPod2Set.add("Varshit");
 
-
-
         List<String> namesPod1 = new ArrayList<>();
         namesPod1.add("Srujan");
         namesPod1.add("Kushal");
@@ -40,26 +38,45 @@ public class MainSrujan {
         namesPod2.add("Varshit");
         namesPod2.add("Srujan");
 
-        Map<Integer ,String> namesPod1Map = new HashMap<>();
-        namesPod1Map.put(308351,"Fijul");
-        namesPod1Map.put(308348,"Srujan");
-        namesPod1Map.put(308232,"Shoaib");
-        namesPod1Map.put(308250,"Hema");
-        namesPod1Map.put(308323,"Kushal");
+        Map<Integer, String> namesPod1Map = new HashMap<>();
+        namesPod1Map.put(308351, "Fijul");
+        namesPod1Map.put(308348, "Srujan");
+        namesPod1Map.put(308232, "Shoaib");
+        namesPod1Map.put(308250, "Hema");
+        namesPod1Map.put(308323, "Kushal");
         namesPod1Map.put(308340, "Gopi");
 
-        Map<Integer ,String> namesPod2Map = new HashMap<>();
-        namesPod2Map.put(308348,"Srujan");
-        namesPod2Map.put(123457,"Durga");
-        namesPod2Map.put(123458,"Hema");
-        namesPod2Map.put(123459,"Abhinav");
-        namesPod2Map.put(123450,"Vishnu");
+        Map<Integer, String> namesPod2Map = new HashMap<>();
+        namesPod2Map.put(308348, "Srujan");
+        namesPod2Map.put(123457, "Durga");
+        namesPod2Map.put(123458, "Hema");
+        namesPod2Map.put(123459, "Abhinav");
+        namesPod2Map.put(123450, "Vishnu");
         namesPod2Map.put(012345, "Madhav");
 
+        CollectionsSrujan objectToTestSrujan = new CollectionsSrujan();
+        objectToTestSrujan.doIterationSrujan(namesPod1);
+        objectToTestSrujan.doIteration2Srujan(namesPod1);
+        objectToTestSrujan.doIteration2SrujanLambda(namesPod1);
+        objectToTestSrujan.doIterationNames1Names2(namesPod1,namesPod2);
+        objectToTestSrujan.doIterationNames1Names2Lambda(namesPod1,namesPod2);
+        objectToTestSrujan.doIterationNames1Names2Merged(namesPod1,namesPod2);
+        objectToTestSrujan.doIterationNames1Names2MergedLambda(namesPod1,namesPod2);
+        objectToTestSrujan.doIterationHashSet(namesPod1Set);
+        objectToTestSrujan.doIteration2NamesSet(namesPod1Set,namesPod2Set);
+        objectToTestSrujan.doIteration2NamesSetLambda(namesPod1Set,namesPod2Set);
+        objectToTestSrujan.doIterationMergedSet(namesPod1Set,namesPod2Set);
+        objectToTestSrujan.doIterationMergedSetLambda(namesPod1Set,namesPod2Set);
+        objectToTestSrujan.doIterationTempSet(namesPod1Set,namesPod2Set);
+        objectToTestSrujan.doIterationTempSetLambda(namesPod1Set,namesPod2Set);
 
-
-
-        CollectionsSrujan2 objectToTestSrujan = new CollectionsSrujan2();
-        System.out.println(objectToTestSrujan.doIteration2MapsSrujan(namesPod1Map,namesPod2Map));
+        CollectionsSrujan2 objectToTestSrujan2 = new CollectionsSrujan2();
+        objectToTestSrujan2.doIterationMap(namesPod1Map);
+        objectToTestSrujan2.doIterationSkipMap(namesPod1Map);
+        objectToTestSrujan2.doIterationContainKey(namesPod1Map);
+        objectToTestSrujan2.doIterationContainValue(namesPod1Map);
+        objectToTestSrujan2.doIterationContainValueLambda(namesPod1Map);
+        objectToTestSrujan2.doIteration2MapsSrujan(namesPod1Map,namesPod2Map);
+        objectToTestSrujan2.doIteration2MapsSrujanLambda(namesPod1Map,namesPod2Map);
     }
 }
